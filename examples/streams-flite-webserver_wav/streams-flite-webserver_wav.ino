87119c0890dae587c4d1b886e91f51efd9038be6
Phil Schatzmann <phil.schatzmann@gmail.com>
2021-06-23 10:15:34 +0200

    example for Flite TTS engine

---
+++ examples/streams-flite-webserver_wav/streams-flite-webserver_wav.ino
@@ -0,0 +1,38 @@
+/**
+ * @file streams-flite-webserver_wav.ino
+ * 
+ * @author Phil Schatzmann
+ * @copyright GPLv3
+ * 
+ */
+
+#include "flite_arduino.h"
+#include "AudioWAVServer.h"
+
+using namespace audio_tools;  
+
+AudioWAVServer server("ssid","password");
+
+// Callback which provides the audio data 
+void outputData(Stream &out){
+  Serial.print("providing data...");
+  Flite flite(out);
+
+  // Setup Audio Info
+  FliteOutputBase *o = flite.getOutput();
+  
+  flite.say("Hallo, my name is Alice");
+  Serial.printf("info %d, %d, %d", o->sampleRate(), o->channels(), o->bitsPerSample());
+}
+
+void setup(){
+  Serial.begin(115200);
+  server.begin(outputData, 8000,1,16);
+}
+
+
+// Arduino loop  
+void loop() {
+  // Handle new connections
+  server.doLoop();  
+}
