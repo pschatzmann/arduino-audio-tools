aa723ab34a7c03d831a818bef07e0b7f514088cd
Phil Schatzmann <phil.schatzmann@gmail.com>
2021-07-24 14:16:51 +0200

    Portaudio support

--- examples-desktop/generator/generator.cpp
+++ examples-desktop/generator/generator.cpp
@@ -1,33 +1,3 @@
+// Simple wrapper to make Arduino sketch compilable by cpp in cmake
 #include "Arduino.h"
-#include "AudioTools.h"
-
-using namespace audio_tools;  
-
-uint16_t sample_rate=44100;
-uint8_t channels = 2;                                     // The stream will have 2 channels 
-SineWaveGenerator<int16_t> sineWave(32000);               // subclass of SoundGenerator with max amplitude of 32000
-GeneratedSoundStream<int16_t> in(sineWave, channels);     // Stream generated from sine wave
-DefaultStream out;                                        // On desktop we use 
-StreamCopy copier(out, in); // copy in to out
-
-// Arduino Setup
-void setup(void) {
-  Serial.begin(115200);
-
-  // open output
-  auto config = out.defaultConfig();
-  config.sample_rate = sample_rate;
-  config.channels = channels;
-  config.bits_per_sample = sizeof(int16_t)*8;
-  out.begin();
-
-  // Setup sine wave
-  sineWave.begin(sample_rate, B4);
-
-}
-
-// Arduino loop  
-void loop() {
-  if (out)
-    copier.copy();
-}
+#include "generator.ino"
---
+++ examples-desktop/generator/generator.ino
@@ -0,0 +1,32 @@
+#include "AudioTools.h"
+
+using namespace audio_tools;  
+
+uint16_t sample_rate=44100;
+uint8_t channels = 2;                                     // The stream will have 2 channels 
+SineWaveGenerator<int16_t> sineWave(32000);               // subclass of SoundGenerator with max amplitude of 32000
+GeneratedSoundStream<int16_t> in(sineWave, channels);     // Stream generated from sine wave
+DefaultStream out;                                        // On desktop we use 
+StreamCopy copier(out, in); // copy in to out
+
+// Arduino Setup
+void setup(void) {
+  Serial.begin(115200);
+
+  // open output
+  auto config = out.defaultConfig();
+  config.sample_rate = sample_rate;
+  config.channels = channels;
+  config.bits_per_sample = sizeof(int16_t)*8;
+  out.begin();
+
+  // Setup sine wave
+  sineWave.begin(sample_rate, B4);
+
+}
+
+// Arduino loop  
+void loop() {
+  if (out)
+    copier.copy();
+}
