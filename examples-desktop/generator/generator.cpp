ec3ee52208d4dc4dc5e991e1573375d5adeedac2
Phil Schatzmann <phil.schatzmann@gmail.com>
2021-07-24 15:28:03 +0200

    Portaudio correction

--- examples-desktop/generator/generator.cpp
+++ examples-desktop/generator/generator.cpp
@@ -1,3 +1,11 @@
 // Simple wrapper to make Arduino sketch compilable by cpp in cmake
 #include "Arduino.h"
 #include "generator.ino"
+
+// Privide main()
+int main(){
+  setup();
+  while(true){
+    loop();
+  }
+}
