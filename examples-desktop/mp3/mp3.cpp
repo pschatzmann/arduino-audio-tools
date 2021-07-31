7a53ee0dd2cdbe9ec2e8367b71adb19196a15f77
Phil Schatzmann <phil.schatzmann@gmail.com>
2021-07-31 12:05:53 +0200

    mp3 example

--- examples-desktop/mp3/mp3.cpp
+++ examples-desktop/mp3/mp3.cpp
@@ -8,16 +8,17 @@ using namespace audio_tools;
 
 // MemoryStream -> AudioOutputStream -> MP3Decoder -> CsvStream
 MemoryStream mp3(BabyElephantWalk60_mp3, BabyElephantWalk60_mp3_len);
-PortAudioStream portaudio_stream;   // Output of sound on desktop 
-MP3DecoderMini decoder(portaudio_stream);  // decode wav to pcm and send it to printer
-AudioOutputStream out(decoder);     // output to decoder
-StreamCopy copier(out, mp3);        // copy in to out
+PortAudioStream portaudio_stream;       // Output of sound on desktop 
+MP3DecoderMini mp3_decoder(portaudio_stream);  // decode wav to pcm and send it to printer
+AudioOutputStream out(mp3_decoder);     // output to decoder
+StreamCopy copier(out, mp3);            // copy in to out
 
 
 /// open portaudio with audio inform provided by mp3
 void mp3InfoCallback(MP3MiniAudioInfo &info){
     PortAudioConfig pc;
     pc = info;
+    pc.is_output = true;
     portaudio_stream.begin(pc);
 }
 
@@ -25,14 +26,15 @@ void setup(){
   Serial.begin(115200);
   AudioLogger::instance().begin(Serial, AudioLogger::Debug);  
   // this is not really necessary
-  decoder.begin();
+  mp3_decoder.setMP3InfoCallback(mp3InfoCallback);
+  mp3_decoder.begin();
 }
 
 void loop(){
   if (mp3) {
     copier.copy();
   } else {
-    auto info = decoder.audioInfo();
+    auto info = mp3_decoder.audioInfo();
     LOGI("The audio rate from the mp3 file is %d", info.sample_rate);
     LOGI("The channels from the mp3 file is %d", info.channels);
     exit(0);
