3cfe6dd05373a042de2d74ed22fdadd4981e30a1
Phil Schatzmann <phil.schatzmann@gmail.com>
2021-08-05 09:39:09 +0200

    helix aac with tests

--- tests/aac-helix/aac-helix.cpp
+++ tests/aac-helix/aac-helix.cpp
@@ -1,21 +1,21 @@
 #include "Arduino.h"
 #include "AudioTools.h"
-#include "AudioCodecs/CodecAAC.h"
+#include "AudioCodecs/CodecAACHelix.h"
 #include "audio.h"
 
 using namespace audio_tools;  
 
 MemoryStream aac(gs_16b_2c_44100hz_aac, gs_16b_2c_44100hz_aac_len);
 PortAudioStream portaudio_stream;   // Output of sound on desktop 
-EncodedAudioStream enc(portaudio_stream, new AACDecoder()); // aac data source
-StreamCopy copier(enc, aac); // copy in to out
+EncodedAudioStream dec(portaudio_stream, new AACDecoderHelix()); // aac data source
+StreamCopy copier(dec, aac); // copy in to out
 
 void setup(){
   Serial.begin(115200);
   AudioLogger::instance().begin(Serial, AudioLogger::Info);  
 
-  in.setNotifyAudioChange(portaudio_stream);
-  in.begin();
+  dec.setNotifyAudioChange(portaudio_stream);
+  dec.begin();
 
   portaudio_stream.begin();
 }
@@ -24,9 +24,14 @@ void loop(){
   if (aac) {
     copier.copy();
   } else {
-    auto info = in.decoder().audioInfo();
+    auto info = dec.decoder().audioInfo();
     LOGI("The audio rate from the aac file is %d", info.sample_rate);
     LOGI("The channels from the aac file is %d", info.channels);
     exit(0);
   }
+}
+
+int main(){
+  setup();
+  while(true) loop();
 }
