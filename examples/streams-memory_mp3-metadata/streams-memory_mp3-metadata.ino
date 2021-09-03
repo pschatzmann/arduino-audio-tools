f20d89ff0818f10fcbafa88dfdc45c0268a621c9
Phil Schatzmann <phil.schatzmann@gmail.com>
2021-09-03 09:20:23 +0200

    mp3 metadata example

---
+++ examples/streams-memory_mp3-metadata/streams-memory_mp3-metadata.ino
@@ -0,0 +1,43 @@
+/**
+ * @file streams-memory_mp3-metadata.ino
+ * @brief In this example we provide the metadata from a MP3 file. 
+ * @author Phil Schatzmann
+ * @copyright GPLv3
+ */
+
+#include "AudioTools.h"
+#include "sample-12s.h"
+
+using namespace audio_tools;  
+
+MemoryStream mp3(sample_12s_mp3, sample_12s_mp3_len);
+MetaDataID3 out;
+StreamCopy copier(out, mp3); // copy in to out
+bool title_printed = false;
+
+// callback for meta data
+void printMetaData(MetaDataType type, const char* str, int len){
+  Serial.print("==> ");
+  Serial.print(MetaDataTypeStr[type]);
+  Serial.print(": ");
+  Serial.println(str);
+  title_printed = true;
+}
+
+void setup(){
+  Serial.begin(115200);
+  AudioLogger::instance().begin(Serial, AudioLogger::Info);  
+
+  out.setCallback(printMetaData);
+  out.begin();
+  mp3.begin();
+}
+
+void loop(){
+  if (mp3) {
+    copier.copy();
+  } else {
+    stop();
+  }
+}
+
