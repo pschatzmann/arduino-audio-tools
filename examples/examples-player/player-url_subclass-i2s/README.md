2c8d0796478709a807e3c78296365487557574a1
Phil Schatzmann <phil.schatzmann@gmail.com>
2021-11-06 14:15:07 +0100

    Suclassing Example

---
+++ examples/examples-player/player-url_subclass-i2s/README.md
@@ -0,0 +1,10 @@
+# A Simple Streaming Audio Player
+
+This is just a simple example which demonstrates how to use your own subclasses to add additional functionality.
+
+We provide the URL ICY parameters in our own AudioSourceIcyUrl subclass!
+
+### Dependencies
+
+- https://github.com/pschatzmann/arduino-audio-tools
+- https://github.com/pschatzmann/arduino-libhelix
---
+++ examples/examples-player/player-url_subclass-i2s/player-url_subclass-i2s.ino
@@ -0,0 +1,85 @@
+/**
+ * @file player-url-i2s.ino
+ * @brief see https://github.com/pschatzmann/arduino-audio-tools/blob/main/examples/examples-player/player-url_subclass-i2s/README.md
+ * 
+ * @author Phil Schatzmann
+ * @copyright GPLv3
+ */
+
+// set this in AudioConfig.h or here after installing https://github.com/pschatzmann/arduino-libhelix.git
+#define USE_HELIX 
+
+#include "AudioTools.h"
+#include "AudioCodecs/CodecMP3Helix.h"
+#include "AudioSourceIcyUrl.h"
+
+using namespace audio_tools;  
+
+const char *urls[] = {
+  "http://centralcharts.ice.infomaniak.ch/centralcharts-128.mp3",
+  "http://centraljazz.ice.infomaniak.ch/centraljazz-128.mp3",
+  "http://centralrock.ice.infomaniak.ch/centralrock-128.mp3",
+  "http://centralcountry.ice.infomaniak.ch/centralcountry-128.mp3",
+  "http://centralfunk.ice.infomaniak.ch/centralfunk-128.mp3"
+};
+const char *wifi = "wifi";
+const char *password = "password";
+
+URLStream urlStream(wifi, password);
+AudioSourceIcyUrl source(urlStream, urls, "audio/mp3");
+I2SStream i2s;
+MP3DecoderHelix decoder;
+AudioPlayer player(source, i2s, decoder);
+
+// additional controls
+const int volumePin = A0;
+Debouncer nextButtonDebouncer(2000);
+const int nextButtonPin = 13;
+
+void printMetaData(MetaDataType type, const char* str, int len){
+  Serial.print("==> ");
+  Serial.print(MetaDataTypeStr[type]);
+  Serial.print(": ");
+  Serial.println(str);
+}
+
+void setup() {
+  Serial.begin(115200);
+  AudioLogger::instance().begin(Serial, AudioLogger::Info);
+
+  // setup output
+  auto cfg = i2s.defaultConfig(TX_MODE);
+  i2s.begin(cfg);
+
+  // setup player
+  player.setCallbackMetadata(printMetaData);
+  player.begin();
+  Serial.println(source.icyName());
+}
+
+// Sets the volume control from a linear potentiometer input
+void updateVolume() {
+  // Reading potentiometer value (range is 0 - 4095)
+  float vol = static_cast<float>(analogRead(volumePin));
+  // min in 0 - max is 1.0
+  player.setVolume(vol/4095.0);
+}
+
+
+// Moves to the next url when we touch the pin
+void updatePosition() {
+   if (digitalRead(nextButtonPin)) {
+      Serial.println("Moving to next url");
+      if (nextButtonDebouncer.debounce()){
+        player.next();
+        Serial.println(source.icyName());
+      }
+  }
+}
+
+
+void loop() {
+  //updateVolume(); // remove comments to activate volume control
+  //updatePosition();  // remove comments to activate position control
+  player.copy();
+}
