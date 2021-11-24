710bbf4bbc391adabdc6280702d20981929afa6c
PODAEN <Davy.podevijn@gmail.com>
2021-11-24 13:46:59 +0100

    Added using namespace

--- src/AudioTools.h
+++ src/AudioTools.h
@@ -39,8 +39,11 @@
  * @brief typedefs for Default
  * 
  */
-//#if defined(__linux__) || defined(_WIN32) || defined(__APPLE__)
-//typedef PortAudioStream DefaultStream;
-//#elif defined(ESP32) || defined(ESP8266) || defined(__SAMD21G18A__)
-//typedef I2SStream DefaultStream;
-//#endif
+
+using namespace audio_tools;
+
+#if defined(__linux__) || defined(_WIN32) || defined(__APPLE__)
+typedef PortAudioStream DefaultStream;
+#elif defined(ESP32) || defined(ESP8266) || defined(__SAMD21G18A__)
+typedef I2SStream DefaultStream;
+#endif
