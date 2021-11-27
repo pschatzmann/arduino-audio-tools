5180c47fcc0185fbf4f270884b8a46de8ba1ae8a
Phil Schatzmann <phil.schatzmann@gmail.com>
2021-11-27 18:56:59 +0100

    ICYStream in Readme

--- src/AudioHttp/ICYStream.h
+++ src/AudioHttp/ICYStream.h
@@ -11,7 +11,7 @@ namespace audio_tools {
  * @brief Icecast/Shoutcast Audio Stream which splits the data into metadata and audio data. The Audio data is provided via the
  * regular stream functions. The metadata is handled with the help of the MetaDataICY state machine and provided via a callback method.
  * 
- * This is basically just a URLStreamD with the metadata turned on.
+ * This is basically just a URLStream with the metadata turned on.
  * 
  * @author Phil Schatzmann
  * @copyright GPLv3
