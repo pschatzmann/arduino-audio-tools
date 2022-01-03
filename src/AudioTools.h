d5eb9821cd142f36a23815e99052bc45e9390be2
PODAEN <Davy.podevijn@gmail.com>
2022-01-03 08:04:45 +0100

    Sd and url player support

--- src/AudioTools.h
+++ src/AudioTools.h
@@ -69,8 +69,6 @@ using namespace audio_tools;
  * 
  */
 
-using namespace audio_tools;
-
 #if defined(__linux__) || defined(_WIN32) || defined(__APPLE__)
 typedef PortAudioStream DefaultStream;
 #elif defined(ESP32) || defined(ESP8266) || defined(__SAMD21G18A__)
