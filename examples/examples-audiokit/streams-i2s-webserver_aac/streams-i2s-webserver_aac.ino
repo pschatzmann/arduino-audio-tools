0b849da269e3e287d4af446cca37a998cd9ee8ca
Phil Schatzmann <phil.schatzmann@gmail.com>
2022-01-24 13:18:10 +0100

    AudioKit Examples - mic to webserver

---
+++ examples/examples-audiokit/streams-i2s-webserver_aac/streams-i2s-webserver_aac.ino
@@ -0,0 +1,38 @@
+/**
+ * @file streams-i2s-webserver_aac.ino
+ *
+ *  This sketch reads sound data from the AudioKit. The result is provided as AAC stream which can be listened to in a Web Browser
+ *
+ * @author Phil Schatzmann
+ * @copyright GPLv3
+ */
+#define USE_FDK
+
+#include "AudioTools.h"
+#include "AudioLibs/AudioKit.h"
+
+AudioEncoderServer server(new AACEncoderFDK(),"ssid","password");  
+AudioKitStream kit;    
+
+// Arduino setup
+void setup(){
+  Serial.begin(115200);
+  AudioLogger::instance().begin(Serial, AudioLogger::Warning);
+
+  // start i2s input with default configuration
+  Serial.println("starting AudioKit...");
+  auto config = kit.defaultConfig(RX_MODE);
+  config.input_device = AUDIO_HAL_ADC_INPUT_LINE1;
+  config.sample_rate = 44100;
+  kit.begin(config);
+  Serial.println("AudioKit started");
+
+  // start data sink
+  server.begin(kit, config);
+}
+
+// Arduino loop  
+void loop() {
+  // Handle new connections
+  server.doLoop();  
+}
---
+++ examples/examples-audiokit/streams-i2s-webserver_wav/streams-i2s-webserver_wav.ino
@@ -0,0 +1,37 @@
+/**
+ * @file streams-i2s-webserver_wav.ino
+ *
+ *  This sketch reads sound data from the AudioKit. The result is provided as WAV stream which can be listened to in a Web Browser
+ *
+ * @author Phil Schatzmann
+ * @copyright GPLv3
+ */
+
+#include "AudioTools.h"
+#include "AudioLibs/AudioKit.h"
+
+AudioEncoderServer server(new WAVEncoder(),"ssid","password");  
+AudioKitStream kit;    
+
+// Arduino setup
+void setup(){
+  Serial.begin(115200);
+  AudioLogger::instance().begin(Serial, AudioLogger::Warning);
+
+  // start i2s input with default configuration
+  Serial.println("starting AudioKit...");
+  auto config = kit.defaultConfig(RX_MODE);
+  config.input_device = AUDIO_HAL_ADC_INPUT_LINE1;
+  config.sample_rate = 44100;
+  kit.begin(config);
+  Serial.println("AudioKit started");
+
+  // start data sink
+  server.begin(kit, config);
+}
+
+// Arduino loop  
+void loop() {
+  // Handle new connections
+  server.doLoop();  
+}
