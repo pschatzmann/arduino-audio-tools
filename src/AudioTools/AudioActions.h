03c1ea443e9506dce3fccdf60020ddc8f4970c0c
Phil Schatzmann <phil.schatzmann@gmail.com>
2022-01-27 04:14:28 +0100

    setTouchLimit

--- src/AudioTools/AudioActions.h
+++ src/AudioTools/AudioActions.h
@@ -135,6 +135,7 @@ class AudioActions {
 
   /// Defines the debounce delay
   void setDeboundDelay(int value) { debounceDelayValue = value; }
+  /// Defines the touch limit (Default 20)
   void setTouchLimit(int value) { setTouchLimit = value; }
 
  protected:
