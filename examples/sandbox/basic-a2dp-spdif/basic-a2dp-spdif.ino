847d553bff77c2f673f8e0a50c09cfa3c933c22c
Phil Schatzmann <phil.schatzmann@gmail.com>
2022-01-30 16:16:52 +0100

    a2dp-spdif example - explicit config

--- examples/sandbox/basic-a2dp-spdif/basic-a2dp-spdif.ino
+++ examples/sandbox/basic-a2dp-spdif/basic-a2dp-spdif.ino
@@ -6,6 +6,14 @@
  * @copyright GPLv3
  */
 
+/**
+ * @file basic-a2dp-audiospdif.ino
+ * @brief A2DP Sink with output to SPDIFStream
+ * 
+ * @author Phil Schatzmann
+ * @copyright GPLv3
+ */
+
 #define USE_A2DP
 #include "AudioTools.h"
 #include "AudioExperiments/AudioSPDIF.h"
@@ -22,15 +30,20 @@ void read_data_stream(const uint8_t *data, uint32_t length) {
 void setup() {
   Serial.begin(115200);
   AudioLogger::instance().begin(Serial, AudioLogger::Info);
+  
+  // register callback
+  a2dp_sink.set_auto_reconnect(false);
+  a2dp_sink.set_stream_reader(read_data_stream, false);
+  a2dp_sink.start("a2dp-spdif");
 
   // setup output
   auto cfg = spdif.defaultConfig();
-  cfg.pin_data = 22;
+  cfg.pin_data = 23;
+  cfg.sample_rate = a2dp_sink.sample_rate();
+  cfg.channels = 2;
+  cfg.bits_per_sample = 16;
   spdif.begin(cfg);
 
-  // register callback
-  a2dp_sink.set_stream_reader(read_data_stream, false);
-  a2dp_sink.start("a2dp-spdif");
 }
 
 void loop() {
