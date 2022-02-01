396b38da29b7cb563d8c9d42d60385901fc73e31
Phil Schatzmann <phil.schatzmann@gmail.com>
2022-02-01 15:33:40 +0100

    Compile errors for RP2040

---
+++ src/AudioTools/AudioRuntime.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file AudioRuntime.cpp
+ * @author Phil Schatzmann
+ * @brief Some platform specific exceptional things which can't be implemented in a header
+ * @version 0.1
+ * @date 2022-02-01
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include "AudioConfig.h"
+#include "AudioTools/AudioLogger.h"
+
+#if defined(ARDUINO_ARCH_RP2040) && defined(FIX_SYNC_SYNCHRONIZE)
+extern "C" void __sync_synchronize(){
+    LOGE("__sync_synchronize not implemented")
+}
+#endif
