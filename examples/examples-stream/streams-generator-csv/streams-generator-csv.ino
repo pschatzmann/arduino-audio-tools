8feb86af8dd398833de0262e4f62cc0e10990842
Phil Schatzmann <phil.schatzmann@gmail.com>
2022-02-02 09:53:22 +0100

    Comments

--- examples/examples-stream/streams-generator-csv/streams-generator-csv.ino
+++ examples/examples-stream/streams-generator-csv/streams-generator-csv.ino
@@ -13,7 +13,7 @@ uint8_t channels = 2;                                      // The stream will ha
 SineWaveGenerator<int16_t> sineWave(32000);                // subclass of SoundGenerator with max amplitude of 32000
 GeneratedSoundStream<int16_t> sound(sineWave);             // Stream generated from sine wave
 CsvStream<int16_t> out(Serial); 
-StreamCopy copier(out, sound);                             // copies sound into i2s
+StreamCopy copier(out, sound);                             // copies sound to out
 
 // Arduino Setup
 void setup(void) {  
@@ -21,7 +21,7 @@ void setup(void) {
   Serial.begin(115200);
   AudioLogger::instance().begin(Serial, AudioLogger::Warning);
 
-  // start the bluetooth
+  // Define CSV Output
   auto config = out.defaultConfig();
   config.sample_rate = sample_rate; 
   config.channels = channels;
