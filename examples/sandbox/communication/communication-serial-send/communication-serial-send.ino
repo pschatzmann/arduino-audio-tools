94f4ae938defaa779dc0fcbbfb02f38c45693455
Phil Schatzmann <phil.schatzmann@gmail.com>
2022-03-09 18:06:03 +0100

    Communication compile errors

--- examples/sandbox/communication/communication-serial-send/communication-serial-send.ino
+++ examples/sandbox/communication/communication-serial-send/communication-serial-send.ino
@@ -17,8 +17,8 @@
 
 uint16_t sample_rate = 44100;
 uint8_t channels = 2;  // The stream will have 2 channels
-SineWaveGenerator<sound_t> sineWave( 32000);  // subclass of SoundGenerator with max amplitude of 32000
-GeneratedSoundStream<sound_t> sound( sineWave);                      // Stream generated from sine wave
+SineWaveGenerator<int16_t> sineWave( 32000);  // subclass of SoundGenerator with max amplitude of 32000
+GeneratedSoundStream<int16_t> sound( sineWave);                      // Stream generated from sine wave
 StreamCopy copier(Serial2, sound);  // copies sound into i2s
 
 void setup() {
