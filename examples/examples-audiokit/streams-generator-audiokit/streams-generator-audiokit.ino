dadc4388c269caad94172b0366242b4e620c131b
Phil Schatzmann <phil.schatzmann@gmail.com>
2022-03-11 13:11:05 +0100

    AudioKit Generator broken signal

--- examples/examples-audiokit/streams-generator-audiokit/streams-generator-audiokit.ino
+++ examples/examples-audiokit/streams-generator-audiokit/streams-generator-audiokit.ino
@@ -8,7 +8,7 @@
 #include "AudioTools.h"
 #include "AudioLibs/AudioKit.h"
 
-uint16_t sample_rate=44100;
+uint16_t sample_rate=32000;
 uint8_t channels = 2;                                      // The stream will have 2 channels 
 SineWaveGenerator<int16_t> sineWave(32000);                // subclass of SoundGenerator with max amplitude of 32000
 GeneratedSoundStream<int16_t> sound(sineWave);             // Stream generated from sine wave
