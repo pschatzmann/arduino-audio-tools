4b54517d18454cb587fa1f19734f2ded2781d176
Phil Schatzmann <phil.schatzmann@gmail.com>
2022-04-20 15:19:43 +0200

    BitVector

--- src/AudioBasic/BitVector.h
+++ src/AudioBasic/BitVector.h
@@ -2,6 +2,8 @@
 #include <stdint.h>
 #include "Vector.h"
 
+namespace audio_tools {
+
 /**
  * @brief Space optimized vector which stores the boolean values as bits
  * @author Phil Schatzmann
@@ -30,12 +32,20 @@ class BitVector {
         while(offset>vector.size()){
             vector.push_back(0l);
         }
-        if (value) {
-            // set bit
-            vector[offset] |= 1UL << bit;
-        } else {
-            // clear bit
-            vector[offset] &= ~(1UL << bit);
+        // check if we need to updte the value
+        if (((vector[offset] >> bit) & 1U) != value) {
+            // needs update
+            if (value) {
+                // set bit
+                vector[offset] |= 1UL << bit;
+            } else {
+                // clear bit
+                vector[offset] &= ~(1UL << bit);
+            }
+            // call change handler to notify about change
+            if (changeHandler!=nullptr){
+                changeHandler(index, value, ref);
+            }
         }
     }
 
@@ -44,7 +54,21 @@ class BitVector {
         vector.shrink_to_fit();
     }
 
+    size_t size() {
+        return vector.size() * sizeof(uint64_t);
+    }
+
+    /// Defines a callback method which is called when a value is updated
+    void setChangeCallback(void (*hdler)(int idx, bool value, void*ref), void*ref=nullptr){
+        changeHandler = hdler;
+        this->ref = ref;
+    }
+
   protected:
     Vector<uint64_t> vector;
+    void (*changeHandler)(int idx, bool value, void*ref) = nullptr;
+    void *ref;
 
 };
+
+}
