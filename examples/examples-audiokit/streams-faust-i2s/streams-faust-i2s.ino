53046438da5c652baae9319d9f3c3e420752a523
Phil Schatzmann <phil.schatzmann@gmail.com>
2022-04-22 14:12:49 +0200

    Faust Example cleanup

--- examples/examples-audiokit/streams-faust-i2s/streams-faust-i2s.ino
+++ examples/examples-audiokit/streams-faust-i2s/streams-faust-i2s.ino
@@ -1,7 +1,7 @@
 /**
  * @file streams-faust-i2s.ino
  * @author Phil Schatzmann
- * @brief Example how to use Faust
+ * @brief Example how to use Faust as Audio Source
  * @version 0.1
  * @date 2022-04-22
  * 
@@ -14,9 +14,9 @@
 #include "AudioLibs/AudioFaust.h"
 #include "Noise.h"
 
-AudioKitStream out; 
 mydsp dsp;
 FaustStream faust(dsp);
+AudioKitStream out; 
 StreamCopy copier(out, faust);  // copy mic to tfl
 
 // Arduino Setup
@@ -25,16 +25,16 @@ void setup(void) {
   Serial.begin(115200);
   AudioLogger::instance().begin(Serial, AudioLogger::Warning);
 
-  // Setup sine wave
+  // Setup Faust
   auto cfg = faust.defaultConfig();
   faust.begin(cfg);
 
   // start I2S
-  auto config = out.defaultConfig(TX_MODE);
-  config.sample_rate = cfg.sample_rate; 
-  config.channels = cfg.channels;
-  config.bits_per_sample = cfg.bits_per_sample;
-  out.begin(config);
+  auto cfg_i2s = out.defaultConfig(TX_MODE);
+  cfg_i2s.sample_rate = cfg.sample_rate; 
+  cfg_i2s.channels = cfg.channels;
+  cfg_i2s.bits_per_sample = cfg.bits_per_sample;
+  out.begin(cfg_i2s);
 
 }
 
