ab677691a1fd196e32cec48ce0386c6fd40214f3
Phil Schatzmann <phil.schatzmann@gmail.com>
2022-04-26 22:30:36 +0200

    ip example

--- examples/examples-communication/communication-ip-send/communication-ip-send.ino
+++ examples/examples-communication/communication-ip-send/communication-ip-send.ino
@@ -13,8 +13,8 @@
 #include "AudioTools.h"
 #include <WiFi.h>
 
-uint16_t sample_rate = 22050;
-uint8_t channels = 2;  // The stream will have 2 channels
+uint16_t sample_rate = 16000;
+uint8_t channels = 1;  // The stream will have 2 channels
 SineWaveGenerator<int16_t> sineWave( 32000);  // subclass of SoundGenerator with max amplitude of 32000
 GeneratedSoundStream<int16_t> sound( sineWave);  // Stream generated from sine wave
 WiFiClient client;                  
