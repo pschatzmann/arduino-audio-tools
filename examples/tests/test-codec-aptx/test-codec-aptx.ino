d01689de3328e5174f46b114943c534976c37745
Phil Schatzmann <phil.schatzmann@gmail.com>
2022-05-07 16:24:17 +0200

    APTX test case

--- examples/tests/test-codec-aptx/test-codec-aptx.ino
+++ examples/tests/test-codec-aptx/test-codec-aptx.ino
@@ -12,7 +12,7 @@
 #include "AudioCodecs/CodecAPTX.h"
 #include "AudioLibs/AudioKit.h"
 
-uint16_t sample_rate = 44100;
+uint16_t sample_rate = 24000;
 uint8_t channels = 2;  // The stream will have 2 channels
 SineWaveGenerator<int16_t> sineWave( 32000);  // subclass of SoundGenerator with max amplitude of 32000
 GeneratedSoundStream<int16_t> sound( sineWave); // Stream generated from sine wave
@@ -30,6 +30,9 @@ void setup() {
   // start I2S
   Serial.println("starting I2S...");
   auto config = out.defaultConfig(TX_MODE);
+  config.sample_rate = sample_rate;
+  config.channels = channels;
+  config.bits_per_sample = 16;
   out.begin(config);
 
   // Setup sine wave
