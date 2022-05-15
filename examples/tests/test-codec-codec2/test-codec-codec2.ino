675f40d93eed78e8b88de27b6b642d6d854ccd97
Phil Schatzmann <phil.schatzmann@gmail.com>
2022-05-15 18:28:46 +0200

    Codec2

--- examples/tests/test-codec-codec2/test-codec-codec2.ino
+++ examples/tests/test-codec-codec2/test-codec-codec2.ino
@@ -2,6 +2,9 @@
  * @file communication-codec-test.ino
  * @author Phil Schatzmann
  * @brief generate sine wave -> encoder -> decoder -> csv (Serial)
+ * Unfortunately codec2 needs more stack then we have available in Arduino. So we do the processing
+ * in a separate task with a stack of 20k
+ * 
  * @version 0.1
  * @date 2022-04-30
  * 
@@ -12,7 +15,7 @@
 #include "AudioCodecs/CodecCodec2.h"
 #include "AudioLibs/AudioKit.h"
 
-uint16_t sample_rate = 2400;
+uint16_t sample_rate = 8000;
 uint8_t channels = 1;  // The stream will have 2 channels
 SineWaveGenerator<int16_t> sineWave( 32000);  // subclass of SoundGenerator with max amplitude of 32000
 GeneratedSoundStream<int16_t> sound( sineWave); // Stream generated from sine wave
@@ -20,6 +23,19 @@ CsvStream<int16_t> out(Serial, channels);
 EncodedAudioStream decoder(&out, new Codec2Decoder()); // encode and write
 EncodedAudioStream encoder(&decoder, new Codec2Encoder()); // encode and write
 StreamCopy copier(encoder, sound);     
+
+
+void loop1(void*) {
+  // setup decoder
+  decoder.begin();
+  // setup encoder
+  encoder.begin();
+  // processing loop
+  while(true){
+    copier.copy();
+    delay(1);
+  }
+}
 
 void setup() {
   Serial.begin(115200);
@@ -32,16 +48,12 @@ void setup() {
   cfgs.bits_per_sample = 16;
   sineWave.begin(cfgs, N_B4);
 
-  // start decoder
-  decoder.begin();
-
-  // start encoder
-  encoder.begin(cfgs);
+  int stack = 20000;
+  xTaskCreate(loop1,"loopTask", stack, nullptr,1, nullptr);
 
   Serial.println("Test started...");
 }
 
 
-void loop() { 
-  copier.copy();
+void loop() {
 }
