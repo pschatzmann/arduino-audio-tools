2b35341528f7b8b858cf4cba7816c194d79bef33
Phil Schatzmann <phil.schatzmann@gmail.com>
2022-05-29 12:57:16 +0200

    float16

--- src/AudioBasic/Float16.h
+++ src/AudioBasic/Float16.h
@@ -5,7 +5,6 @@ namespace audio_tools {
 
 /**
  * @brief Stores float values with 2 bytes
- * see https://stackoverflow.com/questions/1659440/32-bit-to-16-bit-floating-point-conversion
  * @author Phil Schatzmann
  * @copyright GPLv3
  */
@@ -54,19 +53,23 @@ class float16 {
   protected:
     uint16_t value=0;
 
+    /// see https://stackoverflow.com/questions/1659440/32-bit-to-16-bit-floating-point-conversion
     static uint32_t as_uint(const float x) {
         return *(uint*)&x;
     }
+    /// see https://stackoverflow.com/questions/1659440/32-bit-to-16-bit-floating-point-conversion
     static float as_float(const uint32_t x) {
         return *(float*)&x;
     }
 
+    /// see https://stackoverflow.com/questions/1659440/32-bit-to-16-bit-floating-point-conversion
     static float half_to_float(const uint16_t x) { // IEEE-754 16-bit floating-point format (without infinity): 1-5-10, exp-15, +-131008.0, +-6.1035156E-5, +-5.9604645E-8, 3.311 digits
         const uint32_t e = (x&0x7C00)>>10; // exponent
         const uint32_t m = (x&0x03FF)<<13; // mantissa
         const uint32_t v = as_uint((float)m)>>23; // evil log2 bit hack to count leading zeros in denormalized format
         return as_float((x&0x8000)<<16 | (e!=0)*((e+112)<<23|m) | ((e==0)&(m!=0))*((v-37)<<23|((m<<(150-v))&0x007FE000))); // sign : normalized : denormalized
     }
+    /// see https://stackoverflow.com/questions/1659440/32-bit-to-16-bit-floating-point-conversion
     static uint16_t float_to_half(const float x) { // IEEE-754 16-bit floating-point format (without infinity): 1-5-10, exp-15, +-131008.0, +-6.1035156E-5, +-5.9604645E-8, 3.311 digits
         const uint32_t b = as_uint(x)+0x00001000; // round-to-nearest-even: add last bit after truncated mantissa
         const uint32_t e = (b&0x7F800000)>>23; // exponent
