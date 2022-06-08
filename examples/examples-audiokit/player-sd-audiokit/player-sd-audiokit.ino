bd843a2f9a4137f0c03c5ad20abbfa33092972e6
Phil Schatzmann <phil.schatzmann@gmail.com>
2022-06-08 21:58:16 +0200

    Audiokit SD support for multiple spi ports

--- examples/examples-audiokit/player-sd-audiokit/player-sd-audiokit.ino
+++ examples/examples-audiokit/player-sd-audiokit/player-sd-audiokit.ino
@@ -14,8 +14,8 @@
 
 const char *startFilePath="/";
 const char* ext="mp3";
-int speedMz = 10;
-AudioSourceSdFat source(startFilePath, ext, PIN_AUDIO_KIT_SD_CARD_CS, speedMz);
+SdSpiConfig sdcfg(PIN_AUDIO_KIT_SD_CARD_CS, DEDICATED_SPI, SD_SCK_MHZ(10) , &AUDIOKIT_SD_SPI);
+AudioSourceSdFat source(startFilePath, ext, sdcfg);
 AudioKitStream kit;
 MP3DecoderHelix decoder;  // or change to MP3DecoderMAD
 AudioPlayer player(source, kit, decoder);
