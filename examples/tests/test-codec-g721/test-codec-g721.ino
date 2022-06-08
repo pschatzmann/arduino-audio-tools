d650553af3698a6c956a13afc81659d5653012c6
Phil Schatzmann <phil.schatzmann@gmail.com>
2022-06-08 15:53:19 +0200

    CodecG7xx.h

---
+++ examples/tests/test-codec-g721/test-codec-g721.ino
@@ -0,0 +1,55 @@
+/**
+ * @file communication-codec-test.ino
+ * @author Phil Schatzmann
+ * @brief generate sine wave -> encoder -> decoder -> audiokit (i2s)
+ * @version 0.1
+ * @date 2022-04-30
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include "AudioTools.h"
+#include "AudioCodecs/CodecG7xx.h"
+#include "AudioLibs/AudioKit.h"
+
+uint16_t sample_rate = 8000;
+uint8_t channels = 1;  // The stream will have 2 channels
+SineWaveGenerator<int16_t> sineWave( 32000);  // subclass of SoundGenerator with max amplitude of 32000
+GeneratedSoundStream<int16_t> sound( sineWave); // Stream generated from sine wave
+AudioKitStream out; 
+EncodedAudioStream decoder(&out, new G721Decoder()); // encode and write
+EncodedAudioStream encoder(&decoder, new G721Encoder()); // encode and write
+StreamCopy copier(encoder, sound);     
+
+void setup() {
+  Serial.begin(115200);
+  AudioLogger::instance().begin(Serial, AudioLogger::Warning);
+
+  // start I2S
+  Serial.println("starting I2S...");
+  auto cfgi = out.defaultConfig(TX_MODE);
+  cfgi.sample_rate = sample_rate;
+  cfgi.channels = channels;
+  cfgi.bits_per_sample = 16;
+  out.begin(cfgi);
+
+  // Setup sine wave
+  auto cfgs = sineWave.defaultConfig();
+  cfgs.sample_rate = sample_rate;
+  cfgs.channels = channels;
+  cfgs.bits_per_sample = 16;
+  sineWave.begin(cfgs, N_B4);
+
+  // start decoder
+  decoder.begin();
+
+  // start encoder
+  encoder.begin(cfgs);
+
+  Serial.println("Test started...");
+}
+
+
+void loop() { 
+  copier.copy();
+}
