b68a13273f60d11ca25b0a01d54d2bfa666cb4b5
Phil Schatzmann <phil.schatzmann@gmail.com>
2022-06-27 15:46:59 +0200

    ESP-Now Example

--- examples/examples-communication/esp-now/communication-espnow-send/communication-espnow-send.ino
+++ examples/examples-communication/esp-now/communication-espnow-send/communication-espnow-send.ino
@@ -11,7 +11,7 @@
 #include "AudioTools.h"
 #include "AudioLibs/Communication.h"
 
-uint16_t sample_rate = 10000;
+uint16_t sample_rate = 8000;
 uint8_t channels = 1;  // The stream will have 2 channels
 SineWaveGenerator<int16_t> sineWave( 32000);  // subclass of SoundGenerator with max amplitude of 32000
 GeneratedSoundStream<int16_t> sound( sineWave); // Stream generated from sine wave
@@ -21,7 +21,7 @@ const char *peers[] = {"A8:48:FA:0B:93:01"};
 
 void setup() {
   Serial.begin(115200);
-  AudioLogger::instance().begin(Serial, AudioLogger::Info);
+  AudioLogger::instance().begin(Serial, AudioLogger::Warning);
 
   auto cfg = now.defaultConfig();
   cfg.mac_address = "A8:48:FA:0B:93:02";
