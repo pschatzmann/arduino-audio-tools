370c9935e842d898fc974e0ec0187213179f69ea
Phil Schatzmann <phil.schatzmann@gmail.com>
2022-07-28 22:00:15 +0200

    streams-audiokit-multioutput-server.ino

---
+++ examples/examples-audiokit/streams-audiokit-multioutput-server/streams-audiokit-multioutput-server.ino
@@ -0,0 +1,50 @@
+/**
+ * @file streams-audiokit-multioutput-server.ino
+ * @author Phil Schatzmann
+ * @brief MultiOutput Example using add - output to speaker and webserver
+ * @version 0.1
+ * @date 2022-07-26
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include "AudioTools.h"
+#include "AudioLibs/AudioKit.h"
+
+const int buffer_count = 10;
+const int buffer_size = 1024;
+AudioKitStream kit; // input & output
+CallbackBufferedStream<uint8_t> queue(buffer_size, buffer_count, true); 
+AudioEncoderServer server(new WAVEncoder(),"Phil Schatzmann","sabrina01");  
+MultiOutput out(queue, kit);
+StreamCopy copier(out, kit); // copy kit to kit
+
+// Arduino Setup
+void setup(void) {
+   Serial.begin(115200);
+   AudioLogger::instance().begin(Serial, AudioLogger::Info);
+
+   // setup audiokit
+   auto cfg = kit.defaultConfig(RXTX_MODE);
+   cfg.sd_active = false;
+   cfg.input_device = AUDIO_HAL_ADC_INPUT_LINE2; // input from microphone
+   cfg.sample_rate = 16000;
+   kit.setVolume(0.5);
+   kit.begin(cfg);
+
+   // start queue
+   queue.begin();
+
+   // start server
+   server.begin(queue, cfg);
+   Serial.println("started...");
+
+}
+
+// Arduino loop - copy data
+void loop() {
+   copier.copy();
+   server.copy();
+}
+
