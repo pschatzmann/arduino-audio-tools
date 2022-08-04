f290c184901780a78e10bb7d9284b9e4eba77ebb
Phil Schatzmann <phil.schatzmann@gmail.com>
2022-08-04 22:56:32 +0200

    MemoryManager for ESP32 psram

--- src/AudioLibs/MemoryManager.h
+++ src/AudioLibs/MemoryManager.h
@@ -10,16 +10,19 @@ namespace audio_tools {
 /**
  * @brief MemoryManager which activates the use of external SPIRAM memory.
  * When external memory is in use, the allocation strategy is to initially try
- to satisfy smaller allocation requests with internal memory and larger requests
- with external memory. This sets the limit between the two, as well as generally
- enabling allocation in external memory.
+ * to satisfy smaller allocation requests with internal memory and larger requests
+ * with external memory. This sets the limit between the two, as well as generally
+ * enabling allocation in external memory.
  */
 class MemoryManager {
 public:
+  /// Default Constructor - call begin() to activate PSRAM
   MemoryManager() = default;
+  /// Constructor which activates PSRAM. This constructor automatically calls begin()
   MemoryManager(int limit) {
     begin(limit);
   };
+  /// Activate the PSRAM for allocated memory > limit 
   bool begin(int limit = 10000) {
 #ifdef ESP32
     LOGI("Activate PSRAM from %d", limit);
