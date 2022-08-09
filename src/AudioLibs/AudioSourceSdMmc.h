e39424879cbc79f51b94f99c0e6d5817672c0c93
Phil Schatzmann <phil.schatzmann@gmail.com>
2022-08-09 17:50:52 +0200

    AudioSourceSd &  AudioSourceSdMmc: optimize redundant code

--- src/AudioLibs/AudioSourceSdMmc.h
+++ src/AudioLibs/AudioSourceSdMmc.h
@@ -3,6 +3,7 @@
 #include "AudioBasic/StrExt.h"
 #include "AudioLogger.h"
 #include "AudioTools/AudioSource.h"
+#include "AudioLibs/AudioSourceIndex.h"
 #include "FS.h"
 #include "SD_MMC.h"
 
@@ -30,134 +31,6 @@ namespace audio_tools {
  * @copyright GPLv3
  */
 class AudioSourceSdMmc : public AudioSource {
-protected:
-  /**
-   * @brief We store all the relevant file names in an sequential index
-   * file
-   */
-  class MMCFileIndex {
-  public:
-    MMCFileIndex() = default;
-    void begin(bool setupIndex, const char *startDir, const char *extension,
-               const char *file_name_pattern) {
-      this->ext = extension;
-      this->file_name_pattern = file_name_pattern;
-      idx_path = String(startDir)+"/idx.txt";
-      idx_defpath = String(startDir)+"/idx-def.txt";
-      int idx_file_size = indexFileSize();
-      LOGI("Index file size: %d", idx_file_size);
-      String keyNew = String(startDir) + "|" + extension + "|" + file_name_pattern;
-      String keyOld = getIndexDef();
-      if (setupIndex && (keyNew != keyOld || idx_file_size==0)) {
-        File idxfile = SD_MMC.open(idx_path.c_str(), FILE_WRITE);
-        LOGW("Creating index file");
-        listDir(idxfile, startDir);
-        LOGI("Indexing completed");
-        idxfile.close();
-        // update index definition file
-        saveIndexDef(keyNew);
-      } 
-    }
-
-    /// Access file name by index
-    const char *operator[](int idx) {
-      // return null when inx too big
-      if (size>=0 && idx>=size) {
-        LOGE("idx %d > size %d", idx, size);
-        return nullptr;
-      }
-      // find record
-      File idxfile = SD_MMC.open(idx_path.c_str());
-      int count=0;
-
-      if (idxfile.available()==0){
-        LOGE("Index file is empty");
-      }
-
-      bool found = false;
-      while (idxfile.available()>0 && !found) {
-        result = idxfile.readStringUntil('\n');
-        LOGD("%d -> %s", count, result.c_str());
-        if (count==idx) {
-          found=true;
-        }
-        count++;
-      }
-      if (!found){
-        size = count;
-      }
-      idxfile.close();
-
-      return found ? result.c_str(): nullptr;
-    }
-
-  protected:
-    String result;
-    String idx_path;
-    String idx_defpath;
-    
-    const char *ext = nullptr;
-    const char *file_name_pattern = nullptr;
-    long size=-1;
-
-    /// Writes the index file
-    void listDir(File &idxfile, const char *dirname) {
-      File root = SD_MMC.open(dirname);
-      if (!root) {
-        return;
-      }
-      if (!root.isDirectory()) {
-        return;
-      }
-
-      File file = root.openNextFile();
-      while (file) {
-        if (file.isDirectory()) {
-          listDir(idxfile, file.name());
-        } else {
-          if (isValidAudioFile(file)) {
-            LOGI("Adding file to index: %s", file.name());
-            idxfile.println(file.name());
-          }
-        }
-        file = root.openNextFile();
-      }
-    }
-
-    /// checks if the file is a valid audio file
-    bool isValidAudioFile(File &file) {
-      const char *file_name = file.name();
-      if (file.isDirectory()) {
-        LOGD("-> isValidAudioFile: '%s': %d", file_name, false);
-        return false;
-      }
-      Str strFileName(file_name);
-      bool result = strFileName.endsWithIgnoreCase(ext) &&
-                    strFileName.matches(file_name_pattern);
-      LOGD("-> isValidAudioFile: '%s': %d", file_name, result);
-      return result;
-    }
-
-    String getIndexDef() {
-      File idxdef = SD_MMC.open(idx_defpath.c_str());
-      String key1 = idxdef.readString();
-      idxdef.close();
-      return key1;
-    }
-    void saveIndexDef(String keyNew){
-        File idxdef = SD_MMC.open(idx_defpath.c_str(), FILE_WRITE);
-        idxdef.write((const uint8_t *)keyNew.c_str(), keyNew.length());
-        idxdef.close();
-    }
-
-    size_t indexFileSize() {
-        File idxfile = SD_MMC.open(idx_path.c_str());
-        size_t result = idxfile.size();
-        idxfile.close();
-        return result;
-    }
-  };
-
 public:
   /// Default constructor
   AudioSourceSdMmc(const char *startFilePath = "/", const char *ext = ".mp3", bool setupIndex=true) {
@@ -220,8 +93,8 @@ public:
   virtual void setPath(const char *p) { start_path = p; }
 
 protected:
+  AudioSourceIndex<fs::SDMMCFS,fs::File> idx{SD_MMC};
   File file;
-  MMCFileIndex idx;
   size_t idx_pos = 0;
   const char *file_name;
   const char *exension = nullptr;
