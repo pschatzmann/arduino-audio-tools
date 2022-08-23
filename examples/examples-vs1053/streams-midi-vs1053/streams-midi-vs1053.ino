0bb3e4170c5daec30928e02d30d2704d876c56db
Phil Schatzmann <phil.schatzmann@gmail.com>
2022-08-23 15:43:31 +0200

    vs1053 midi

--- examples/examples-vs1053/streams-midi-vs1053/streams-midi-vs1053.ino
+++ examples/examples-vs1053/streams-midi-vs1053/streams-midi-vs1053.ino
@@ -13,7 +13,6 @@
 
 
 VS1053Stream vs1053; // final output
-MidiStreamOut out(vs1053);
 uint16_t note = 65; // 0 to 128
 uint16_t amplitude = 128; // 0 to 128
 int channel = 0;
@@ -50,9 +49,9 @@ void loop() {
     Serial.print("playing ");
     Serial.println(++note);
 
-    out.noteOn(channel, note, amplitude );
+    noteOn(channel, note, amplitude );
     delay(900);
-    out.noteOff(channel, note, 20 );
+    noteOff(channel, note, 20 );
     delay(200);
     if (note>=90) {
       note = 64;
