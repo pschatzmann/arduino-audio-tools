e4f44efe4cb17df0de3627dd3dd8e71c2e64ed4c
Phil Schatzmann <phil.schatzmann@gmail.com>
2022-08-25 13:17:01 +0200

    vs1053 Example for midi library

--- examples/examples-vs1053/streams-midi_lib-vs1053/streams-midi_lib-vs1053.ino
+++ examples/examples-vs1053/streams-midi_lib-vs1053/streams-midi_lib-vs1053.ino
@@ -2,39 +2,33 @@
  * @file streams-midi-vs1053.ino
  * @author Phil Schatzmann
  * @brief Demo that shows how to send streaming midi commands to the vs1053
+ * This example using my https://github.com/pschatzmann/arduino-midi.git library 
  * @version 0.1
  * @date 2022-08-23
  * 
  * @copyright Copyright (c) 2022
  * 
  */
+
 #include "AudioTools.h"
 #include "AudioLibs/VS1053Stream.h"
+#include "Midi.h"
 
 
 VS1053Stream vs1053; // final output
-uint16_t note = 65; // 0 to 128
-uint16_t amplitude = 128; // 0 to 128
-int channel = 0;
+MidiStreamOut midi_out(vs1053);
+uint8_t note = 65; // 0 to 128
+uint8_t amplitude = 128; // 0 to 128
 uint8_t instrument = 0;
 
-//Send a MIDI note-on message.  Like pressing a piano key
-void noteOn(uint8_t channel, uint8_t note, uint8_t attack_velocity=100) {
-  vs1053.sendMidiMessage( (0x90 | channel), note, attack_velocity);
-}
-
-//Send a MIDI note-off message.  Like releasing a piano key
-void noteOff(uint8_t channel, uint8_t note, uint8_t release_velocity=100) {
-  vs1053.sendMidiMessage( (0x80 | channel), note, release_velocity);
-}
-
-void selectInstrument(uint8_t channel, uint8_t instrument ){
+void selectInstrument(uint8_t instrument, uint8_t bank=0x00){
  // Continuous controller 0, bank select: 0 gives you the default bank depending on the channel
  // 0x78 (percussion) for Channel 10, i.e. channel = 9 , 0x79 (melodic)  for other channels
-  vs1053.sendMidiMessage(0xB0| channel, 0, 0x00); //0x00 default bank 
+  //vs1053.sendMidiMessage(0xB0| channel, 0, 0x00); //0x00 default bank 
+  midi_out.controlChange(BANK_SELECT, bank);
 
   // Patch change, select instrument
-  vs1053.sendMidiMessage(0xC0| channel, instrument, 0);
+  midi_out.programChange(instrument);
 }
 
 
@@ -60,15 +54,15 @@ void loop() {
     Serial.print("playing ");
     Serial.println(++note);
 
-    noteOn(channel, note, amplitude );
+    midi_out.noteOn(note, amplitude );
     delay(900);
-    noteOff(channel, note, 20 );
+    midi_out.noteOff(note, 20 );
     delay(200);
     if (note>=90) {
       note = 64;
       Serial.println();
       Serial.print("selecting Instrument ");
       Serial.println(++instrument);
-      selectInstrument(channel, instrument);
+      selectInstrument(instrument);
     }
 }
