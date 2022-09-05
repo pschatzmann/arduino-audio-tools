fa7917286dcd5c16b601f6bc1c5b95fee3f8ebd8
Phil Schatzmann <phil.schatzmann@gmail.com>
2022-09-05 21:36:17 +0200

    AudioServerEx examples DRAFT

---
+++ examples/examples-webserver/streams-generator-webserverex_wav/streams-generator-webserverex_wav.ino
@@ -0,0 +1,47 @@
+/**
+ * @file streams-generator-server_wav.ino
+ *
+ *  This sketch generates a test sine wave. The result is provided as WAV stream which can be listened to in a Web Browser
+ *
+ * @author Phil Schatzmann
+ * @copyright GPLv3
+ * 
+ */
+
+#include "AudioTools.h"
+#include "AudioLibs/AudioServerEx.h"
+
+// WIFI
+const char *ssid = "ssid";
+const char *password = "password";
+AudioWAVServerEx server;
+// Sound Generation
+const int sample_rate = 10000;
+const int channels = 1;
+SineWaveGenerator<int16_t> sineWave;            // Subclass of SoundGenerator with max amplitude of 32000
+GeneratedSoundStream<int16_t> in(sineWave);     // Stream generated from sine wave
+
+
+void setup() {
+  Serial.begin(115200);
+  AudioLogger::instance().begin(Serial,AudioLogger::Info);
+
+  // start server
+  auto cfg = server.defaultConfig();
+  cfg.sample_rate = sample_rate;
+  cfg.channels = channels;
+  cfg.ssid = ssid;
+  cfg.password = password; 
+  cfg.input = &in;
+  server.begin(cfg);
+
+  // start generation of sound
+  sineWave.begin(channels, sample_rate, N_B4);
+  in.begin();
+}
+
+
+// copy the data
+void loop() {
+  server.copy();
+}
