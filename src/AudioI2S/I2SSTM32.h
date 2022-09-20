40132069bc379cdd3c42f82086eaff3475de631b
Phil Schatzmann <phil.schatzmann@gmail.com>
2022-09-20 16:47:44 +0200

    stm32 parameters

--- src/AudioI2S/I2SSTM32.h
+++ src/AudioI2S/I2SSTM32.h
@@ -9,7 +9,7 @@ namespace audio_tools {
 /**
  * @brief Basic I2S API - for the STM32
  * Depends on https://github.com/pschatzmann/STM32F411-i2s-prototype
- * We just add a write and read buffer!
+ * We just add a write and read buffer and pass some parameters to the STM32 API!
  * @author Phil Schatzmann
  * @copyright GPLv3
  */
@@ -38,14 +38,12 @@ class I2SBase {
         LOGE("Bits per second not supported: %d", cfg.bits_per_sample);
         return false;
       }
-      if (cfg.sample_rate!=44100){
-        LOGE("Sample rate not supported: %d", cfg.sample_rate);
-        return false;
-      }
       if (cfg.channels!=2){
         LOGE("Channels not supported: %d", cfg.channels);
         return false;
       }
+      
+      setupDefaultI2SParameters();
 
       switch(cfg.rx_tx_mode){
         case RX_MODE:
@@ -123,6 +121,74 @@ class I2SBase {
         p_tx_buffer = nullptr;
       }
     }
+
+    void setupDefaultI2SParameters() {
+      i2s_default_samplerate = getSampleRate(cfg);
+      i2s_default_mode = getMode(cfg);
+      i2s_default_standard = getStandard(cfg);
+      i2s_default_fullduplexmode = cfg.rx_tx_mode == RXTX_MODE ? I2S_FULLDUPLEXMODE_ENABLE : I2S_FULLDUPLEXMODE_DISABLE;
+    }
+
+    uint32_t getMode(I2SConfig &cfg){
+      if (cfg.is_master) {
+        switch(cfg.rx_tx_mode){
+          case RX_MODE:
+            return I2S_MODE_MASTER_RX;
+          case TX_MODE:
+            return I2S_MODE_MASTER_TX;
+          default:
+            LOGE("RXTX_MODE not supported");
+            return I2S_MODE_MASTER_TX;
+        }
+      } else {
+        switch(cfg.rx_tx_mode){
+          case RX_MODE:
+            return I2S_MODE_SLAVE_RX;
+          case TX_MODE:
+            return I2S_MODE_SLAVE_TX;
+          default:
+            LOGE("RXTX_MODE not supported");
+            return I2S_MODE_SLAVE_TX;
+        }
+      }
+    }
+
+    
+    uint32_t getStandard(I2SConfig &cfg){
+      uint32_t result;
+      switch(cfg.i2s_format) {
+          case I2S_PHILIPS_FORMAT:
+          return I2S_STANDARD_PHILIPS;
+        case I2S_STD_FORMAT:
+        case I2S_LSB_FORMAT:
+        case I2S_RIGHT_JUSTIFIED_FORMAT:
+          return I2S_STANDARD_MSB;
+        case I2S_MSB_FORMAT:
+        case I2S_LEFT_JUSTIFIED_FORMAT:
+          return I2S_STANDARD_LSB;
+      }
+      return I2S_STANDARD_PHILIPS;
+    }
+
+    uint32_t getSampleRate(I2SConfig &cfg){
+      switch(cfg.sample_rate){
+        case I2S_AUDIOFREQ_192K:              
+        case I2S_AUDIOFREQ_96K:             
+        case I2S_AUDIOFREQ_48K:               
+        case I2S_AUDIOFREQ_44K:               
+        case I2S_AUDIOFREQ_32K:               
+        case I2S_AUDIOFREQ_22K:               
+        case I2S_AUDIOFREQ_16K:               
+        case I2S_AUDIOFREQ_11K:               
+        case I2S_AUDIOFREQ_8K:
+          return cfg.sample_rate;
+        default:
+          LOGE("Unsupported sample rate: %u", cfg.sample_rate);
+          return cfg.sample_rate;
+      }
+    }
+
+
 };
 
 }
