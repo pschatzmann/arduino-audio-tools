2fb7364900b1f4644d49964fa70c9d809688968b
Phil Schatzmann <phil.schatzmann@gmail.com>
2022-09-20 15:44:47 +0200

    STM32 I2S

--- src/AudioI2S/I2SSTM32.h
+++ src/AudioI2S/I2SSTM32.h
@@ -4,12 +4,16 @@
 #include "AudioI2S/I2SConfig.h"
 
 namespace audio_tools {
+#include "stm32-i2s.h"
 
 /**
  * @brief Basic I2S API - for the STM32
+ * Depends on https://github.com/pschatzmann/STM32F411-i2s-prototype
+ * We just add a write and read buffer!
  * @author Phil Schatzmann
  * @copyright GPLv3
  */
+
 class I2SBase {
   friend class I2SStream;
 
@@ -28,33 +32,47 @@ class I2SBase {
 
     /// starts the DAC 
     bool begin(I2SConfig cfg) {
-      bool result = true;
-      this->cfg = cfg;
-      i2s.Instance = SPI2;
-      if (cfg.channels=!2){
-        LOGE("Unsupported channels %d - must be 2", cfg.channels);
+      deleteBuffers();
+
+      if (cfg.bits_per_sample!=16){
+        LOGE("Bits per second not supported: %d", cfg.bits_per_sample);
+        return false;
+      }
+      if (cfg.sample_rate!=44100){
+        LOGE("Sample rate not supported: %d", cfg.sample_rate);
+        return false;
+      }
+      if (cfg.channels!=2){
+        LOGE("Channels not supported: %d", cfg.channels);
+        return false;
       }
 
-      i2s.Init.Mode = getMode(cfg);
-      i2s.Init.Standard = getStandard(cfg);
-      i2s.Init.DataFormat = getDataFormat(cfg);
-      i2s.Init.MCLKOutput = I2S_MCLKOUTPUT_ENABLE;
-      i2s.Init.AudioFreq = cfg.sample_rate;
-      i2s.Init.CPOL = I2S_CPOL_LOW;
-      i2s.Init.ClockSource = I2S_CLOCK_PLL;
-      i2s.Init.FullDuplexMode = cfg.rx_tx_mode==RXTX_MODE? I2S_FULLDUPLEXMODE_ENABLE: I2S_FULLDUPLEXMODE_DISABLE;
-      if (HAL_I2S_Init(&i2s) != HAL_OK){
-        LOGE("HAL_I2S_Init failed");
-        result = false;
+      switch(cfg.rx_tx_mode){
+        case RX_MODE:
+          p_rx_buffer = new NBuffer<uint8_t>(cfg.buffer_size, cfg.buffer_count);
+        	startI2SReceive(&hi2s3, writeFromReceive, cfg.buffer_size);
+          break;
+        case TX_MODE:
+          p_tx_buffer = new NBuffer<uint8_t>(cfg.buffer_size, cfg.buffer_count);
+      	  startI2STransmit(&hi2s3, readToTransmit, cfg.buffer_size);
+          break;
+        case RXTX_MODE:
+          p_tx_buffer = new NBuffer<uint8_t>(cfg.buffer_size, cfg.buffer_count);
+	        startI2STransmitReceive(&hi2s3, readToTransmit, writeFromReceive, cfg.buffer_size);
+          break;
+        default:
+          LOGE("Unsupported mode");
+          return false;
+
       }
-      return result;
+
+      return true;
     }
 
     /// stops the I2C and unistalls the driver
     void end(){
-      if (HAL_I2S_DeInit(&i2s) != HAL_OK){
-        LOGE("HAL_I2S_DeInit failed");
-      }
+      stopI2S();
+      deleteBuffers();
     }
 
     /// we assume the data is already available in the buffer
@@ -74,87 +92,37 @@ class I2SBase {
 
     /// writes the data to the I2S interface
     size_t writeBytes(const void *src, size_t size_bytes){
-      size_t result = 0;
-      HAL_StatusTypeDef res = HAL_I2S_Transmit(&i2s, (uint16_t*)src, size_bytes/2, HAL_MAX_DELAY);
-      if(res == HAL_OK) {
-        result = size_bytes;
-      } else {
-        LOGE("HAL_I2S_Transmit failed");
-      }
-            
-      return result;
+      return p_tx_buffer->writeArray((uint8_t*)src, size_bytes);
     }
 
     size_t readBytes(void *dest, size_t size_bytes){
-      size_t result = 0;
-      HAL_StatusTypeDef res = HAL_I2S_Receive(&i2s, (uint16_t*)dest, size_bytes, HAL_MAX_DELAY);
-      if(res == HAL_OK) {
-        result = size_bytes;
-      } else {
-        LOGE("HAL_I2S_Receive failed");
-      }
+      return p_rx_buffer->readArray((uint8_t*)dest, size_bytes);
+    }
 
-      return result;
+    static void writeFromReceive(uint8_t *buffer, uint16_t byteCount){
+      p_rx_buffer->writeArray(buffer, byteCount);
+    }
+
+    static void readToTransmit(uint8_t *buffer, uint16_t byteCount) {
+      memset(buffer,0,byteCount);
+      p_tx_buffer->readArray(buffer, byteCount);
     }
 
   protected:
     I2SConfig cfg;
-    I2S_HandleTypeDef i2s;
+    inline static NBuffer<uint8_t> *p_tx_buffer=nullptr;
+    inline static NBuffer<uint8_t> *p_rx_buffer=nullptr;
 
-    uint32_t getMode(I2SConfig &cfg){
-      if (cfg.is_master) {
-        switch(cfg.rx_tx_mode){
-          case RX_MODE:
-            return I2S_MODE_MASTER_RX;
-          case TX_MODE:
-            return I2S_MODE_MASTER_TX;
-          default:
-            LOGE("RXTX_MODE not supported");
-            return I2S_MODE_MASTER_TX;
-        }
-      } else {
-        switch(cfg.rx_tx_mode){
-          case RX_MODE:
-            return I2S_MODE_SLAVE_RX;
-          case TX_MODE:
-            return I2S_MODE_SLAVE_TX;
-          default:
-            LOGE("RXTX_MODE not supported");
-            return I2S_MODE_SLAVE_TX;
-        }
+    void deleteBuffers() {
+      if (p_rx_buffer!=nullptr) {
+        delete p_rx_buffer;
+        p_rx_buffer = nullptr;
+      }
+      if (p_tx_buffer!=nullptr) {
+        delete p_tx_buffer;
+        p_tx_buffer = nullptr;
       }
     }
-
-    
-    uint32_t getStandard(I2SConfig &cfg){
-      uint32_t result;
-      switch(cfg.i2s_format) {
-          case I2S_PHILIPS_FORMAT:
-          return I2S_STANDARD_PHILIPS;
-        case I2S_STD_FORMAT:
-        case I2S_LSB_FORMAT:
-        case I2S_RIGHT_JUSTIFIED_FORMAT:
-          return I2S_STANDARD_MSB;
-        case I2S_MSB_FORMAT:
-        case I2S_LEFT_JUSTIFIED_FORMAT:
-          return I2S_STANDARD_LSB;
-      }
-      return I2S_STANDARD_PHILIPS;
-    }
-
-    uint32_t getDataFormat(I2SConfig &cfg) {
-        switch(cfg.bits_per_sample){
-        case 16:
-          return I2S_DATAFORMAT_16B;
-        case 24:
-          return I2S_DATAFORMAT_24B;
-        case 32:
-          return I2S_DATAFORMAT_32B;
-      }
-      return I2S_DATAFORMAT_16B;
-
-    }
-
 };
 
 }
