f34742e98fe113b8e0a7d7e17ed677c60f69ee18
Phil Schatzmann <phil.schatzmann@gmail.com>
2022-09-27 09:57:52 +0200

    DRAFT cmsis fft & tests

---
+++ examples/tests/fft/fft-cmsis/fft-cmsis.ino
@@ -0,0 +1,56 @@
+#include "AudioTools.h"
+#include "AudioLibs/AudioCmsisFFT.h" // or AudioKissFFT
+
+AudioCmsisFFT fft; // or AudioKissFFT
+SineFromTable<int16_t> sineWave(32000);
+GeneratedSoundStream<int16_t> in(sineWave);
+StreamCopy copier(fft, in);
+uint16_t sample_rate = 44100;
+int bits_per_sample = 16;
+int channels = 1;
+float value = 0;
+uint64_t timestamp;
+
+// display fft result
+void fftResult(AudioFFTBase &fft) {
+  int diff;
+  auto result = fft.result();
+  if (result.magnitude > 100) {
+    Serial.print(result.frequency);
+    Serial.print(" ");
+    Serial.print(result.magnitude);
+    Serial.print(" => ");
+    Serial.print(result.frequencyAsNote(diff));
+    Serial.print(" diff: ");
+    Serial.print(diff);
+    Serial.print(" - time ms ");
+    Serial.print(millis()-timestamp);
+    Serial.println();
+  }
+  timestamp = millis();
+}
+
+void setup() {
+  Serial.begin(115200);
+  AudioLogger::instance().begin(Serial, AudioLogger::Warning);
+
+  // set the frequency
+  sineWave.setFrequency(N_B4);
+
+  // Setup sine wave
+  auto cfg = in.defaultConfig();
+  cfg.channels = channels;
+  cfg.sample_rate = sample_rate;
+  in.begin(cfg);
+
+  // Setup FFT
+  auto tcfg = fft.defaultConfig();
+  tcfg.copyFrom(cfg);
+  tcfg.length = 8192;
+  tcfg.callback = &fftResult;
+  fft.begin(tcfg);
+
+  timestamp = millis();
+}
+
+void loop() { copier.copy(); }
