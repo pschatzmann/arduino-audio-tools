775bbf29051b2eb61c40b73200cd2aa6a06261a7
Phil Schatzmann <phil.schatzmann@gmail.com>
2022-10-09 11:02:28 +0200

    audiokit sd examle

---
+++ examples/examples-audiokit/streams-sd-audiolit/streams-sd-audiokit.ino
@@ -0,0 +1,50 @@
+/**
+ * @file streams-sd-audiokit.ino
+ * @author Phil Schatzmann
+ * @brief Just a small demo, how to use files with the SD library
+ * @version 0.1
+ * @date 2022-10-09
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <SPI.h>
+#include <SD.h>
+#include "AudioTools.h"
+#include "AudioLibs/AudioKit.h"
+#include "AudioCodecs/CodecMP3Helix.h"
+
+
+const int chipSelect=PIN_AUDIO_KIT_SD_CARD_CS;
+AudioKitStream i2s; // final output of decoded stream
+EncodedAudioStream decoder(&i2s, new MP3DecoderHelix()); // Decoding stream
+StreamCopy copier; 
+File audioFile;
+
+void setup(){
+  Serial.begin(115200);
+  AudioLogger::instance().begin(Serial, AudioLogger::Info);  
+
+  // setup audiokit before SD!
+  auto config = i2s.defaultConfig(TX_MODE);
+  config.sd_active = true;
+  i2s.begin(config);
+
+  // setup file
+  SD.begin(chipSelect);
+  audioFile = SD.open("/ZZ Top/Unknown Album/Lowrider.mp3");
+
+  // setup I2S based on sampling rate provided by decoder
+  decoder.begin();
+
+  // begin copy
+  copier.setCheckAvailableForWrite(false);
+  copier.begin(decoder, audioFile);
+
+}
+
+void loop(){
+  if (!copier.copy()) {
+    stop();
+  }
+}
