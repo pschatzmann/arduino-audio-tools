f51a0eec8f146361b8803bf3b7618719dc9a1877
Phil Schatzmann <phil.schatzmann@gmail.com>
2022-11-15 16:16:48 +0100

    espeak-ng examples

---
+++ examples/examples-tts/streams-espeak-i2s/streams-espeak-i2s.ino
@@ -0,0 +1,40 @@
+/**
+ * @file espeak-min.ino
+ * @author Phil Schatzmann
+ * @brief Arduino C++ API - minimum example. The espeak-ng-data is stored on in
+ * progmem with the arduino-posix-fs library and we output audio to I2S with the
+ * help of the AudioTools library
+ * @version 0.1
+ * @date 2022-10-27
+ *
+ * @copyright Copyright (c) 2022
+ */
+
+#include "AudioTools.h" // https://github.com/pschatzmann/arduino-audio-tools
+#include "FileSystems.h" // https://github.com/pschatzmann/arduino-posix-fs
+#include "espeak.h"
+
+I2SStream i2s; // or replace with any other audio sink
+ESpeakPROGMEM espeak(i2s);
+
+void setup() {
+  Serial.begin(115200);
+  //file_systems::FSLogger.begin(file_systems::FSInfo, Serial); 
+
+  // setup espeak
+  espeak.begin();
+
+  // setup output
+  audio_info espeak_info = espeak.audioInfo();
+  auto cfg = i2s.defaultConfig();
+  cfg.channels = espeak_info.channels; // 1
+  cfg.sample_rate = espeak_info.sample_rate; // 22050
+  cfg.bits_per_sample = espeak_info.bits_per_sample; // 16
+  i2s.begin(cfg);
+
+}
+
+void loop() {
+  espeak.say("Hello world!");
+  delay(5000);
+}
