b5cc8deb61b719a01ce2f3e3ea3134b137bffe49
Phil Schatzmann <phil.schatzmann@gmail.com>
2022-11-24 09:19:51 +0100

    SPIFFS support for AudioPlayer

---
+++ examples/examples-player/player-spiffs-i2s/player-spiffs-i2s.ino
@@ -0,0 +1,43 @@
+/**
+ * @file player-spiffs-i2s.ino
+ * @brief example using the SPIFFS library
+ * 
+ * @author Phil Schatzmann
+ * @copyright GPLv3
+ */
+
+#include "AudioTools.h"
+#include "AudioLibs/AudioSourceSPIFFS.h"
+#include "AudioCodecs/CodecMP3Helix.h"
+
+const char *startFilePath="/";
+const char* ext="mp3";
+AudioSourceSPIFFS source(startFilePath, ext);
+I2SStream i2s;
+MP3DecoderHelix decoder;
+AudioPlayer player(source, i2s, decoder);
+
+void printMetaData(MetaDataType type, const char* str, int len){
+  Serial.print("==> ");
+  Serial.print(toStr(type));
+  Serial.print(": ");
+  Serial.println(str);
+}
+
+void setup() {
+  Serial.begin(115200);
+  AudioLogger::instance().begin(Serial, AudioLogger::Info);
+
+  // setup output
+  auto cfg = i2s.defaultConfig(TX_MODE);
+  i2s.begin(cfg);
+
+  // setup player
+  //source.setFileFilter("*Bob Dylan*");
+  player.setMetadataCallback(printMetaData);
+  player.begin();
+}
+
+void loop() {
+  player.copy();
+}
