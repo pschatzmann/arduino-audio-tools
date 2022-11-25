d5f9dbf514a7b46b98bfd2df53776c2194802a76
Phil Schatzmann <phil.schatzmann@gmail.com>
2022-11-25 10:25:14 +0100

    Concurrency Tests

--- examples/tests/concurrency/synchBufferRTOS/synchBufferRTOS.ino
+++ examples/tests/concurrency/synchBufferRTOS/synchBufferRTOS.ino
@@ -1,3 +1,13 @@
+/**
+ * @file synchBufferRTOS.ino
+ * @author Phil Schatzmann
+ * @brief Multitask with SynchronizedBufferRTOS
+ * @version 0.1
+ * @date 2022-11-25
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #include "AudioTools.h"
 #include "AudioLibs/SynchronizedBuffer.h"
 #include "freertos-all.h"
@@ -13,21 +23,44 @@ Task writeTask("write",3000,10, [](){
 });
 
 Task readTask("read",3000,10, [](){ 
-    int16_t data[512];
-    uint64_t start = millis();
-    buffer.readArray(data, 512);
-    for (int j=0;j<512;j++){
-        assert(data[j]==j);
-    }
-    Serial.print("runtime: ");
-    Serial.println(millis()-start);
-});
+    static uint64_t start = millis();
+    static size_t total_bytes=0;
+    static size_t errors=0;
+    static int16_t data[512];
 
+    // read data
+    buffer.readArray(data, 512);
+
+    // check data
+    for (int j=0;j<512;j++){
+        if (data[j]!=j)
+            errors++;
+    }
+    // calculate bytes per second
+    total_bytes+=sizeof(int16_t)*512;
+    if (total_bytes>=1024000){
+        uint64_t duration = millis()-start;
+        float mbps = static_cast<float>(total_bytes) / duration / 1000.0;
+
+        // print result
+        Serial.print("Mbytes per second: ");
+        Serial.print(mbps);
+        Serial.print(" with ");
+        Serial.print(errors);
+        Serial.println(" errors");
+
+        start = millis();
+        errors = 0;
+        total_bytes = 0;
+    }
+});
 
 void setup(){
     Serial.begin(115200);
+    AudioLogger::instance().begin(Serial, AudioLogger::Info);
+
     writeTask.Start(0);
-    writeTask.Start(1);
+    readTask.Start(1);
 }
 
 void loop(){
--- examples/tests/concurrency/synchRingBuffer/synchRingBuffer.ino
+++ examples/tests/concurrency/synchRingBuffer/synchRingBuffer.ino
@@ -1,10 +1,20 @@
+/**
+ * @file synchBufferRTOS.ino
+ * @author Phil Schatzmann
+ * @brief Multitask with SynchronizedBuffer using RingBuffer
+ * @version 0.1
+ * @date 2022-11-25
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #include "AudioTools.h"
 #include "AudioLibs/SynchronizedBuffer.h"
 #include "freertos-all.h"
 
 audio_tools::Mutex mutex;
-RingBuffer<int16_t> rbuffer(512*4);
-SynchronizedBuffer<int16_t> buffer(rbuffer, mutex);
+RingBuffer<int16_t> nbuffer(512*4);
+SynchronizedBuffer<int16_t> buffer(nbuffer, mutex);
 
 Task writeTask("write",3000,10, [](){ 
     int16_t data[512];
@@ -14,22 +24,45 @@ Task writeTask("write",3000,10, [](){
     buffer.writeArray(data, 512);
 });
 
+
 Task readTask("read",3000,10, [](){ 
-    int16_t data[512];
-    uint64_t start = micros();
+    static uint64_t start = millis();
+    static size_t total_bytes=0;
+    static size_t errors=0;
+    static int16_t data[512];
+
+    // read data
     buffer.readArray(data, 512);
+
+    // check data
     for (int j=0;j<512;j++){
-        assert(data[j]==j);
+        if (data[j]!=j)
+            errors++;
     }
-    Serial.print("runtime: ");
-    Serial.println(micros()-start);
+    // calculate bytes per second
+    total_bytes+=sizeof(int16_t)*512;
+    if (total_bytes>=1024000){
+        uint64_t duration = millis()-start;
+        float mbps = static_cast<float>(total_bytes) / duration / 1000.0;
+
+        // print result
+        Serial.print("Mbytes per second: ");
+        Serial.print(mbps);
+        Serial.print(" with ");
+        Serial.print(errors);
+        Serial.println(" errors");
+
+        start = millis();
+        errors = 0;
+        total_bytes = 0;
+    }
 });
 
 
 void setup(){
     Serial.begin(115200);
     writeTask.Start(0);
-    writeTask.Start(1);
+    readTask.Start(1);
 }
 
 void loop(){
