f11c5c5a0dca6a5c58544e4e1bc6c6e8335ea964
Phil Schatzmann <phil.schatzmann@gmail.com>
2022-11-25 08:46:22 +0100

    task synchronization

---
+++ examples/examples-basic-api/base-SynchronizedBufferRTOS/base-SynchronizedBufferRTOS.ino
@@ -0,0 +1,56 @@
+/**
+ * @file base-SynchronizedBufferRTOS.ino
+ * @author Phil Schatzmann
+ * @brief Data provider on core 0 with data consumer on core 1
+ * @version 0.1
+ * @date 2022-11-25
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include "AudioTools.h"
+#include "AudioLibs/SynchronizedBuffer.h"
+#include "freertos-all.h"
+
+SynchronizedBufferRTOS<int16_t> buffer(nbuffer, mutex);
+void doWrite(); // forward declaration 
+Task writeTask("write",5000,10, doWrite);
+
+// create data and write it to buffer
+void doWrite() {
+    int16_t data[512];
+    for (int j=0;j<512;j++){
+        data[j]=j;
+    }
+    buffer.writeArray(data, 512);
+}
+
+void setup(){
+    // Setup logging
+    Serial.begin(115200);
+    AudioLogger::instance().begin(Serial, AudioLogger::Info);
+    
+    // start on core 0
+    writeTask.Start(0); 
+}
+
+// The loop runs on core 1: We read back the data
+void loop(){
+    int16_t data[512];
+    uint64_t start = micros();
+    buffer.readArray(data, 512));
+
+    // process (verify) data
+    int error=0;
+    for (int j=0;j<512;j++){
+        if(data[j]!=j){
+          error++;
+        }
+    }
+
+    // write result
+    Serial.print("error: ");
+    Serial.print(error);
+    Serial.print(" runtime: ");
+    Serial.println(micros()-start);
+  }
