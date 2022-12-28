66b3e836ceaaab6dd674c531d0018bfc60988e3a
Phil Schatzmann <phil.schatzmann@gmail.com>
2022-12-28 09:59:26 +0100

    Notes freq as float

--- src/AudioTools/MusicalNotes.h
+++ src/AudioTools/MusicalNotes.h
@@ -4,114 +4,114 @@
 
 #pragma once
 
-#define N_C0  16
-#define N_CS0 17
-#define N_D0  18
-#define N_DS0 19
-#define N_E0  21
-#define N_F0  22
-#define N_FS0 23
-#define N_G0  25
-#define N_GS0 26
-#define N_A0  28
-#define N_AS0 29
-#define N_B0  31
-#define N_C1  33
-#define N_CS1 35
-#define N_D1  37
-#define N_DS1 39
-#define N_E1  41
-#define N_F1  44
-#define N_FS1 46
-#define N_G1  49
-#define N_GS1 52
-#define N_A1  55
-#define N_AS1 58
-#define N_B1  62
-#define N_C2  65
-#define N_CS2 69
-#define N_D2  73
-#define N_DS2 78
-#define N_E2  82
-#define N_F2  87
-#define N_FS2 93
-#define N_G2  98
-#define N_GS2 104
-#define N_A2  110
-#define N_AS2 117
-#define N_B2  123
-#define N_C3  131
-#define N_CS3 139
-#define N_D3  147
-#define N_DS3 156
-#define N_E3  165
-#define N_F3  175
-#define N_FS3 185
-#define N_G3  196
-#define N_GS3 208
-#define N_A3  220
-#define N_AS3 233
-#define N_B3  247
-#define N_C4  262
-#define N_CS4 277
-#define N_D4  294
-#define N_DS4 311
-#define N_E4  330
-#define N_F4  349
-#define N_FS4 370
-#define N_G4  392
-#define N_GS4 415
-#define N_A4  440
-#define N_AS4 466
-#define N_B4  494
-#define N_C5  523
-#define N_CS5 554
-#define N_D5  587
-#define N_DS5 622
-#define N_E5  659
-#define N_F5  698
-#define N_FS5 740
-#define N_G5  784
-#define N_GS5 831
-#define N_A5  880
-#define N_AS5 932
-#define N_B5  988
-#define N_C6  1047
-#define N_CS6 1109
-#define N_D6  1175
-#define N_DS6 1245
-#define N_E6  1319
-#define N_F6  1397
-#define N_FS6 1480
-#define N_G6  1568
-#define N_GS6 1661
-#define N_A6  1760
-#define N_AS6 1865
-#define N_B6  1976
-#define N_C7  2093
-#define N_CS7 2217
-#define N_D7  2349
-#define N_DS7 2489
-#define N_E7  2637
-#define N_F7  2794
-#define N_FS7 2960
-#define N_G7  3136
-#define N_GS7 3322
-#define N_A7  3520
-#define N_AS7 3729
-#define N_B7  3951
-#define N_C8  4186
-#define N_CS8 4435
-#define N_D8  4699
-#define N_DS8 4978
-#define N_E8  5274
-#define N_F8  5588
-#define N_FS8 5920
-#define N_G8  6272
-#define N_GS8 6645
-#define N_A8  7040
-#define N_AS8 7459
-#define N_B8  7902
+#define N_C0  16.35f
+#define N_CS0 17.32f
+#define N_D0  18.35f
+#define N_DS0 19.45f
+#define N_E0  20.60f
+#define N_F0  21.83f
+#define N_FS0 23.12f
+#define N_G0  24.50f
+#define N_GS0 25.96f
+#define N_A0  27.50f
+#define N_AS0 29.14f
+#define N_B0  30.87f
+#define N_C1  32.70f
+#define N_CS1 34.65f
+#define N_D1  36.71f
+#define N_DS1 38.89f
+#define N_E1  41.20f
+#define N_F1  43.65f
+#define N_FS1 46.25f
+#define N_G1  49.00f
+#define N_GS1 51.91f
+#define N_A1  55.00f
+#define N_AS1 58.27f
+#define N_B1  61.74f
+#define N_C2  65.41f
+#define N_CS2 69.30f
+#define N_D2  73.42f
+#define N_DS2 77.78f
+#define N_E2  82.41f
+#define N_F2  87.31f
+#define N_FS2 92.50f
+#define N_G2  98.00f
+#define N_GS2 103.83f
+#define N_A2  110.00f
+#define N_AS2 116.54f
+#define N_B2  123.47f
+#define N_C3  130.81f
+#define N_CS3 138.59f
+#define N_D3  146.83f
+#define N_DS3 155.56f
+#define N_E3  164.81f
+#define N_F3  174.61f
+#define N_FS3 185.00f
+#define N_G3  196.00f
+#define N_GS3 207.65f
+#define N_A3  220.00f
+#define N_AS3 233.08f
+#define N_B3  246.94f
+#define N_C4  261.63f
+#define N_CS4 277.18f
+#define N_D4  293.66f
+#define N_DS4 311.13f
+#define N_E4  329.63f
+#define N_F4  349.23f
+#define N_FS4 369.99f
+#define N_G4  392.00f
+#define N_GS4 415.30f
+#define N_A4  440.00f
+#define N_AS4 466.16f
+#define N_B4  493.88f
+#define N_C5  523.25f
+#define N_CS5 554.37f
+#define N_D5  587.33f
+#define N_DS5 622.25f
+#define N_E5  659.25f
+#define N_F5  698.46f
+#define N_FS5 739.99f
+#define N_G5  783.99f
+#define N_GS5 830.61f
+#define N_A5  880.00f
+#define N_AS5 932.33f
+#define N_B5  987.77f
+#define N_C6  1046.5f
+#define N_CS6 1108.73f
+#define N_D6  1174.66f
+#define N_DS6 1244.51f
+#define N_E6  1318.51f
+#define N_F6  1396.91f
+#define N_FS6 1479.89f
+#define N_G6  1567.89f
+#define N_GS6 1661.22f
+#define N_A6  1760.00f
+#define N_AS6 1864.66f
+#define N_B6  1975.53f
+#define N_C7  2093.00f
+#define N_CS7 2217.46f
+#define N_D7  2349.32f
+#define N_DS7 2489.02f
+#define N_E7  2637.02f
+#define N_F7  2793.83f
+#define N_FS7 2959.96f
+#define N_G7  3135.96f
+#define N_GS7 3322.44f
+#define N_A7  3520.00f
+#define N_AS7 3729.31f
+#define N_B7  3951.07f
+#define N_C8  4186.01f
+#define N_CS8 4434.92f
+#define N_D8  4698.63f
+#define N_DS8 4978.03f
+#define N_E8  5274.04f
+#define N_F8  5587.65f
+#define N_FS8 5919.91f
+#define N_G8  6271.93f
+#define N_GS8 6644.88f
+#define N_A8  7040.00f
+#define N_AS8 7458.62f
+#define N_B8  7902.13f
 
 namespace audio_tools {
 
@@ -128,14 +128,14 @@ public:
     enum MusicalNotesEnum {C, CS, D, DS, E, F, FS, G, GS, A, AS, B};
 
     /// Determines the frequency of the indicate note and octave (0-8)
-    int frequency(MusicalNotesEnum note, uint8_t octave){
+    float frequency(MusicalNotesEnum note, uint8_t octave){
         if (note>11) return 0;
         if (octave>8) return 0;
         return notes[octave][note];
     }
 
     /// Determines the frequency of the indicate note index from 0 to 107
-    int frequency(uint16_t idx){
+    float frequency(uint16_t idx){
         MusicalNotesEnum mainNote = (MusicalNotesEnum) (idx % 12);
         uint8_t octave = idx / 12;
         return frequency(mainNote, octave);
@@ -146,32 +146,32 @@ public:
     }
     
     /// Determines the frequency of the indicate main note index (0-6)  and octave (0-8)
-    int mainFrequency(uint8_t mainNoteIdx, uint8_t octave){
+    float mainFrequency(uint8_t mainNoteIdx, uint8_t octave){
         if (mainNoteIdx>6) return 0;
-        static uint16_t mainNotes[] = {0,2,4,5,7,9,11};
+        static int mainNotes[] = {0,2,4,5,7,9,11};
         MusicalNotesEnum note = (MusicalNotesEnum) mainNotes[mainNoteIdx];
         return frequency(note, octave);
     }
     
     /// Determines the frequency of the indicate main note index from 0 to 62
-    int mainFrequency(uint64_t idx){
+    float mainFrequency(uint64_t idx){
         uint8_t mainNote = idx % 7;
         uint8_t level = idx /7;
         return mainFrequency(mainNote, level);
     }
 
     /// Returns true if the frequency is audible (in the range of 20 Hz to 20 kHz)
-    bool isAudible(uint16_t frequency){
+    bool isAudible(float frequency){
         return frequency >= 20 && frequency<=20000;
     }
 
     /// Determines the closes note for a frequency. We also return the frequency difference
-    const char* note(int frequency, int &diff){
-        uint16_t* all_notes = (uint16_t*) notes;
+    const char* note(float frequency, float &diff){
+        float* all_notes = (float*) notes;
         const int note_count = 12*9;
 
         // find closest note
-        int min_diff = frequency;
+        float min_diff = frequency;
         int min_pos = 0;
         for (int j=0; j<note_count; j++){
             int tmp = abs(frequency - all_notes[j]);
@@ -181,14 +181,14 @@ public:
             }
         }
 
-        int noteFrequency = all_notes[min_pos];
+        float noteFrequency = all_notes[min_pos];
         diff = frequency - noteFrequency;
         return notes_str[min_pos];
     }
 
     /// Determines the closes note for a frequency
-    const char* note(int frequency){
-        int diff;
+    const char* note(float frequency){
+        float diff;
         return note(frequency, diff);
     }
 
@@ -209,7 +209,7 @@ public:
 
 protected:
 
-    uint16_t notes[9][12] = {
+    float notes[9][12] = {
         {N_C0, N_CS0, N_D0, N_DS0, N_E0, N_F0, N_FS0, N_G0, N_GS0, N_A0, N_AS0, N_B0},
         {N_C1, N_CS1, N_D1, N_DS1, N_E1, N_F1, N_FS1, N_G1, N_GS1, N_A1, N_AS1, N_B1},
         {N_C2, N_CS2, N_D2, N_DS2, N_E2, N_F2, N_FS2, N_G2, N_GS2, N_A2, N_AS2, N_B2},
