e9f16f611910ae2549f1a5b931507a2b46138415
pschatzmann <phil.schatzmann@gmail.com>
2023-01-31 19:02:01 +0100

    LinuxStdio

--- cmake-tests/generator/generator.cpp
+++ cmake-tests/generator/generator.cpp
@@ -1,31 +1,28 @@
 // Simple wrapper for Arduino sketch to compilable with cpp in cmake
 #include "Arduino.h"
 #include "AudioTools.h"
-#include "AudioLibs/PortAudioStream.h"
+//#include "AudioLibs/PortAudioStream.h"
+#include "AudioLibs/LinuxStdio.h"
 
-uint16_t sample_rate=44100;
-uint8_t channels = 2;                                             // The stream will have 2 channels 
+LinuxStdio out;                                              // Output to Desktop
 SineWaveGenerator<int16_t> sine_wave(32000);                                           // subclass of SoundGenerator with max amplitude of 32000
 GeneratedSoundStream<int16_t> in_stream(sine_wave);                   // Stream generated from sine wave
-PortAudioStream out;                                              // Output to Desktop
 StreamCopy copier(out, in_stream);                                // copies sound to out
-ChannelReducer<int16_t> reducer(channels,1);
 
 void setup(){
   Serial.begin(115200);
-  AudioLogger::instance().begin(Serial, AudioLogger::Info);  
+  AudioLogger::instance().begin(Serial, AudioLogger::Warning);
 
-  sine_wave.begin(channels, sample_rate, N_B4);
+  auto cfg = out.defaultConfig();
+  if (!out.begin(cfg)){
+    stop();
+  }
+
+  sine_wave.begin(cfg.channels, cfg.sample_rate, N_B4);
   in_stream.begin();
-  
-  auto cfg1 = out.defaultConfig();
-  cfg1.sample_rate = sample_rate;
-  cfg1.channels = channels;
-  cfg1.bits_per_sample = 16;
-  out.begin(cfg1);
 }
 
 void loop(){
-    copier.copy(reducer);
+    copier.copy();
 }
 
---
+++ src/AudioLibs/LinuxStdio.h
@@ -0,0 +1,50 @@
+#pragma once
+#include <unistd.h>
+#include "AudioTools/AudioStreams.h"
+
+/**
+ * @brief Direct binary Audio Output to stdout
+ * @author Phil Schatzmann
+ * @ingroup io
+ * @copyright GPLv3
+ */
+namespace audio_tools {
+class LinuxStdio : public AudioStreamX {
+public:
+  AudioBaseInfo defaultConfig() {
+    AudioBaseInfo def;
+    def.bits_per_sample = 16;
+    def.sample_rate = 44100;
+    def.channels = 2;
+    return def;
+  }
+
+  bool begin(AudioBaseInfo info) {
+    is_open = true;
+    return true;
+  }
+  int available() override { return DEFAULT_BUFFER_SIZE; }
+
+  size_t readBytes(uint8_t* buffer, size_t len){
+    return ::read(0, buffer, len);
+  }
+
+  int availableForWrite() override { return DEFAULT_BUFFER_SIZE; }
+
+  size_t write(const uint8_t *buffer, size_t len) override {
+    if (!is_open)
+      return 0;
+    // write to stdout
+    return ::write(1, buffer, len);
+  }
+
+  void end() {
+    is_open = false;
+  }
+
+protected:
+  bool is_open = false;
+  FILE *out;
+};
+
+} // namespace audio_tools
