fb20c38b2f784ad23b29675de33a448b85db3849
Phil Schatzmann <phil.schatzmann@gmail.com>
2023-02-12 15:50:45 +0100

    Doxygen correction

--- src/AudioAnalog/AnalogAudio.h
+++ src/AudioAnalog/AnalogAudio.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "AudioConfig.h"
-#if defined(USE_I2S_ANALOG) || defined(DOXYGEN)
+#if defined(USE_I2S_ANALOG) 
 #include "AudioAnalog/AnalogAudioBase.h"
 #include "AudioAnalog/AnalogAudioESP32.h"
 #include "AudioAnalog/AnalogAudioArduino.h"
