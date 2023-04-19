34e97731e758527b2196c26d06db9fcf582e7f1f
pschatzmann <phil.schatzmann@gmail.com>
2023-04-20 00:20:31 +0200

    correct examples with compile errors

--- examples/tests/conversion/channel-converter-increase-in/channel-converter-increase-in.ino
+++ examples/tests/conversion/channel-converter-increase-in/channel-converter-increase-in.ino
@@ -1,7 +1,6 @@
 #include "AudioTools.h"
 
-uint16_t sample_rate=44100;
-uint8_t from_channels = 1;                                           // The stream will have 2 channels 
+AudioInfo info(44100, 1, 16);
 uint8_t to_channels = 2;                                             // The stream will have 2 channels 
 SineWaveGenerator<int16_t> sine_wave(32000);                         // subclass of SoundGenerator with max amplitude of 32000
 GeneratedSoundStream<int16_t> in_stream(sine_wave);                  // Stream generated from sine wave
@@ -13,8 +12,8 @@ void setup(){
   Serial.begin(115200);
   AudioLogger::instance().begin(Serial, AudioLogger::Info);  
 
-  sine_wave.begin(from_channels, sample_rate, N_B4);
-  conv.begin(from_channels, to_channels);
+  sine_wave.begin(info, N_B4);
+  conv.begin(info, to_channels);
   in_stream.begin();
 
   out.begin();
