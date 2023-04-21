81673d9651100e2e6954288667fd7f2ab4bf05ee
pschatzmann <phil.schatzmann@gmail.com>
2023-04-21 09:55:47 +0200

    cleanup

--- examples/examples-stream/streams-memory_raw-i2s/streams-memory_raw-i2s.ino
+++ examples/examples-stream/streams-memory_raw-i2s/streams-memory_raw-i2s.ino
@@ -11,24 +11,17 @@
 #include "AudioTools.h"
 #include "StarWars30.h"
 
-
-
-uint8_t channels = 1;
-uint16_t sample_rate = 22050;
-
+AudioInfo info(22050, 1, 16);
 I2SStream i2s;  // Output to I2S
 MemoryStream music(StarWars30_raw, StarWars30_raw_len);
-StreamCopyT<int16_t> copier(i2s, music); // copies sound into i2s
-
+StreamCopy copier(i2s, music); // copies sound into i2s
 
 void setup(){
     Serial.begin(115200);
     AudioLogger::instance().begin(Serial, AudioLogger::Info);
 
     auto config = i2s.defaultConfig(TX_MODE);
-    config.sample_rate = sample_rate;
-    config.channels = channels;
-    config.bits_per_sample = 16;
+    config.copyFrom(info);
     i2s.begin(config);
 
 }
