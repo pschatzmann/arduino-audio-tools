65e8d69617cae29af51d586c6a17a23d595c12f0
pschatzmann <phil.schatzmann@gmail.com>
2023-04-23 11:09:50 +0200

    udp examples

--- examples/examples-communication/udp/communication-udp-send/communication-udp-send.ino
+++ examples/examples-communication/udp/communication-udp-send/communication-udp-send.ino
@@ -1,7 +1,8 @@
 /**
  * @file example-udp-send.ino
  * @author Phil Schatzmann
- * @brief Sending audio over udp
+ * @brief Sending audio over udp. Because the SineWaveGenerator is providing the data as fast 
+ * as possible we need to throttle the speed.
  * @version 0.1
  * @date 2022-03-09
  *
@@ -13,42 +14,33 @@
 #include "AudioTools.h"
 #include "AudioLibs/Communication.h"
 
-uint16_t sample_rate = 44100;
-uint8_t channels = 2;  // The stream will have 2 channels
+const char *ssid = "ssid";
+const char *password = "password";
+AudioInfo info(22000, 1, 16);
 SineWaveGenerator<int16_t> sineWave( 32000);  // subclass of SoundGenerator with max amplitude of 32000
 GeneratedSoundStream<int16_t> sound( sineWave);  // Stream generated from sine wave
-UDPStream udp; 
+UDPStream udp(ssid, password); 
 Throttle throttle;
 IPAddress udpAddress(192, 168, 1, 255);
 const int udpPort = 7000;
 StreamCopy copier(udp, sound);  // copies sound into i2s
-const char *ssid = "ssid";
-const char *password = "password";
 
 
 void setup() {
   Serial.begin(115200);
   AudioLogger::instance().begin(Serial, AudioLogger::Info);
 
-  // connect to WIFI
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
-  }
-  Serial.println();
-  Serial.println(WiFi.localIP());
-
-
   // Define udp address and port
   udp.begin(udpAddress, udpPort);
+
+  // Define Throttle
   auto cfg = throttle.defaultConfig();
-  cfg.channels = channels;
-  cfg.sample_rate = sample_rate;
+  cfg.copyFrom(info);
+  //cfg.correction_ms = 0;
   throttle.begin(cfg);
 
   // Setup sine wave
-  sineWave.begin(channels, sample_rate, N_B4);
+  sineWave.begin(info, N_B4);
   Serial.println("started...");
 }
 
