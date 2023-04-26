6a547266592afb412f96aa49334a55a939e3131f
pschatzmann <phil.schatzmann@gmail.com>
2023-04-26 09:06:17 +0200

    decoding read example

---
+++ examples/tests/codecs/test-mp3-helix-reading/test-mp3-helix-reading.ino
@@ -0,0 +1,33 @@
+/**
+ * @brief Tesing the MP3DecoderHelix
+ * 
+ */
+// install https://github.com/pschatzmann/arduino-libhelix.git
+
+#include "AudioTools.h"
+#include "AudioLibs/AudioKit.h"
+#include "AudioCodecs/CodecMP3Helix.h"
+#include "BabyElephantWalk60_mp3.h"
+
+MemoryStream mp3(BabyElephantWalk60_mp3, BabyElephantWalk60_mp3_len);
+//CsvStream<int16_t> out(Serial,2);  
+AudioKitStream out;
+EncodedAudioStream pcm_source(&mp3, new MP3DecoderHelix()); // output to decoder
+StreamCopy copier(out, pcm_source);   
+
+void setup(){
+  Serial.begin(115200);
+  AudioLogger::instance().begin(Serial, AudioLogger::Info);  
+ 
+  // setup I2s
+  out.begin(out.defaultConfig());
+
+  // make sure that i2s is updated
+  pcm_source.setNotifyAudioChange(out);
+  // setup pcm_source
+  pcm_source.begin();
+}
+
+void loop(){
+  copier.copy();
+}
