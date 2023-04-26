41efaaf72532853d68c77590f83d67681b33c35d
pschatzmann <phil.schatzmann@gmail.com>
2023-04-26 16:31:38 +0200

    ILBC codec

--- examples/tests/codecs/test-codec-iLBC/test-codec-iLBC.ino
+++ examples/tests/codecs/test-codec-iLBC/test-codec-iLBC.ino
@@ -1,5 +1,5 @@
 /**
- * @file communication-codec-test.ino
+ * @file test-codec-iLBC.ino
  * @author Phil Schatzmann
  * @brief generate sine wave -> encoder -> decoder -> audiokit (i2s)
  * @version 0.1
@@ -20,6 +20,16 @@ EncodedAudioStream decoder(&out, new ILBCDecoder()); // encode and write
 EncodedAudioStream encoder(&decoder, new ILBCEncoder()); // encode and write
 StreamCopy copier(encoder, sound);     
 
+void loop1(void*) {
+  // start decoder
+  decoder.begin(info);
+
+  // start encoder
+  encoder.begin(info);
+  while(true){
+    copier.copy();
+  }
+}
 
 void setup() {
   Serial.begin(115200);
@@ -34,16 +44,12 @@ void setup() {
   // Setup sine wave
   sineWave.begin(info, N_B4);
 
-  // start decoder
-  decoder.begin(info);
-
-  // start encoder
-  encoder.begin(info);
+  int stack = 20000;
+  xTaskCreate(loop1,"loopTask", stack, nullptr,1, nullptr);
 
   Serial.println("Test started...");
 }
 
 
 void loop() { 
-  copier.copy();
 }
