ae85e349401428e0d4498f3fd7762ac0ee8c725d
pschatzmann <phil.schatzmann@gmail.com>
2023-05-02 12:09:11 +0200

    Helix callback with additional argument

--- src/AudioCodecs/CodecMP3Helix.h
+++ src/AudioCodecs/CodecMP3Helix.h
@@ -7,9 +7,6 @@
 
 namespace audio_tools {
 
-// audio change notification target
-static AudioInfoSupport *audioChangeMP3Helix=nullptr;
-
 /**
  * @brief MP3 Decoder using libhelix: https://github.com/pschatzmann/arduino-libhelix
  * This is basically just a simple wrapper to provide AudioInfo and AudioInfoSupport
@@ -124,18 +121,19 @@ class MP3DecoderHelix : public AudioDecoder  {
         void setNotifyAudioChange(AudioInfoSupport &bi){
             TRACED();
             audioChangeMP3Helix = &bi;
-            if (mp3!=nullptr)  mp3->setInfoCallback(infoCallback);
+            if (mp3!=nullptr)  mp3->setInfoCallback(infoCallback, this);
         }
 
         /// notifies the subscriber about a change
-        static void infoCallback(MP3FrameInfo &i){
-            if (audioChangeMP3Helix!=nullptr){
+        static void infoCallback(MP3FrameInfo &i, void * ref){
+            MP3DecoderHelix* p_helix = (MP3DecoderHelix* )ref;
+            if (p_helix!=nullptr && p_helix->audioChangeMP3Helix!=nullptr){
                 TRACED();
                 AudioInfo baseInfo;
                 baseInfo.channels = i.nChans;
                 baseInfo.sample_rate = i.samprate;
                 baseInfo.bits_per_sample = i.bitsPerSample;
-                audioChangeMP3Helix->setAudioInfo(baseInfo);   
+                p_helix->audioChangeMP3Helix->setAudioInfo(baseInfo);   
             }
         }
 
@@ -184,6 +182,9 @@ class MP3DecoderHelix : public AudioDecoder  {
         libhelix::MP3DecoderHelix *mp3=nullptr;
         MetaDataFilter<libhelix::MP3DecoderHelix> filter;
         bool use_filter = false;
+        // audio change notification target
+        AudioInfoSupport *audioChangeMP3Helix=nullptr;
+
 
 };
 
