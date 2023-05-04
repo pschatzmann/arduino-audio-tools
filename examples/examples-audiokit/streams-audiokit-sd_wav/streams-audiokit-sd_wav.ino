1815a3b7898be16de5d654d71aecbe8b5bbefc8b
pschatzmann <phil.schatzmann@gmail.com>
2023-05-04 11:22:57 +0200

    streams-audiokit-sd_wav.ino

---
+++ examples/examples-audiokit/streams-audiokit-sd_wav/streams-audiokit-sd_wav.ino
@@ -0,0 +1,66 @@
+/**
+ * @file streams-audiokit-sd_wav.ino
+ * @author Phil Schatzmann
+ * @brief Demonstrates how to use an encoder to write to a file
+ * @version 0.1
+ * @date 2023-05-04
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+#include <SD.h>
+#include <SPI.h>
+
+#include "AudioTools.h"
+#include "AudioLibs/AudioKit.h"
+
+const char* file_name = "/rec.wav";
+AudioInfo info(16000, 1, 16);
+AudioKitStream in; // replace with any other audio source 
+File file;  // final output stream
+EncodedAudioStream out(&file, new WAVEncoder());
+StreamCopy copier(out, in);  // copies data
+uint64_t timeout;
+
+
+void setup() {
+  Serial.begin(115200);
+  AudioLogger::instance().begin(Serial, AudioLogger::Warning);
+
+  // Open SD drive
+  if (!SD.begin(PIN_AUDIO_KIT_SD_CARD_CS)) {
+    Serial.println("Initialization failed!");
+    stop();
+  }
+  
+  // open file
+  file = SD.open(file_name, FILE_WRITE);
+
+  // setup input
+  auto cfg = in.defaultConfig(RX_MODE);
+  cfg.sd_active = true;
+  cfg.copyFrom(info);
+  in.begin(cfg);
+
+  // setup output
+  out.begin(info);
+
+  // set timeout - recoring for 5 seconds
+  timeout = millis() + 5000;
+}
+
+void loop() {
+  if (timeout < millis()) {
+    // record to wav file
+    copier.copy();
+  } else {
+    // close file when done
+    if (file) {
+      file.flush(); 
+      Serial.print("File has ");
+      Serial.print(file.size());
+      Serial.println(" bytes");
+      file.close();
+    }
+  }
+}
