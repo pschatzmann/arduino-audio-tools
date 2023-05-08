4cca158f422f291ce58ec3e04b482043a2d82f69
pschatzmann <phil.schatzmann@gmail.com>
2023-05-08 17:17:01 +0200

    server encoding examples

---
+++ examples/examples-webserver/streams-generator-webserver_aac/streams-generator-webserver_aac.ino
@@ -0,0 +1,39 @@
+/**
+ * @file streams-generator-server_aac.ino
+ *
+ *  This sketch generates a test sine wave. The result is provided as AAC stream which can be listened to in a Web Browser
+ *
+ * @author Phil Schatzmann
+ * @copyright GPLv3
+ * 
+ */
+
+#include "AudioTools.h"
+#include "AudioCodecs/CodecAACFDK.h"
+
+// WIFI
+const char *ssid = "ssid";
+const char *password = "password";
+
+AudioInfo info(16000,1,16);
+AACEncoderFDK fdk;
+AudioEncoderServer server(&fdk, ssid, password);
+SineWaveGenerator<int16_t> sineWave;            // Subclass of SoundGenerator with max amplitude of 32000
+GeneratedSoundStream<int16_t> in(sineWave);     // Stream generated from sine wave
+
+void setup() {
+  Serial.begin(115200);
+  AudioLogger::instance().begin(Serial,AudioLogger::Info);
+
+  // start server
+  server.begin(in, info);
+
+  // start generation of sound
+  sineWave.begin(info, N_B4);
+}
+
+
+// copy the data
+void loop() {
+  server.copy();
+}
---
+++ examples/examples-webserver/streams-generator-webserver_mp3/streams-generator-webserver_mp3.ino
@@ -0,0 +1,39 @@
+/**
+ * @file streams-generator-server_ogg.ino
+ *
+ *  This sketch generates a test sine wave. The result is provided as mp3 stream which can be listened to in a Web Browser
+ *  Please note that MP3EncoderLAME needs a processor with PSRAM !
+ * @author Phil Schatzmann
+ * @copyright GPLv3
+ * 
+ */
+
+#include "AudioTools.h"
+#include "AudioCodecs/CodecMP3LAME.h"
+
+// WIFI
+const char *ssid = "ssid";
+const char *password = "password";
+
+AudioInfo info(16000,1,16);
+MP3EncoderLAME mp3;
+AudioEncoderServer server(&mp3, ssid, password);
+SineWaveGenerator<int16_t> sineWave;            // Subclass of SoundGenerator with max amplitude of 32000
+GeneratedSoundStream<int16_t> in(sineWave);     // Stream generated from sine wave
+
+void setup() {
+  Serial.begin(115200);
+  AudioLogger::instance().begin(Serial,AudioLogger::Info);
+
+  // start server
+  server.begin(in, info);
+
+  // start generation of sound
+  sineWave.begin(info, N_B4);
+}
+
+
+// copy the data
+void loop() {
+  server.copy();
+}
