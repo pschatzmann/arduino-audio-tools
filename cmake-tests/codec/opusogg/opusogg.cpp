a99044876c7fc370e0447d5dea0f9515107b917f
pschatzmann <phil.schatzmann@gmail.com>
2023-05-08 12:57:18 +0200

    example opusogg

--- cmake-tests/codec/opusogg/opusogg.cpp
+++ cmake-tests/codec/opusogg/opusogg.cpp
@@ -11,23 +11,20 @@
 #include "AudioTools.h"
 #include "AudioCodecs/CodecOpusOgg.h"
 
-int sample_rate = 24000;
-int channels = 1;  // The stream will have 2 channels
 int application = OPUS_APPLICATION_AUDIO; // Opus application
-
+AudioInfo info(24000, 1, 16);
 SineWaveGenerator<int16_t> sineWave( 32000);  // subclass of SoundGenerator with max amplitude of 32000
 GeneratedSoundStream<int16_t> sound( sineWave); // Stream generated from sine wave
-CsvOutput<int16_t> out(Serial, channels);   // Output of sound on desktop 
+CsvOutput<int16_t> out(Serial);   // Output of sound on desktop 
 OpusOggEncoder enc;
 OpusOggDecoder dec;
 EncodedAudioStream decoder(&out, &dec); // encode and write 
 EncodedAudioStream encoder(&decoder, &enc); // encode and write 
-//EncodedAudioStream encoder(&decoder, &enc); // encode and write 
 StreamCopy copier(encoder, sound);     
 
 void setup() {
   Serial.begin(115200);
-  AudioLogger::instance().begin(Serial, AudioLogger::Debug);
+  AudioLogger::instance().begin(Serial, AudioLogger::Warning);
 
   // Setup output
   auto cfgo = out.defaultConfig();
@@ -35,18 +32,14 @@ void setup() {
   out.begin(cfgo);
 
   // Setup sine wave
-  auto cfgs = sineWave.defaultConfig();
-  cfgs.sample_rate = sample_rate;
-  cfgs.channels = channels;
-  cfgs.bits_per_sample = 16;
-  sineWave.begin(cfgs, N_B4);
+  sineWave.begin(info, N_B4);
 
   // Opus decoder needs to know the audio info
-  decoder.begin(cfgs);
+  decoder.begin(info);
 
   // configure and start encoder
   enc.config().application = application;
-  encoder.begin(cfgs);
+  encoder.begin(info);
 
   Serial.println("Test started...");
 }
