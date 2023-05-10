280cb76e37cdd90410ef7e95d08a19311143c58e
pschatzmann <phil.schatzmann@gmail.com>
2023-05-10 10:24:52 +0200

    ADPCM Codec

---
+++ cmake-tests/codec/adpcm/adpcm.cpp
@@ -0,0 +1,51 @@
+/**
+ * @file test-codec-adpcm.ino
+ * @author Phil Schatzmann
+ * @brief generate sine wave -> encoder -> decoder -> audiokit (i2s)
+ * @version 0.1
+ * @date 2022-04-30
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include "AudioTools.h"
+#include "AudioCodecs/CodecADPCM.h"
+#include "AudioLibs/PortAudioStream.h"
+
+AudioInfo info(8000, 2, 16);
+SineWaveGenerator<int16_t> sineWave( 32000);  // subclass of SoundGenerator with max amplitude of 32000
+GeneratedSoundStream<int16_t> sound( sineWave); // Stream generated from sine wave
+//I2SStream out; 
+PortAudioStream out; 
+EncodedAudioStream decoder(&out, new ADPCMDecoder()); // encode and write
+EncodedAudioStream encoder(&decoder, new ADPCMEncoder()); // encode and write
+StreamCopy copier(encoder, sound);     
+
+void setup() {
+  Serial.begin(115200);
+  AudioLogger::instance().begin(Serial, AudioLogger::Warning);
+
+  // start I2S
+  Serial.println("starting I2S...");
+  auto cfgi = out.defaultConfig(TX_MODE);
+  cfgi.copyFrom(info);
+  out.begin(cfgi);
+
+  // Setup sine wave
+  auto cfgs = sineWave.defaultConfig();
+  cfgs.copyFrom(info);
+  sineWave.begin(info, N_B4);
+
+  // start decoder
+  decoder.begin(info);
+
+  // start encoder
+  encoder.begin(info);
+
+  Serial.println("Test started...");
+}
+
+
+void loop() { 
+  copier.copy();
+}
