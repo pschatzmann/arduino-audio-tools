d190c163fda60ec2771f53fe73fddc8709a1ac94
LinusHeu <118672629+LinusHeu@users.noreply.github.com>
2023-06-05 01:10:40 +0200

    Fix I2S Output on RP2040 (#885)

--- src/AudioI2S/I2SRP2040.h
+++ src/AudioI2S/I2SRP2040.h
@@ -11,15 +11,11 @@
 
 namespace audio_tools {
 
-#if !defined(ARDUINO_ARCH_MBED_RP2040)//todo why this if?
-//static ::I2S I2S;
-#endif
-
-
 /**
  * @brief Basic I2S API - for the ...
  * @ingroup platform
  * @author Phil Schatzmann
+ * @author LinusHeu
  * @copyright GPLv3
  */
 class I2SDriverRP2040 {
@@ -34,7 +30,7 @@ class I2SDriverRP2040 {
     }
 
     /// starts the DAC with the default config in TX Mode
-    bool begin(RxTxMode mode = TX_MODE)  {
+    bool begin(RxTxMode mode = TX_MODE) {
       TRACED();
       return begin(defaultConfig(mode));
     }
@@ -48,11 +44,11 @@ class I2SDriverRP2040 {
       case TX_MODE:
         i2s = I2S(OUTPUT);
         break;
-      case RX_MODE:
-        i2s = I2S(INPUT);
-        break;
+      //case RX_MODE:
+      //  i2s = I2S(INPUT);
+      //  break;
       default:
-        LOGE("Unsupported mode: only TX_MODE, RX_MODE is supported");
+        LOGE("Unsupported mode: only TX_MODE is supported");
         return false;
         break;
       }
@@ -86,7 +82,7 @@ class I2SDriverRP2040 {
         return false;
       }
 
-      if(cfg.i2s_format == I2S_LEFT_JUSTIFIED_FORMAT){//todo is I2S_LEFT_JUSTIFIED_FORMAT even LSBJ?
+      if(cfg.i2s_format == I2S_LEFT_JUSTIFIED_FORMAT){
         if(!i2s.setLSBJFormat()){
           LOGE("Could not set LSB Format")
           return false;
@@ -97,7 +93,7 @@ class I2SDriverRP2040 {
       }
 
       if (cfg.channels < 1 || cfg.channels > 2 ){
-        LOGE("Unsupported channels: '%d' - only 1 or 2 is supported", cfg.channels);
+        LOGE("Unsupported channels: '%d'", cfg.channels);
         return false;
       }
 
@@ -109,8 +105,9 @@ class I2SDriverRP2040 {
     }
 
     /// stops the I2C and uninstalls the driver
-    void end()  {
-      i2s.end();//todo flush before end?
+    void end() {
+      flush();
+      i2s.end();
     }
 
     /// provides the actual configuration
@@ -119,21 +116,27 @@ class I2SDriverRP2040 {
     }
 
     /// writes the data to the I2S interface 
-    size_t writeBytes(const void *src, size_t size_bytes)  {
+    size_t writeBytes(const void *src, size_t size_bytes) {
       TRACED();
       size_t result = 0;
-      int16_t *p16 = (int16_t *)src;
-      
+
       if (cfg.channels==1){
         result = writeExpandChannel(src,size_bytes);
       } else if (cfg.channels==2){
-        size_bytes = size_bytes /4*4;//ensures that size_bytes is always multiple of 4. Just don't write the rest, if it isn't
-        result = i2s.write((uint8_t*) src, size_bytes);//NOT Blocking
-      } 
+        const uint8_t *p = (const uint8_t*) src;
+        while(size_bytes >= sizeof(int32_t)){
+          bool justWritten = i2s.write(*(int32_t*)p,true); //I2S::write(int32,bool) actually only returns 0 or 1
+          if(justWritten){
+            size_bytes -= sizeof(int32_t);
+            p += sizeof(int32_t);
+            result += sizeof(int32_t);
+          } else return result;
+        }
+      }
       return result;
     }
 
-    size_t readBytes(void *dest, size_t size_bytes)  {
+    size_t readBytes(void *dest, size_t size_bytes) {
       TRACEE();
       size_t result = 0;
       return result;
@@ -148,12 +151,12 @@ class I2SDriverRP2040 {
       } 
     }
 
-    int available()  {
+    int available() {
       return 0;
     }
 
-    void flush()   {
-      return i2s.flush();
+    void flush() {
+      i2s.flush();
     }
 
   protected:
@@ -163,25 +166,21 @@ class I2SDriverRP2040 {
     //writes 1 channel to I2S while expanding it to 2 channels
     //writes multiple of 4 bytes like I2S::write() wants
     //returns amount of bytes written from src to i2s
-    size_t writeExpandChannel(const void *src, size_t size_bytes){
+    size_t writeExpandChannel(const void *src, size_t size_bytes) {
       size_t writtenBytes = 0;
-
       switch(cfg.bits_per_sample){
         case 8:
-        for(int i = 0; i<size_bytes - 1; i += 2){//2 Samples read at a time to write 4 bytes
+        for(int i = 0; i<size_bytes - 1; i += 2){//2 Samples written at a time to write 4 bytes
           int8_t frame[4];
           int8_t *data = (int8_t*) src;
           frame[0] = data[i];
           frame[1] = data[i];
           frame[2] = data[i+1];//i<size_bytes-1 in for() because of this +1
           frame[3] = data[i+1];
-          size_t justWritten = i2s.write(*(uint32_t*)frame, true);//blocking
-          if(!justWritten){//write() only returns true or false. 
-            return writtenBytes;
-          } else {
-            //half because we doubled the bytes before writing to get 2 channels from 1
+          bool justWritten = i2s.write(*(uint32_t*)frame, true);//blocking
+          if(justWritten){
             writtenBytes += 2;
-          }
+          } else return writtenBytes;
         }
         break;
         case 16:
@@ -190,15 +189,12 @@ class I2SDriverRP2040 {
           int16_t *data = (int16_t *) src;
           frame[0] = data[i];
           frame[1] = data[i];
-          size_t justWritten = i2s.write(*(uint32_t*)frame, true);//blocking
-          if(!justWritten){//write() only returns true or false. 
-            return writtenBytes;
-          } else{
-            //half because we doubled the bytes before writing to get 2 channels from 1
-            writtenBytes += 2;
-          }
+          bool justWritten = i2s.write(*(uint32_t*)frame, true);//blocking
+          if(justWritten){
+            writtenBytes += sizeof(int16_t);
+          } else return writtenBytes;
         }
-        break;//TODO 24 and 32 BPS DO NOT HAVE the "make I2SRP2040-write blocking" workaround! Only for testing!
+        break;
         case 24://24bps are already stored as left-aligned int32_t => handle just like 32bps
         case 32:
         for(int i = 0; i<size_bytes/sizeof(int32_t); i++){//1 sample written at a time to write 2*4 bytes
@@ -206,21 +202,13 @@ class I2SDriverRP2040 {
           int32_t *data = (int32_t *) src;
           frame[0] = data[i];
           frame[1] = data[i];
-          size_t justWritten = i2s.write((uint8_t*) frame, 8);
-          if(justWritten != 8){//because we write 8 bytes, write() might return 0,4,8
-            writtenBytes += justWritten /2;
-            return writtenBytes;
-          } else {
-            //half because we doubled the bytes before writing to get 2 channels from 1
-            writtenBytes += justWritten /2;
-          }
-
+          bool justWritten = i2s.write(frame[0], true) && i2s.write(frame[1], true);
+          if(justWritten){
+            writtenBytes += sizeof(int32_t);
+          } else return writtenBytes;
         }
-        break;
       }
-      //mono=>stereo = doubling of bytes. We return here how many bytes from src were written
       return writtenBytes;
-
     }
 };
 
