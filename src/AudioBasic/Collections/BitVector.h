bd0a230d4e643ea03fa361d6cbac0a4cf5deec40
pschatzmann <phil.schatzmann@gmail.com>
2023-06-20 00:29:40 +0200

    PDMStream DRAFT

--- src/AudioBasic/Collections/BitVector.h
+++ src/AudioBasic/Collections/BitVector.h
@@ -18,7 +18,8 @@ class BitVector {
         return get(index);
     }
 
-    bool get(size_t index){
+    bool get(int64_t index){
+        if (index<0) return false;
         bool result = false;
         int offset = index / sizeof(uint64_t);
         int bit = index % sizeof(uint64_t);
@@ -30,7 +31,8 @@ class BitVector {
         return result;
     }
 
-    void set(size_t index, bool value){
+    void set(int64_t index, bool value){
+        if (index<0) return;
         max_idx = max(max_idx, index+1);
         int offset = index / sizeof(uint64_t);
         int bit = index % sizeof(uint64_t);
@@ -70,11 +72,58 @@ class BitVector {
         this->ref = ref;
     }
 
+    void resize(int size){
+        max_idx = size;
+        int round_up = size%64 != 0;
+        vector.resize(size/64 + round_up);
+    }
+
+    // shifts n bits: + to the left; - to the right
+    void shift(int n){
+        if (n==0) return;
+        for (int j=0; j < max_idx -n; j++){
+            set(j, get(j+n));
+        }
+    }
+
+    int8_t toInt8(int n){
+        int8_t result;
+        for (int j=0;j<8;j++){
+            bool x = get(n);
+            result ^= (-x ^ result) & (1UL << j);
+
+        }
+        return result;
+    }
+
+    int16_t toInt16(int n){
+        int16_t result;
+        for (int j=0;j<16;j++){
+            bool x = get(n);
+            result ^= (-x ^ result) & (1UL << j);
+
+        }
+        return result;
+    }
+
+    template <typename T>
+    T toInt(int n){
+        T result;
+        for (int j=0;j<sizeof(T)*8;j++){
+            bool x = get(n);
+            // set bit at pos
+            result ^= (-x ^ result) & (1UL << j);
+
+        }
+        return result;
+    }
+
+
   protected:
     Vector<uint64_t> vector;
     void (*changeHandler)(int idx, bool value, void*ref) = nullptr;
     void *ref;
-    size_t max_idx=0;
+    int64_t max_idx = 0;
 
 };
 
