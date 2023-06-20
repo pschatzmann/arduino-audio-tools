d2b31bc4adc84036df717436048bd5ee5935e590
pschatzmann <phil.schatzmann@gmail.com>
2023-06-20 15:21:53 +0200

    VolumeStream

---
+++ examples/examples-stream/streams-generator-volume/streams-generator-volume.ino
@@ -0,0 +1,41 @@
+/**
+ * @file streams-generator-volume.ino
+ * @author Phil Schatzmann
+ * @brief Determines the output volume (=amplitude)
+ * @copyright GPLv3
+ */
+ 
+#include "AudioTools.h"
+
+AudioInfo info(44100, 2, 16);
+SineWaveGenerator<int16_t> sineWave(32000);                // subclass of SoundGenerator with max amplitude of 32000
+GeneratedSoundStream<int16_t> sound(sineWave);             // Stream generated from sine wave
+VolumeOutput out; 
+StreamCopy copier(out, sound);                             // copies sound into i2s
+
+// Arduino Setup
+void setup(void) {  
+  // Open Serial 
+  Serial.begin(115200);
+  while(!Serial);
+  AudioLogger::instance().begin(Serial, AudioLogger::Info);
+
+  // start Volume Output
+  auto config = out.defaultConfig();
+  config.copyFrom(info); 
+  out.begin(config);
+
+  // Setup sine wave
+  sineWave.begin(info, N_B4);
+  Serial.println("started...");
+}
+
+// Arduino loop - copy sound to out 
+void loop() {
+  copier.copy();
+  Serial.print(out.volume()); // overall
+  Serial.print(" ");
+  Serial.print(out.volume(0)); // left
+  Serial.print(" ");
+  Serial.println(out.volume(1)); // right
+}
