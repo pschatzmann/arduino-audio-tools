2174cecef7f0341d32e5ebbe30919e8cd6e7758f
pschatzmann <phil.schatzmann@gmail.com>
2023-06-28 22:01:12 +0200

    PDM Example

--- examples/examples-stream/streams-i2s_pdm-serial/streams-i2s_pdm-serial.ino
+++ examples/examples-stream/streams-i2s_pdm-serial/streams-i2s_pdm-serial.ino
@@ -1,7 +1,7 @@
 /**
- * @file streams-i2s-serial.ino
+ * @file streams-i2s_pdm-serial.ino
  * @author Phil Schatzmann
- * @brief see https://github.com/pschatzmann/arduino-audio-tools/blob/main/examples/examples-stream/streams-i2s-serial/README.md
+ * @brief see https://github.com/pschatzmann/arduino-audio-tools/blob/main/examples/examples-stream/streams-i2s_pdm-serial/README.md
  * 
  * @author Phil Schatzmann
  * @copyright GPLv3
@@ -10,9 +10,9 @@
 
 #include "AudioTools.h"
 
-AudioInfo info(44100, 2, 32);
+AudioInfo info(44100, 1, 16);
 I2SStream i2sStream; // Access I2S as stream
-CsvOutput<int32_t> csvStream(Serial);
+CsvOutput<int16_t> csvStream(Serial);
 StreamCopy copier(csvStream, i2sStream); // copy i2sStream to csvStream
 
 // Arduino Setup
@@ -22,11 +22,11 @@ void setup(void) {
     
     auto cfg = i2sStream.defaultConfig(RX_MODE);
     cfg.copyFrom(info);
-    cfg.i2s_format = I2S_STD_FORMAT; // or try with I2S_LSB_FORMAT
-    cfg.is_master = true;
-     // this module nees a master clock if the ESP32 is master
-    cfg.use_apll = false;  // try with yes
-    cfg.pin_mck = 3; 
+    cfg.signal_type = PDM;
+    cfg.channel_format = I2S_CHANNEL_FMT_ALL_RIGHT;  // try with left
+    cfg.use_apll = false;  
+    cfg.auto_clear = false;
+    cfg.pin_bck = I2S_PIN_NO_CHANGE; // not used
     i2sStream.begin(cfg);
 
     // make sure that we have the correct channels set up
