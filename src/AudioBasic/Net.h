1ca9394244e22466db6bd1af3af7c1b4dde0f34f
Phil Schatzmann <phil.schatzmann@gmail.com>
2023-07-23 10:38:48 +0200

    cmake compile warnings

--- src/AudioBasic/Net.h
+++ src/AudioBasic/Net.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#ifndef htons
 #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
 #  define htonl(x) (((x) << 24 & 0xFF000000UL) | ((x) << 8 & 0x00FF0000UL) | ((x) >> 8 & 0x0000FF00UL) | ((x) >> 24 & 0x000000FFUL))
 #  define htons(x) (((uint16_t)(x)&0xff00) >> 8) | (((uint16_t)(x)&0X00FF) << 8)
@@ -12,4 +13,5 @@
 #  define ntohs(x) x
 #else
 #error Could not determine byte order
+#endif
 #endif
