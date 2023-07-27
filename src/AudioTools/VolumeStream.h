f8c5c0b350e7966587dbc7a85541c3aa97cc93a7
LinusHeu <118672629+LinusHeu@users.noreply.github.com>
2023-07-27 11:07:12 +0200

    Fixedpoint math for VolumeStream (#945)
    works at 16bit, too slow for 32bit

--- src/AudioTools/VolumeStream.h
+++ src/AudioTools/VolumeStream.h
@@ -187,7 +187,15 @@ class VolumeStream : public AudioStream {
               LOGI("setVolume: %f", volume_value);
               float factor = volumeControl().getVolumeFactor(volume_value);
               volume_values[channel]=volume_value;
+              #ifdef PREFER_FIXEDPOINT
+              //convert float to fixed point 2.6
+              //Fixedpoint-Math from https://github.com/earlephilhower/ESP8266Audio/blob/0abcf71012f6128d52a6bcd155ed1404d6cc6dcd/src/AudioOutput.h#L67
+              if(factor > 4.0) factor = 4.0;//factor can only be >1 if allow_boost == true TODO: should we update volume_values[channel] if factor got clipped to 4.0?
+              uint8_t factorF2P6 = (uint8_t) (factor*(1<<6));
+              factor_for_channel[channel] = factorF2P6;
+              #else
               factor_for_channel[channel]=factor;
+              #endif
             } else {
               LOGE("Invalid channel %d - max: %d", channel, info.channels-1);
             }
@@ -211,7 +219,11 @@ class VolumeStream : public AudioStream {
         SimulatedAudioPot pot_vc;
         CachedVolumeControl cached_volume{pot_vc};
         Vector<float> volume_values;
+        #ifdef PREFER_FIXEDPOINT
+        Vector<uint8_t> factor_for_channel; //Fixed point 2.6
+        #else
         Vector<float> factor_for_channel;
+        #endif
         bool is_started = false;
         float max_value = 32767; // max value for clipping
         int max_channels=0;
@@ -256,7 +268,11 @@ class VolumeStream : public AudioStream {
             return cached_volume;
         }
 
+        #ifdef PREFER_FIXEDPOINT
+        uint8_t factorForChannel(int channel){
+        #else
         float factorForChannel(int channel){
+        #endif
             return factor_for_channel.size()==0? 1.0 : factor_for_channel[channel];
         }
 
@@ -266,7 +282,7 @@ class VolumeStream : public AudioStream {
                     applyVolume16((int16_t*)buffer, size/2);
                     break;
                 case 24:
-                    applyVolume24((int24_t*)buffer, size/3);
+                    applyVolume24((int24_t*)buffer, size/3);//TODO is that size/3 right? sizeof(int24_t) is 4
                     break;
                 case 32:
                     applyVolume32((int32_t*)buffer, size/4);
@@ -278,7 +294,11 @@ class VolumeStream : public AudioStream {
 
         void applyVolume16(int16_t* data, size_t size){
             for (size_t j=0;j<size;j++){
+                #ifdef PREFER_FIXEDPOINT
+                int32_t result = (data[j] * factorForChannel(j%info.channels)) >> 6; //Fixedpoint-Math from https://github.com/earlephilhower/ESP8266Audio/blob/0abcf71012f6128d52a6bcd155ed1404d6cc6dcd/src/AudioOutput.h#L67
+                #else
                 float result = factorForChannel(j%info.channels) * data[j];
+                #endif
                 if (!info.allow_boost){
                     if (result>max_value) result = max_value;
                     if (result<-max_value) result = -max_value;
@@ -289,7 +309,11 @@ class VolumeStream : public AudioStream {
 
         void applyVolume24(int24_t* data, size_t size) {
             for (size_t j=0;j<size;j++){
+                #ifdef PREFER_FIXEDPOINT
+                int32_t result = (data[j] * factorForChannel(j%info.channels)) >> 6; //8bits * 24bits = fits into 32
+                #else
                 float result = factorForChannel(j%info.channels) * data[j];
+                #endif
                 if (!info.allow_boost){
                     if (result>max_value) result = max_value;
                     if (result<-max_value) result = -max_value;
@@ -301,7 +325,11 @@ class VolumeStream : public AudioStream {
 
         void applyVolume32(int32_t* data, size_t size) {
             for (size_t j=0;j<size;j++){
+                #ifdef PREFER_FIXEDPOINT
+                int64_t result = (static_cast<int64_t>(data[j]) * static_cast<int64_t>(factorForChannel(j%info.channels))) >> 6;
+                #else
                 float result = factorForChannel(j%info.channels) * data[j];
+                #endif
                 if (!info.allow_boost){
                     if (result>max_value) result = max_value;
                     if (result<-max_value) result = -max_value;
