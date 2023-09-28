ad557c410f7602b77c3d4390853af7a387117c49
Phil Schatzmann <phil.schatzmann@gmail.com>
2023-09-28 08:34:42 +0200

    Faust examples

---
+++ examples/examples-faust/streams-i2s-faust_pitchshift-i2s/streams-i2s-faust_pitchshift-i2s.ino
@@ -0,0 +1,42 @@
+/**
+ * @file streams-i2s-faust_pitchshift-i2s.ino
+ * @author Phil Schatzmann
+ * @brief Example how to use Faust for pitch shifting in stereo
+ * @version 0.1
+ * @date 2022-04-22
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include "AudioTools.h"
+#include "AudioLibs/AudioKit.h"
+#include "AudioLibs/AudioFaust.h"
+#include "pitchShifter.h"
+
+AudioKitStream io; 
+FaustStream<mydsp> faust(io);  // final output to io
+StreamCopy copier(faust, io);  // copy mic to faust
+
+// Arduino Setup
+void setup(void) {  
+  // Open Serial 
+  Serial.begin(115200);
+  AudioLogger::instance().begin(Serial, AudioLogger::Warning);
+
+  // Setup Faust
+  auto cfg = faust.defaultConfig();
+  faust.begin(cfg);
+
+
+  // start I2S
+  auto cfg_i2s = io.defaultConfig(RXTX_MODE);
+  cfg_i2s.copyFrom(cfg);
+  io.begin(cfg_i2s);
+
+}
+
+// Arduino loop - copy sound to out 
+void loop() {
+  copier.copy();
+}
