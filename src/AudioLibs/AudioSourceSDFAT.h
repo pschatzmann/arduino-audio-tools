a3a6ee468203b60a18d01edae901890fbe8f9bdf
Phil Schatzmann <phil.schatzmann@gmail.com>
2023-10-07 19:20:17 +0200

    AudioSourceSDFAT increase default to 10Mhz

--- src/AudioLibs/AudioSourceSDFAT.h
+++ src/AudioLibs/AudioSourceSDFAT.h
@@ -48,7 +48,7 @@ namespace audio_tools {
 class AudioSourceSDFAT : public AudioSource {
 public:
   /// Default constructor
-  AudioSourceSDFAT(const char* startFilePath = "/", const char* ext = ".mp3", int chipSelect = PIN_CS, int speedMHz = 2,int spi_mode=DEDICATED_SPI, bool setupIndex=true) {
+  AudioSourceSDFAT(const char* startFilePath = "/", const char* ext = ".mp3", int chipSelect = PIN_CS, int speedMHz = 10,int spi_mode=DEDICATED_SPI, bool setupIndex=true) {
         TRACED();
         LOGI("SD chipSelect: %d", chipSelect);
         LOGI("SD speedMHz: %d", speedMHz);
