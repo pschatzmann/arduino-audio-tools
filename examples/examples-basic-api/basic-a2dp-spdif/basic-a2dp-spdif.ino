25f819fca906c9ed4aa8ee674cc9368dc99b7527
Phil Schatzmann <phil.schatzmann@gmail.com>
2023-10-08 19:13:41 +0200

    basic-a2dp-spdif example

--- examples/examples-basic-api/basic-a2dp-spdif/basic-a2dp-spdif.ino
+++ examples/examples-basic-api/basic-a2dp-spdif/basic-a2dp-spdif.ino
@@ -5,11 +5,11 @@
  * @author Phil Schatzmann
  * @copyright GPLv3
  */
-
 #include "AudioConfigLocal.h"
 #include "BluetoothA2DPSink.h"
 #include "AudioTools.h"
 
+AudioInfo info(44100, 2, 16);
 BluetoothA2DPSink a2dp_sink;
 SPDIFOutput spdif;
 
@@ -21,21 +21,19 @@ void read_data_stream(const uint8_t *data, uint32_t length) {
 void setup() {
   Serial.begin(115200);
   AudioLogger::instance().begin(Serial, AudioLogger::Warning);
-  
+
+  // setup output
+  auto cfg = spdif.defaultConfig();
+  cfg.copyFrom(info);
+  cfg.pin_data = 23;
+  spdif.begin(cfg);
+
   // register callback
   a2dp_sink.set_stream_reader(read_data_stream, false);
 
   // Start Bluetooth Audio Receiver
   a2dp_sink.set_auto_reconnect(false);
   a2dp_sink.start("a2dp-spdif");
-
-  // setup output
-  auto cfg = spdif.defaultConfig();
-  cfg.pin_data = 23;
-  cfg.sample_rate = a2dp_sink.sample_rate();
-  cfg.channels = 2;
-  cfg.bits_per_sample = 16;
-  spdif.begin(cfg);
 
 }
 
