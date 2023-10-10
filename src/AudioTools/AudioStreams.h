a611570e93fe3ab8a0a522c9983f4e1c2a3e993e
Phil Schatzmann <phil.schatzmann@gmail.com>
2023-10-10 23:20:17 +0200

    vban support

--- src/AudioTools/AudioStreams.h
+++ src/AudioTools/AudioStreams.h
@@ -654,7 +654,7 @@ class GeneratedSoundStream : public AudioStream {
   }
 
   /// This is unbounded so we just return the buffer size
-  virtual int available() override { return DEFAULT_BUFFER_SIZE; }
+  virtual int available() override { return DEFAULT_BUFFER_SIZE*2; }
 
   /// privide the data as byte stream
   size_t readBytes(uint8_t *buffer, size_t length) override {
