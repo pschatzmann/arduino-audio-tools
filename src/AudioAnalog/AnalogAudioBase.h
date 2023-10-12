80b3a37fec416c2681d69b88e1d14be4d929fea5
pschatzmann <phil.schatzmann@gmail.com>
2023-10-12 11:33:21 +0200

    Arduino ESP32 3.0.0

--- src/AudioAnalog/AnalogAudioBase.h
+++ src/AudioAnalog/AnalogAudioBase.h
@@ -63,13 +63,13 @@ class AnalogConfig : public AudioInfo {
     int adc_attenuation = ADC_ATTEN_DB_0;
     int adc_bit_width = SOC_ADC_DIGI_MAX_BITWIDTH;
     #if CONFIG_IDF_TARGET_ESP32C3 || CONFIG_IDF_TARGET_ESP32S3 || CONFIG_IDF_TARGET_ESP32H2 || CONFIG_IDF_TARGET_ESP32C2
-    adc_channel_t channel[3] = {ADC_CHANNEL_2, ADC_CHANNEL_3, (ADC_CHANNEL_0 | 1 << 3)};
+    adc_channel_t adc_channels[3] = {ADC_CHANNEL_2, ADC_CHANNEL_3, (ADC_CHANNEL_0 | 1 << 3)};
     #endif
     #if CONFIG_IDF_TARGET_ESP32S2
-    adc_channel_t channel[3] = {ADC_CHANNEL_2, ADC_CHANNEL_3, (ADC_CHANNEL_0 | 1 << 3)};
+    adc_channel_t adc_channels[3] = {ADC_CHANNEL_2, ADC_CHANNEL_3, (ADC_CHANNEL_0 | 1 << 3)};
     #endif  
     #if CONFIG_IDF_TARGET_ESP32
-    adc_channel_t channel[1] = {ADC_CHANNEL_7};
+    adc_channel_t adc_channels[1] = {ADC_CHANNEL_7};
     #endif
 #endif
 
