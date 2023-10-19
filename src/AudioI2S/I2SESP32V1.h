8afa7f2f612bc5369154563e293f796b37f2f94b
Phil Schatzmann <phil.schatzmann@gmail.com>
2023-10-20 00:04:21 +0200

    I2S ESP32 Driver in Doxygen

--- src/AudioI2S/I2SESP32V1.h
+++ src/AudioI2S/I2SESP32V1.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "AudioConfig.h"
-#if defined(ESP32) && defined(USE_I2S) && ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 0, 0)
+#if defined(ESP32) && defined(USE_I2S) && ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 0, 0) || defined(DOXYGEN)
 
 #include "AudioI2S/I2SConfig.h"
 #include "driver/i2s_pdm.h"
