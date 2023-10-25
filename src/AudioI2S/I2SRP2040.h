343810d265feff3f061aba53b102ceb566607796
Phil Schatzmann <phil.schatzmann@gmail.com>
2023-10-25 13:45:59 +0200

    RP2040 Hower: support for MCLK

--- src/AudioI2S/I2SRP2040.h
+++ src/AudioI2S/I2SRP2040.h
@@ -68,6 +68,10 @@ class I2SDriverRP2040 {
         LOGE("Could not set data pin: %d", cfg.pin_data);
         return false;
       }
+      if (!i2s.setMCLK(cfg.pin_mck)){
+        LOGE("Could not set data pin: %d", cfg.pin_mck);
+        return false;
+      }
 
       if (cfg.bits_per_sample==8 || !i2s.setBitsPerSample(cfg.bits_per_sample)){
         LOGE("Could not set bits per sample: %d", cfg.bits_per_sample);
