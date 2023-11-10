86295404998dd4ea9c06845655bf7ab927e8b87a
pschatzmann <phil.schatzmann@gmail.com>
2023-11-10 09:36:41 +0100

    DRAFT BLE Arduino

--- src/Sandbox/BLE/AudioBLE.h
+++ src/Sandbox/BLE/AudioBLE.h
@@ -1,9 +1,12 @@
 #pragma once
 
 #ifdef ESP32
+//#  include "AudioBLEServer.h"
+//#  include "AudioBLEClient.h"
+ #  include "AudioBLEServerESP32.h"
+ #  include "AudioBLEClientESP32.h"
+#else
 #  include "AudioBLEServer.h"
 #  include "AudioBLEClient.h"
-#else
-#  error Device not supported
 #endif
 
