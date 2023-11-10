f86fc4e2b6b876feed8100656f12524c33513347
pschatzmann <phil.schatzmann@gmail.com>
2023-11-10 09:56:41 +0100

    base-i2s-a2dp.ino

--- examples/examples-audiokit/basic-audiokit-a2dp/basic-audiokit-a2dp.ino
+++ examples/examples-audiokit/basic-audiokit-a2dp/basic-audiokit-a2dp.ino
@@ -9,6 +9,7 @@
 #include "AudioLibs/AudioKit.h"
 #include "AudioLibs/AudioA2DP.h"
 
+AudioInfo info(44100, 2, 16);
 BluetoothA2DPSource a2dp_source;
 AudioKitStream i2s;
 const int16_t BYTES_PER_FRAME = 4;
@@ -27,9 +28,7 @@ void setup(void) {
   Serial.println("starting I2S...");
   auto cfg = i2s.defaultConfig(RX_MODE);
   cfg.i2s_format = I2S_STD_FORMAT; // or try with I2S_LSB_FORMAT
-  cfg.bits_per_sample = 16;
-  cfg.channels = 2;
-  cfg.sample_rate = 44100;
+  cfg.copyFrom(info);
   cfg.input_device = AUDIO_HAL_ADC_INPUT_LINE2; // microphone
   i2s.begin(cfg);
 
