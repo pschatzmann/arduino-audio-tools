7b8094a0de14a5afc38b7bddedf4d19df22debc8
pschatzmann <phil.schatzmann@gmail.com>
2024-02-11 19:43:21 +0100

    AudioActions strange character in source

--- src/AudioTools/AudioActions.h
+++ src/AudioTools/AudioActions.h
@@ -31,7 +31,7 @@ public:
   };
 
   struct Action {
-    int16_t pin = -1;
+    int16_t pin = -1;
     void (*actionOn)(bool pinStatus, int pin, void *ref) = nullptr;
     void (*actionOff)(bool pinStatus, int pin, void *ref) = nullptr;
     void *ref = nullptr;
