8c5cdb63c24a3472242ce485616aa75a90df27cf
Hendrik Langer <hendrik+dev@xd0.de>
2024-02-29 11:18:06 +0100

    move int24_t into namespace

--- src/AudioConfig.h
+++ src/AudioConfig.h
@@ -737,11 +737,13 @@ typedef WiFiClient WiFiClientSecure;
 // select int24 implementation
 #include "AudioBasic/Int24_3bytes_t.h"
 #include "AudioBasic/Int24_4bytes_t.h"
+namespace audio_tools {
 #ifdef USE_3BYTE_INT24
 using int24_t = audio_tools::int24_3bytes_t;
 #else
 using int24_t = audio_tools::int24_4bytes_t;
 #endif
+}
 
 #ifndef ESP32
 #define ESP_IDF_VERSION_VAL(a, b , c) 0
