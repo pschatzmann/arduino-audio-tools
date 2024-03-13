bc88a0f298b2c81e75609ba9472e2b4d184993b2
pschatzmann <phil.schatzmann@gmail.com>
2024-03-13 22:54:34 +0100

    Mozzi Example

---
+++ examples/examples-dsp/examples-mozzi/control_gain/control_gain.ino
@@ -0,0 +1,52 @@
+/**
+ * Demo how to use the Mozzi API to provide a stream on int16_t data.
+ * Inspired by https://sensorium.github.io/Mozzi/examples/#01.Basics
+ */
+#include "AudioTools.h"
+#include "AudioLibs/AudioBoardStream.h"
+#include "AudioLibs/MozziStream.h"
+#include <Oscil.h>                // oscillator template
+#include <tables/sin2048_int8.h>  // sine table for oscillator
+
+const int sample_rate = 16000;
+AudioInfo info(sample_rate, 1, 16);
+AudioBoardStream i2s(AudioKitEs8388V1);  // final output of decoded stream
+MozziStream mozzi;
+StreamCopy copier(i2s, mozzi);
+// use: Oscil <table_size, update_rate> oscilName (wavetable), look in .h file
+// of table #included above
+Oscil<SIN2048_NUM_CELLS, sample_rate> aSin(SIN2048_DATA);
+// control variable, use the smallest data size you can for anything used in
+// audio
+byte gain = 255;
+
+void setup() {
+  Serial.begin(115200);
+  AudioLogger::instance().begin(Serial, AudioLogger::Info);
+
+  // setup mozzi
+  auto cfg = mozzi.defaultConfig();
+  cfg.control_rate = CONTROL_RATE;
+  cfg.copyFrom(info);
+  mozzi.begin(cfg);
+
+  // setup output
+  auto out_cfg = i2s.defaultConfig();
+  out_cfg.copyFrom(info);
+  i2s.begin(out_cfg);
+
+  // setup mozzi sine
+  aSin.setFreq(3320);  // set the frequency
+}
+
+void loop() { copier.copy(); }
+
+void updateControl() {
+  // as byte, this will automatically roll around to 255 when it passes 0
+  gain = gain - 3;
+}
+
+int updateAudio() {
+  return (aSin.next() * gain) >>
+         8;  // shift back to STANDARD audio range, like /256 but faster
+}
---
+++ src/AudioLibs/MozziStream.h
@@ -0,0 +1,131 @@
+
+#include <MozziGuts.h>
+
+#include "AudioTools.h"
+
+void updateControl();
+int updateAudio();
+
+namespace audio_tools {
+
+struct MozziConfig : AudioInfo {
+  uint16_t control_rate = CONTROL_RATE;
+  int input_range_from = 0;
+  int input_range_to = 1023;
+  int16_t output_volume = 10;
+  // STANDARD modes this is between -244 and 243 inclusive
+};
+
+/**
+ * @brief Stream that provides audio information that was generated using the
+ * Mozzi API using the updateControl() and updateAudio() methods.
+ * @author Phil Schatzmann
+ * @copyright GPLv3
+ */
+class MozziStream : public AudioStream {
+ public:
+  MozziConfig defaultConfig() { return cfg; }
+  void setAudioInfo(AudioInfo info) { cfg.copyFrom(info); }
+  int audioRate() { return cfg.sample_rate; }
+  void setInput(Stream& in) { p_input = &in; }
+
+  AudioInfo audioInfo() { return cfg; }
+
+  bool begin(MozziConfig cfg) {
+    this->cfg = cfg;
+    return begin();
+  }
+
+  bool begin() {
+    if (cfg.bits_per_sample != 16){
+        return false;
+    }
+    control_counter_max = info.sample_rate / cfg.control_rate;
+    if (control_counter_max <= 0) {
+      control_counter_max = 1;
+    }
+    control_counter = control_counter_max;
+    active = true;
+    return true;
+  }
+
+  void end() {
+    active = false;
+  }
+
+  void setVolume(int16_t vol) { cfg.output_volume = vol; }
+
+  size_t readBytes(uint8_t* data, size_t byteCount) {
+    if (!active) return 0;
+    int samples = byteCount / sizeof(int16_t);
+    int frames = samples / cfg.channels;
+    int16_t* data16 = (int16_t*)data;
+    int idx = 0;
+    for (int j = 0; j < frames; j++) {
+      int16_t sample = nextSample();
+      for (int ch = 0; ch < cfg.channels; ch++) {
+        data16[idx++] = sample;
+      }
+    }
+    return idx * sizeof(int16_t);
+  }
+
+  /// Write data to buffer so that we can access them by calling getAudioInput()
+  size_t write(const uint8_t* data, size_t byteCount) {
+    if (!active) return 0;
+    if (buffer.size() == 0) {
+      buffer.resize(byteCount * 2);
+    }
+    return buffer.writeArray(data, byteCount);
+  }
+
+  /// Gets the next audio value either from the assigned Input Stream or the
+  /// buffer that was filled by write()
+  int getAudioInput() {
+    int16_t result[cfg.channels] = {0};
+    if (p_input != nullptr) {
+      p_input->readBytes((uint8_t*)&result, sizeof(int16_t) * cfg.channels);
+    } else {
+      buffer.readArray((uint8_t*)&result, sizeof(int16_t) * cfg.channels);
+    }
+    return map(avg(result), -32768, 32767, cfg.input_range_from,
+               cfg.input_range_to);
+  }
+
+ protected:
+  MozziConfig cfg;
+  int control_counter_max;
+  int control_counter;
+  RingBuffer<uint8_t> buffer{0};
+  Stream* p_input = nullptr;
+  bool active = false;
+
+  int16_t avg(int16_t* values) {
+    int total = 0;
+    for (int ch = 0; ch < cfg.channels; ch++) {
+      total += values[ch];
+    }
+    return total / cfg.channels;
+  }
+
+  int16_t nextSample() {
+    // control update
+    if (--control_counter < 0) {
+      control_counter = control_counter_max;
+      if (updateControl != nullptr) {
+        LOGD("updateControl");
+        updateControl();
+      }
+    }
+
+    // return left value
+    int result = updateAudio() * cfg.output_volume;
+    // clip result
+    if (result > 32767) result = 32767;
+    if (result < -32768) result = -32768;
+    //Serial.println(result);
+    return result;
+  }
+};
+
+}  // namespace audio_tools
