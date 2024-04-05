51482c63e193a9ba40485f1539c10648e22a3b81
pschatzmann <phil.schatzmann@gmail.com>
2024-04-05 09:31:44 +0200

    Reformat Timer

--- src/AudioTimer/AudioTimerBase.h
+++ src/AudioTimer/AudioTimerBase.h
@@ -4,45 +4,42 @@
 #include "AudioTools/AudioTypes.h"
 
 /**
- * @defgroup timer Timer 
+ * @defgroup timer Timer
  * @ingroup tools
  * @brief Timer
  */
 
 namespace audio_tools {
 
-typedef void (*repeating_timer_callback_t )(void* obj);
+typedef void (*repeating_timer_callback_t)(void* obj);
 
-enum TimerFunction {DirectTimerCallback, TimerCallbackInThread, SimpleThreadLoop };
-
-
-class TimerAlarmRepeatingDriverBase {
-    public:   
-        virtual ~TimerAlarmRepeatingDriverBase(){
-            end();
-        }
-     
-        virtual bool begin(repeating_timer_callback_t callback_f, uint32_t time, TimeUnit unit = MS) = 0;
-        virtual bool end() { return false;};
-
-        void setCallbackParameter(void* obj){
-          object = obj;
-        }
-
-        void *callbackParameter(){
-            return object;
-        }
-
-        virtual void setTimer(int timer){}
-        virtual void setTimerFunction(TimerFunction function=DirectTimerCallback){}
-        /// @brief Not used
-        virtual void setIsSave(bool is_save){}
-
-    protected:
-        void* object=nullptr;
+enum TimerFunction {
+  DirectTimerCallback,
+  TimerCallbackInThread,
+  SimpleThreadLoop
 };
 
-} // namespace
+class TimerAlarmRepeatingDriverBase {
+ public:
+  virtual ~TimerAlarmRepeatingDriverBase() { end(); }
+
+  virtual bool begin(repeating_timer_callback_t callback_f, uint32_t time,
+                     TimeUnit unit = MS) = 0;
+  virtual bool end() { return false; };
+
+  void setCallbackParameter(void* obj) { object = obj; }
+
+  void* callbackParameter() { return object; }
+
+  virtual void setTimer(int timer) {}
+  virtual void setTimerFunction(TimerFunction function = DirectTimerCallback) {}
+  /// @brief Not used
+  virtual void setIsSave(bool is_save) {}
+
+ protected:
+  void* object = nullptr;
+};
+
+}  // namespace audio_tools
 
 #endif
-
