cec73eec8a6da95c265b62468e4dfce06fa12738
Urs Utzinger <utzinger@email.arizona.edu>
2024-07-26 14:53:16 -0700

    ESP32 onboard ADC, multichannel sorted
    1-6 channels tested on ESP32 module
    other modules not tested yet

--- examples/tests/adc/read-esp32-multi-channel-csv/read-esp32-multi-channel-csv.ino
+++ examples/tests/adc/read-esp32-multi-channel-csv/read-esp32-multi-channel-csv.ino
@@ -3,35 +3,26 @@
  * @author Urs Utzinger
  * @copyright GPLv3
  */
- 
+
+// | attenuation     | range   | accurate range |
+// | ------------    | --------| -------------- |
+// | ADC_ATTEN_DB_0  | 0..1.1V | 100-950mV      |
+// | ADC_ATTEN_DB_2_5| 0..1.5V | 100-1250mV     |
+// | ADC_ATTEN_DB_6  | 0..2.2V | 150-1750mV     |
+// | ADC_ATTEN_DB_12 | 0..3.9V | 150-2450mV     |
+
 #include "Arduino.h"
 #include "AudioTools.h"
 
-#define DEC_FACTOR 96                           // Decimating
-
-#define NUM_CHANNELS 1                          // Only 1 and 2 channels work, results from 3 amd more are returned unordered
-#define ADC_ATTENUATION ADC_ATTEN_DB_11         // ADC attenuation, see below
+#define NUM_CHANNELS 6                          // number of channels
+#define DEC_FACTOR 48                           // decimating
+#define ADC_ATTENUATION ADC_ATTEN_DB_11         // ADC attenuation, see above
 #define ADC_BIT_WIDTH 12                        // the ADC bit width 9..12, values will be stored in 16 bit integers
-
 #define BIT_DEPTH 16                            // default bit width of data returned from analgo_in. Do not change.
 
-//#channels: 8, 7, 6, 5, 4, 3,   2,  1
-// returned
-//        0 48 49 49 95 95 144 240  NA
-//        1 48 47 NA NA NA  NA  NA  NA
-//        2 48 NA NA NA NA  NA  NA  NA
-//        3 48 48 48 96 96 144  NA  NA
-//        4 47 48 47 96 NA  NA  NA  NA
-//        5 47 48 48 NA NA  NA  NA  NA
-//        6 49 48 47 97 97  NA  NA  NA
-//        7 49 48 49 96 96 144 240 480
-// ordered?  U  U  U  U  U   U   O   O
-
-// It looks like maximum buffer size into stream copy is 1024
-// If we specify larger buffer size it defaults to 1024 and the converters no longer work 
-//   as buffer size needs to be divisible by num channels
-#define BUFFER_SIZE_FACTOR (1024 / (NUM_CHANNELS*DEC_FACTOR*BIT_DEPTH/8))
-#define ADC_BUFFER_SIZE (NUM_CHANNELS*DEC_FACTOR*BUFFER_SIZE_FACTOR) // Total number of samples (not bytes), needs to be divisible by 4
+// It looks like Buffer larger than 1024 is capped by stream copy for decimater or csvoutput
+#define BUFFER_SIZE_FACTOR (1024/ (NUM_CHANNELS*DEC_FACTOR*BIT_DEPTH/8)) // 3.5
+#define ADC_BUFFER_SIZE (NUM_CHANNELS*DEC_FACTOR*BUFFER_SIZE_FACTOR) // Total number of samples (not bytes), needs to be divisible by 4 // 3*3*96 = 864
 #define BUFFER_SIZE ADC_BUFFER_SIZE*BIT_DEPTH/8 // Number of bytes for processing buffer
 #define SERIAL_BUFFER_SIZE BUFFER_SIZE/DEC_FACTOR   // There will be factor times less data after decimating
 
@@ -49,17 +40,12 @@
 #define ADC5 ADC_CHANNEL_5 // ADC channel 5
 #define ADC6 ADC_CHANNEL_1 // ADC channel 6, not routed on the ESP32 WROOM module
 #define ADC7 ADC_CHANNEL_2 // ADC channel 7, not routed on ESP32 WROOM module
-
-// | attenuation     | range   | accurate range |
-// | ------------    | --------| -------------- |
-// | ADC_ATTEN_DB_0  | 0..1.1V | 100-950mV      |
-// | ADC_ATTEN_DB_2_5| 0..1.5V | 100-1250mV     |
-// | ADC_ATTEN_DB_6  | 0..2.2V | 150-1750mV     |
-// | ADC_ATTEN_DB_12 | 0..3.9V | 150-2450mV     |
+#define ADC8 ADC_CHANNEL_8 // ADC channel 8, only ESP32S3,S2
+#define ADC9 ADC_CHANNEL_9 // ADC channel 9, only ESP32S3,S2
 
 AudioInfo                     info_serial(SERIAL_SAMPLE_RATE, NUM_CHANNELS, BIT_DEPTH);
 
-AnalogAudioStream             analog_in;  // on board analog to digital converter
+AnalogAudioStream             analog_in; // on board analog to digital converter
 Decimate                      decimater(DEC_FACTOR, NUM_CHANNELS, BIT_DEPTH); // skip samples
 ConverterStream<int16_t>      decimated_stream(analog_in, decimater); // pipe the sound to the decimater
 CsvOutput<int16_t>            serial_out(Serial, NUM_CHANNELS, SERIAL_BUFFER_SIZE); // serial output
@@ -81,7 +67,7 @@ void setup(void) {
   adcConfig.sample_rate = SAMPLE_RATE;                          // sample rate per channel
   adcConfig.channels = NUM_CHANNELS;                            // up to 6 channels
   adcConfig.adc_bit_width = ADC_BIT_WIDTH;                      // Supports only 12
-  adcConfig.buffer_size = ADC_BUFFER_SIZE;                      // in tital number of samples
+  adcConfig.buffer_size = ADC_BUFFER_SIZE;                      // in total number of samples
   adcConfig.adc_calibration_active = true;                      // use and apply the calibration settings of ADC stored in ESP32
   adcConfig.is_auto_center_read = false;                        // do not engage auto-centering of signal (would subtract average of signal)
   adcConfig.adc_attenuation = ADC_ATTENUATION;                  // set analog input range
