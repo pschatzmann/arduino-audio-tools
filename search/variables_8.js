0a5d90d3c5da8eee035162be4b40574ed0dcaf7e
pschatzmann <phil.schatzmann@gmail.com>
2024-09-01 03:20:20 +0000

    Deploying to doxygen from @ pschatzmann/arduino-audio-tools@6cbeb35f703bca73291169089392e92ec738ddb6 🚀

--- search/variables_8.js
+++ search/variables_8.js
@@ -1,8 +1,9 @@
 var searchData=
 [
-  ['max_5fbandwidth_3161',['max_bandwidth',['../structaudio__tools_1_1_opus_encoder_settings.html#ad8cfdd5ef4a0e393aa271992b518dfaa',1,'audio_tools::OpusEncoderSettings']]],
-  ['max_5fmagnitude_3162',['max_magnitude',['../structaudio__tools_1_1_l_e_d_output_config.html#a3ecf65cb448be5006c7ca263a96fd974',1,'audio_tools::LEDOutputConfig::max_magnitude()'],['../structaudio__tools_1_1_l_e_d_output_uno_r4_config.html#a3ecf65cb448be5006c7ca263a96fd974',1,'audio_tools::LEDOutputUnoR4Config::max_magnitude()']]],
-  ['maxdelaybuffersize_3163',['maxDelayBufferSize',['../classaudio__tools_1_1_delay_effect_base.html#aa11da42ff6171a564f70a1911c3953b4',1,'audio_tools::DelayEffectBase']]],
+  ['max_5fbandwidth_3160',['max_bandwidth',['../structaudio__tools_1_1_opus_encoder_settings.html#ad8cfdd5ef4a0e393aa271992b518dfaa',1,'audio_tools::OpusEncoderSettings']]],
+  ['max_5fmagnitude_3161',['max_magnitude',['../structaudio__tools_1_1_l_e_d_output_config.html#a3ecf65cb448be5006c7ca263a96fd974',1,'audio_tools::LEDOutputConfig::max_magnitude()'],['../structaudio__tools_1_1_l_e_d_output_uno_r4_config.html#a3ecf65cb448be5006c7ca263a96fd974',1,'audio_tools::LEDOutputUnoR4Config::max_magnitude()']]],
+  ['maxdelaybuffersize_3162',['maxDelayBufferSize',['../classaudio__tools_1_1_delay_effect_base.html#aa11da42ff6171a564f70a1911c3953b4',1,'audio_tools::DelayEffectBase']]],
+  ['mclk_5fmultiple_3163',['mclk_multiple',['../classaudio__tools_1_1_i2_s_config_e_s_p32_v1.html#aa967219ba3bef5dce6848fa19ffa1fa1',1,'audio_tools::I2SConfigESP32V1']]],
   ['mime_5fpcm_3164',['mime_pcm',['../namespaceaudio__tools.html#a3961afb9c2a414e91d5687346be60285',1,'audio_tools']]],
   ['modmax_3165',['modMax',['../classaudio__tools_1_1_simple_chorus.html#a91cfe04b1a76f7511690aecbb9ddbc31',1,'audio_tools::SimpleChorus']]],
   ['modmin_3166',['modMin',['../classaudio__tools_1_1_simple_chorus.html#aeeb2bf6f51ec4293a9e158b34d14680d',1,'audio_tools::SimpleChorus']]],
