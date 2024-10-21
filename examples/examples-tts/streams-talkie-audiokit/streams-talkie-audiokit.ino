4d62a3861f212644a7b396cb74e76786e97d30c6
pschatzmann <phil.schatzmann@gmail.com>
2024-10-21 14:08:26 +0200

    Talkie example

---
+++ examples/examples-tts/streams-talkie-audiokit/streams-talkie-audiokit.ino
@@ -0,0 +1,40 @@
+/**
+ * @file streams-talkie-audiokit.ino
+ * We use the TalkiePCM TTS library to generate the audio
+ * You need to install https://github.com/pschatzmann/TalkiePCM
+ * @author Phil Schatzmann
+ * @copyright GPLv3
+ */
+
+#include "AudioTools.h"
+#include "AudioTools/AudioLibs/AudioBoardStream.h" //https://github.com/pschatzmann/arduino-audio-driver
+#include "TalkiePCM.h" // https://github.com/pschatzmann/TalkiePCM
+#include "Vocab_US_Large.h"
+
+const AudioInfo info(8000, 2, 16);
+AudioBoardStream out(AudioKitEs8388V1);  // Audio sink
+//CsvOutput<int16_t> out(Serial);
+TalkiePCM voice(out, info.channels);
+
+void setup() {
+  Serial.begin(115200);
+  AudioLogger::instance().begin(Serial, AudioLogger::Info);
+  // setup AudioKit
+  auto cfg = out.defaultConfig();
+  cfg.copyFrom(info);
+  out.begin(cfg);
+  
+  Serial.println("Talking...");
+}
+
+void loop() {
+  voice.say(sp2_DANGER);
+  voice.say(sp2_DANGER);
+  voice.say(sp2_RED);
+  voice.say(sp2_ALERT);
+  voice.say(sp2_MOTOR);
+  voice.say(sp2_IS);
+  voice.say(sp2_ON);
+  voice.say(sp2_FIRE);
+  voice.silence(1000);
+}
