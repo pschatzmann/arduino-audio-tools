e93dab88c866061326db3e2cf7437c00ae6e8f87
pschatzmann <phil.schatzmann@gmail.com>
2025-02-20 09:35:12 +0100

    FFTDriverRealFFT: psram support

--- src/AudioTools/AudioLibs/AudioRealFFT.h
+++ src/AudioTools/AudioLibs/AudioRealFFT.h
@@ -21,35 +21,33 @@ class FFTDriverRealFFT : public FFTDriver {
     public:
         bool begin(int len) override {
             this->len = len;
+            v_x.resize(len);
+            v_f.resize(len);
             if (p_fft_object==nullptr) p_fft_object = new ffft::FFTReal<float>(len);
-            if (p_x==nullptr) p_x = new float[len];
-            if (p_f==nullptr) p_f = new float[len];
             assert(p_fft_object!=nullptr);
-            assert(p_x!=nullptr);
-            assert(p_f!=nullptr);
-            return p_fft_object!=nullptr && p_x!=nullptr && p_f!=nullptr;
+            return p_fft_object!=nullptr;
         }
         void end()override{
-            if (p_fft_object!=nullptr) delete p_fft_object;
-            if (p_x!=nullptr) delete[] p_x;
-            if (p_f!=nullptr) delete[] p_f;
-            p_fft_object = nullptr;
-            p_x = nullptr;
-            p_f = nullptr;
+            if (p_fft_object!=nullptr) {
+                delete p_fft_object;
+                p_fft_object = nullptr;
+            } 
+            v_x.resize(0);
+            v_f.resize(0);
         }
         void setValue(int idx, float value) override{
-            p_x[idx] = value; 
+            v_x[idx] = value; 
         }
 
         void fft() override{
-            memset(p_f,0,len*sizeof(float));
-            p_fft_object->do_fft(p_f, p_x);    
+            memset(v_f.data(),0,len*sizeof(float));
+            p_fft_object->do_fft(v_f.data(), v_x.data());    
         };
 
         /// Inverse fft - convert fft result back to time domain (samples)
         void rfft() override{
            // ifft
-           p_fft_object->do_ifft(p_f, p_x);
+           p_fft_object->do_ifft(v_f.data(), v_x.data());
         }
 
         bool isReverseFFT() override { return true;}
@@ -60,30 +58,30 @@ class FFTDriverRealFFT : public FFTDriver {
 
         /// magnitude w/o sqrt
         float magnitudeFast(int idx) override {
-            return (p_x[idx] * p_x[idx] + p_f[idx] * p_f[idx]);
+            return (v_x[idx] * v_x[idx] + v_f[idx] * v_f[idx]);
         }
 
         bool isValid() override{ return p_fft_object!=nullptr; }
 
         /// get Real value
-        float getValue(int idx) override { return p_x[idx];}
+        float getValue(int idx) override { return v_x[idx];}
 
         bool setBin(int pos, float real, float img) override {
             if (pos>=len) return false;
-            p_x[pos] = real;
-            p_f[pos] = img;
+            v_x[pos] = real;
+            v_f[pos] = img;
             return true;
         }
         bool getBin(int pos, FFTBin &bin) override { 
             if (pos>=len) return false;
-            bin.real = p_x[pos];
-            bin.img = p_f[pos];
+            bin.real = v_x[pos];
+            bin.img = v_f[pos];
             return true;
         }
 
         ffft::FFTReal <float> *p_fft_object=nullptr;
-        float *p_x = nullptr; // real
-        float *p_f = nullptr; // complex
+        Vector<float> v_x{0}; // real
+        Vector<float> v_f{0}; // complex
         int len;
 
 };
