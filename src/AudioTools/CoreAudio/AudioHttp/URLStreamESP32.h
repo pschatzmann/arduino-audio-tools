fd9bcb804a24bdbbf02dc581b16ae364f6936ffe
pschatzmann <phil.schatzmann@gmail.com>
2025-03-04 10:58:47 +0100

    URLStreamESP32 remove ESP_LOGI

--- src/AudioTools/CoreAudio/AudioHttp/URLStreamESP32.h
+++ src/AudioTools/CoreAudio/AudioHttp/URLStreamESP32.h
@@ -332,29 +332,29 @@ class URLStreamESP32 : public AbstractURLStream {
   static esp_err_t http_event_handler(esp_http_client_event_t* evt) {
     switch (evt->event_id) {
       case HTTP_EVENT_ERROR:
-        ESP_LOGI(TAG, "HTTP_EVENT_ERROR");
+        LOGI("HTTP_EVENT_ERROR");
         break;
       case HTTP_EVENT_ON_CONNECTED:
-        ESP_LOGI(TAG, "HTTP_EVENT_ON_CONNECTED");
+        LOGI("HTTP_EVENT_ON_CONNECTED");
         break;
       case HTTP_EVENT_HEADER_SENT:
-        ESP_LOGI(TAG, "HTTP_EVENT_HEADER_SENT");
+        LOGI("HTTP_EVENT_HEADER_SENT");
         break;
       case HTTP_EVENT_ON_HEADER:
-        ESP_LOGI(TAG, "HTTP_EVENT_ON_HEADER, key=%s, value=%s", evt->header_key,
+        LOGI("HTTP_EVENT_ON_HEADER, key=%s, value=%s", evt->header_key,
                  evt->header_value);
         break;
       case HTTP_EVENT_ON_DATA:
-        ESP_LOGI(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
+        LOGI("HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
         break;
       case HTTP_EVENT_ON_FINISH:
-        ESP_LOGI(TAG, "HTTP_EVENT_ON_FINISH");
+        LOGI("HTTP_EVENT_ON_FINISH");
         break;
       case HTTP_EVENT_DISCONNECTED:
-        ESP_LOGI(TAG, "HTTP_EVENT_DISCONNECTED");
+        LOGI("HTTP_EVENT_DISCONNECTED");
         break;
       case HTTP_EVENT_REDIRECT:
-        ESP_LOGI(TAG, "HTTP_EVENT_REDIRECT");
+        LOGI("HTTP_EVENT_REDIRECT");
         break;
     }
     return ESP_OK;
