e5ca627aeeedebcd6abd984e0773291e0d0f3d67
pschatzmann <phil.schatzmann@gmail.com>
2025-03-07 21:15:55 +0100

    Correct AudioLogger to AudioLoggerSTD

--- src/AudioTools/CoreAudio/AudioLogger.h
+++ src/AudioTools/CoreAudio/AudioLogger.h
@@ -3,5 +3,5 @@
 #if defined(USE_IDF_LOGGER)
 #  include "AudioTools/CoreAudio/AudioLoggerIDF.h"
 #else
-#  include "AudioTools/CoreAudio/AudioLogger.h"
+#  include "AudioTools/CoreAudio/AudioLoggerSTD.h"
 #endif
