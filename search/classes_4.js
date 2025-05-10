80927aafd133768c96cbf4cf10bfddad08fb3c3b
pschatzmann <phil.schatzmann@gmail.com>
2025-05-10 20:40:29 +0000

    Deploying to doxygen from @ pschatzmann/arduino-audio-tools@be1207ed94a3672fb961b7c286b0a5e05955be58 🚀

--- search/classes_4.js
+++ search/classes_4.js
@@ -8,11 +8,12 @@ var searchData=
   ['encoderfloat_5',['EncoderFloat',['../classaudio__tools_1_1_encoder_float.html',1,'audio_tools']]],
   ['encoderl16_6',['EncoderL16',['../classaudio__tools_1_1_encoder_l16.html',1,'audio_tools']]],
   ['encoderl8_7',['EncoderL8',['../classaudio__tools_1_1_encoder_l8.html',1,'audio_tools']]],
-  ['envelopefilter_8',['EnvelopeFilter',['../classaudio__tools_1_1_envelope_filter.html',1,'audio_tools']]],
-  ['eqstate_9',['EQSTATE',['../structaudio__tools_1_1_equalizer3_bands_1_1_e_q_s_t_a_t_e.html',1,'audio_tools::Equalizer3Bands']]],
-  ['equalizer3bands_10',['Equalizer3Bands',['../classaudio__tools_1_1_equalizer3_bands.html',1,'audio_tools']]],
-  ['esp3288audiooutput_11',['ESP3288AudioOutput',['../classaudio__tools_1_1_e_s_p3288_audio_output.html',1,'audio_tools']]],
-  ['espnowstream_12',['ESPNowStream',['../classaudio__tools_1_1_e_s_p_now_stream.html',1,'audio_tools']]],
-  ['espnowstreamconfig_13',['ESPNowStreamConfig',['../structaudio__tools_1_1_e_s_p_now_stream_config.html',1,'audio_tools']]],
-  ['exponentialvolumecontrol_14',['ExponentialVolumeControl',['../classaudio__tools_1_1_exponential_volume_control.html',1,'audio_tools']]]
+  ['encodernetworkformat_8',['EncoderNetworkFormat',['../classaudio__tools_1_1_encoder_network_format.html',1,'audio_tools']]],
+  ['envelopefilter_9',['EnvelopeFilter',['../classaudio__tools_1_1_envelope_filter.html',1,'audio_tools']]],
+  ['eqstate_10',['EQSTATE',['../structaudio__tools_1_1_equalizer3_bands_1_1_e_q_s_t_a_t_e.html',1,'audio_tools::Equalizer3Bands']]],
+  ['equalizer3bands_11',['Equalizer3Bands',['../classaudio__tools_1_1_equalizer3_bands.html',1,'audio_tools']]],
+  ['esp3288audiooutput_12',['ESP3288AudioOutput',['../classaudio__tools_1_1_e_s_p3288_audio_output.html',1,'audio_tools']]],
+  ['espnowstream_13',['ESPNowStream',['../classaudio__tools_1_1_e_s_p_now_stream.html',1,'audio_tools']]],
+  ['espnowstreamconfig_14',['ESPNowStreamConfig',['../structaudio__tools_1_1_e_s_p_now_stream_config.html',1,'audio_tools']]],
+  ['exponentialvolumecontrol_15',['ExponentialVolumeControl',['../classaudio__tools_1_1_exponential_volume_control.html',1,'audio_tools']]]
 ];
