7a1bd5f9a80d6d8e170ce556f7611571a330847d
pschatzmann <phil.schatzmann@gmail.com>
2025-05-13 10:50:14 +0200

    Delete aoo

--- src/AudioTools/Communication/AOO.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include "AOO/OSCData.h"
-#include "AOO/AOOSource.h"
-#include "AOO/AOOSink.h"
--- src/AudioTools/Communication/AOO/aoo-protocol.md
+++
@@ -1,2 +0,0 @@
-
-see https://git.iem.at/aoo/aoo/-/blob/cmake_update/doc/aoo_protocol.md
