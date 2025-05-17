55e07f4f83e0b6471eb68993c3ebf8f14ef94010
pschatzmann <phil.schatzmann@gmail.com>
2025-05-17 07:32:30 +0000

    Deploying to doxygen from @ pschatzmann/arduino-audio-tools@c17300874d3612f2641729f58dbdab1bfc169ff7 🚀

--- search/variables_10.js
+++ search/variables_10.js
@@ -5,5 +5,6 @@ var searchData=
   ['window_5ffunction_2',['window_function',['../structaudio__tools_1_1_audio_f_f_t_config.html#a844cd7e00ad0a62016fe513da4d3bf73',1,'audio_tools::AudioFFTConfig']]],
   ['window_5ffunction_5ffft_3',['window_function_fft',['../structaudio__tools_1_1_audio_f_f_t_config.html#a54b93a0d60348be00e5ce74a590619bb',1,'audio_tools::AudioFFTConfig']]],
   ['window_5ffunction_5fifft_4',['window_function_ifft',['../structaudio__tools_1_1_audio_f_f_t_config.html#a239482ff42a24a69a1c1a530da81a291',1,'audio_tools::AudioFFTConfig']]],
-  ['wire_5',['wire',['../classaudio__tools_1_1_w_m8960_config.html#ab8d8f9af97a698a5a73132c347acbaf4',1,'audio_tools::WM8960Config']]]
+  ['wire_5',['wire',['../classaudio__tools_1_1_w_m8960_config.html#ab8d8f9af97a698a5a73132c347acbaf4',1,'audio_tools::WM8960Config']]],
+  ['write_5fcallback_6',['write_callback',['../classaudio__tools_1_1_o_s_c_container_decoder.html#a5b09408634ce84ec8287c57848c967a1',1,'audio_tools::OSCContainerDecoder']]]
 ];
