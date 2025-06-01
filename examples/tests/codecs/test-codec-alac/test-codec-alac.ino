5d657ca4dce51c679dae3c1b758e2b588ff499dd
Phil Schatzmann <phil.schatzmann@gmail.com>
2025-06-01 20:05:05 +0200

    Update test-codec-alac.ino

--- examples/tests/codecs/test-codec-alac/test-codec-alac.ino
+++ examples/tests/codecs/test-codec-alac/test-codec-alac.ino
@@ -12,7 +12,7 @@
 #include "AudioTools/AudioCodecs/CodecALAC.h"
 #include "AudioTools/AudioLibs/AudioBoardStream.h"
 
-SET_LOOP_TASK_STACK_SIZE(16*1024); // 16KB
+// SET_LOOP_TASK_STACK_SIZE(16*1024); // 16KB - not needed
 
 AudioInfo info(44100, 2, 16);
 SineWaveGenerator<int16_t> sineWave( 32000);  // subclass of SoundGenerator with max amplitude of 32000
