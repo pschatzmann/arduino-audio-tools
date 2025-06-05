4701804fe9c44189441d6943fcd14afe9261b514
pschatzmann <phil.schatzmann@gmail.com>
2025-06-05 09:38:52 +0000

    Deploying to doxygen from @ pschatzmann/arduino-audio-tools@a11f33d879ced1731fc3afab7c4fdda507d3cafc 🚀

--- search/variables_d.js
+++ search/variables_d.js
@@ -3,7 +3,7 @@ var searchData=
   ['tableindex_0',['tableIndex',['../classaudio__tools_1_1_modulation_base_class.html#a368304108e41965fa62932f878a4cba1',1,'audio_tools::ModulationBaseClass']]],
   ['target_5fip_1',['target_ip',['../classaudio__tools_1_1_v_b_a_n_config.html#a8e3be7119cfc230626838ce6cecd32cc',1,'audio_tools::VBANConfig']]],
   ['timer_5fid_2',['timer_id',['../structaudio__tools_1_1_p_w_m_config.html#a2f5be4ef54ee5ab425bdbd31d44307ec',1,'audio_tools::PWMConfig']]],
-  ['timestamp_3',['timestamp',['../structaudio__tools_1_1_m4_a_audio_demuxer_1_1_frame.html#a465bef81f6478756e5443025b1f2ddfa',1,'audio_tools::M4AAudioDemuxer::Frame::timestamp'],['../classaudio__tools_1_1_single_buffer.html#a465bef81f6478756e5443025b1f2ddfa',1,'audio_tools::SingleBuffer::timestamp']]],
+  ['timestamp_3',['timestamp',['../classaudio__tools_1_1_single_buffer.html#a465bef81f6478756e5443025b1f2ddfa',1,'audio_tools::SingleBuffer']]],
   ['timestep_4',['timestep',['../classaudio__tools_1_1_modulation_base_class.html#ac80cb97c3e6b8eebe58efacba3b93351',1,'audio_tools::ModulationBaseClass::timeStep'],['../classaudio__tools_1_1_simple_flanger.html#ac80cb97c3e6b8eebe58efacba3b93351',1,'audio_tools::SimpleFlanger::timeStep']]],
   ['to_5fsample_5frate_5',['to_sample_rate',['../structaudio__tools_1_1_resample_config.html#af736bd115bcd9ab90219d854b57642a5',1,'audio_tools::ResampleConfig']]],
   ['tx_5fwrite_5ftimeout_5fms_6',['tx_write_timeout_ms',['../classaudio__tools_1_1_a2_d_p_config.html#a5c89ecff28a57beb306a551107a119bf',1,'audio_tools::A2DPConfig']]],
