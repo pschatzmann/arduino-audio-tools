81f558e7b710530d84343249809a50bebac1a3be
pschatzmann <phil.schatzmann@gmail.com>
2025-06-08 22:03:05 +0000

    Deploying to doxygen from @ pschatzmann/arduino-audio-tools@9cdefc10e7fc11522e476d31310954cee40e0f84 🚀

--- search/searchdata.js
+++ search/searchdata.js
@@ -5,7 +5,7 @@ var indexSectionsWithContent =
   2: "afr",
   3: "acfn",
   4: "_abcdefghiklmnopqrstuvw~",
-  5: "abcdefhiklmnprstuvwxy",
+  5: "abcdefhiklmnoprstuvwxy",
   6: "aipstu",
   7: "abcfgimprt",
   8: "dm",
--- search/variables_d.js
+++ search/variables_d.js
@@ -1,13 +1,13 @@
 var searchData=
 [
-  ['rate_0',['rate',['../structaudio__tools_1_1_e_s_p_now_stream_config.html#add99443712e271bc9d643939e9d1eab7',1,'audio_tools::ESPNowStreamConfig']]],
-  ['read_5fbuf_1',['read_buf',['../classaudio__tools_1_1_redis_buffer.html#a8f477b56ca38ff5ca75ae68638660fac',1,'audio_tools::RedisBuffer']]],
-  ['ref_2',['ref',['../classaudio__tools_1_1_m4_a_audio_demuxer.html#a1830ddb36c14bdd7057683e633f918c8',1,'audio_tools::M4AAudioDemuxer::ref'],['../classaudio__tools_1_1_m4_a_common_demuxer_1_1_sample_extractor.html#a1830ddb36c14bdd7057683e633f918c8',1,'audio_tools::M4ACommonDemuxer::SampleExtractor::ref'],['../classaudio__tools_1_1_m_p4_parser.html#a1830ddb36c14bdd7057683e633f918c8',1,'audio_tools::MP4Parser::ref'],['../structaudio__tools_1_1_audio_f_f_t_config.html#a1830ddb36c14bdd7057683e633f918c8',1,'audio_tools::AudioFFTConfig::ref']]],
-  ['resolution_3',['resolution',['../structaudio__tools_1_1_p_w_m_config.html#a2b8bdf2ccdee1cc9e95e8ec7714b68d2',1,'audio_tools::PWMConfig']]],
-  ['rmsbuffer_4',['rmsBuffer',['../classaudio__tools_1_1_filter_effect_base.html#af98d43bb141b7271517441be89776234',1,'audio_tools::FilterEffectBase']]],
-  ['rmsbufferindex_5',['rmsBufferIndex',['../classaudio__tools_1_1_filter_effect_base.html#aaa8caba16ab06d8a73dd5a2e4c31d4dc',1,'audio_tools::FilterEffectBase']]],
-  ['rmswindowsize_6',['rmsWindowSize',['../classaudio__tools_1_1_filter_effect_base.html#ab547c2a820df89e5452b595bf9a17df5',1,'audio_tools::FilterEffectBase']]],
-  ['rx_5ftx_5fmode_7',['rx_tx_mode',['../classaudio__tools_1_1_i2_s_config_e_s_p32.html#a31a454d24fb1f4d74779a0ff668d54cc',1,'audio_tools::I2SConfigESP32::rx_tx_mode'],['../classaudio__tools_1_1_i2_s_config_e_s_p32_v1.html#a31a454d24fb1f4d74779a0ff668d54cc',1,'audio_tools::I2SConfigESP32V1::rx_tx_mode'],['../classaudio__tools_1_1_i2_s_config_std.html#a31a454d24fb1f4d74779a0ff668d54cc',1,'audio_tools::I2SConfigStd::rx_tx_mode']]],
-  ['rxtx_5fmode_8',['rxtx_mode',['../structaudio__tools_1_1_audio_f_f_t_config.html#af885d3ab35bb6dd1535598c22ff03f30',1,'audio_tools::AudioFFTConfig']]],
-  ['rxtxmodenames_9',['RxTxModeNames',['../namespaceaudio__tools.html#a488e5410a2317335bc89c65de90ab41b',1,'audio_tools']]]
+  ['p_5fdecoder_0',['p_decoder',['../classaudio__tools_1_1_container_m4_a.html#aa7ee53cf73384ae988c7e017179c310f',1,'audio_tools::ContainerM4A::p_decoder'],['../classaudio__tools_1_1_m4_a_audio_file_demuxer.html#aa7ee53cf73384ae988c7e017179c310f',1,'audio_tools::M4AAudioFileDemuxer::p_decoder']]],
+  ['p_5ffile_1',['p_file',['../classaudio__tools_1_1_m4_a_audio_file_demuxer.html#a7dc54ed1a07e49ad60b979f495b84c7b',1,'audio_tools::M4AAudioFileDemuxer::p_file'],['../classaudio__tools_1_1_m4_a_file_sample_size_buffer.html#a7dc54ed1a07e49ad60b979f495b84c7b',1,'audio_tools::M4AFileSampleSizeBuffer::p_file']]],
+  ['p_5fplayer_2',['p_player',['../classaudio__tools_1_1_m4_a_file_sample_size_buffer.html#a86181e15c42b1c583c2cfeab471720d5',1,'audio_tools::M4AFileSampleSizeBuffer']]],
+  ['packet_5floss_5fperc_3',['packet_loss_perc',['../structaudio__tools_1_1_opus_encoder_settings.html#a56381d5e2c16a04fd6d0490b29135539',1,'audio_tools::OpusEncoderSettings']]],
+  ['parseoffset_4',['parseOffset',['../classaudio__tools_1_1_m_p4_parser.html#abfc68c30812c96bcb9c193d6da6e4c83',1,'audio_tools::MP4Parser']]],
+  ['parser_5',['parser',['../classaudio__tools_1_1_m4_a_common_demuxer.html#a03ad2e167852db1649c0f9bfe701aa74',1,'audio_tools::M4ACommonDemuxer']]],
+  ['password_6',['password',['../classaudio__tools_1_1_v_b_a_n_config.html#aa4a2ebcb494493f648ae1e6975672575',1,'audio_tools::VBANConfig']]],
+  ['prediction_5fdisabled_7',['prediction_disabled',['../structaudio__tools_1_1_opus_encoder_settings.html#ad0efc56dbc533dc88f5b9eb78d7068af',1,'audio_tools::OpusEncoderSettings']]],
+  ['primary_5fmaster_5fkey_8',['primary_master_key',['../structaudio__tools_1_1_e_s_p_now_stream_config.html#a4574b7c5bb954f02512bab0493b16955',1,'audio_tools::ESPNowStreamConfig']]],
+  ['pwm_5ffrequency_9',['pwm_frequency',['../structaudio__tools_1_1_p_w_m_config.html#a82eb78ef27e47691d2f08609806b6732',1,'audio_tools::PWMConfig']]]
 ];
