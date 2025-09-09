d11a5506eb69a095491660161ebb121e4e3c2804
pschatzmann <phil.schatzmann@gmail.com>
2025-09-09 18:56:07 +0000

    Deploying to doxygen from @ pschatzmann/arduino-audio-tools@2f8833ff6f59134bd74d85f2fbfe6061e4bfb6c6 🚀

--- search/variables_10.js
+++ search/variables_10.js
@@ -4,11 +4,12 @@ var searchData=
   ['rbuffer_1',['rbuffer',['../classaudio__tools_1_1_decoder_adapter.html#a03b0402f34be88d0b7d4d0a06a9f364f',1,'audio_tools::DecoderAdapter']]],
   ['read_5fbuf_2',['read_buf',['../classaudio__tools_1_1_redis_buffer.html#a8f477b56ca38ff5ca75ae68638660fac',1,'audio_tools::RedisBuffer']]],
   ['ref_3',['ref',['../classaudio__tools_1_1_m4_a_audio_demuxer.html#a1830ddb36c14bdd7057683e633f918c8',1,'audio_tools::M4AAudioDemuxer::ref'],['../classaudio__tools_1_1_m4_a_common_demuxer_1_1_sample_extractor.html#a1830ddb36c14bdd7057683e633f918c8',1,'audio_tools::M4ACommonDemuxer::SampleExtractor::ref'],['../classaudio__tools_1_1_m_p4_parser.html#a1830ddb36c14bdd7057683e633f918c8',1,'audio_tools::MP4Parser::ref'],['../structaudio__tools_1_1_audio_f_f_t_config.html#a1830ddb36c14bdd7057683e633f918c8',1,'audio_tools::AudioFFTConfig::ref'],['../classaudio__tools_1_1_goertzel_stream.html#a1830ddb36c14bdd7057683e633f918c8',1,'audio_tools::GoertzelStream::ref']]],
-  ['resolution_4',['resolution',['../structaudio__tools_1_1_p_w_m_config.html#a2b8bdf2ccdee1cc9e95e8ec7714b68d2',1,'audio_tools::PWMConfig']]],
-  ['rmsbuffer_5',['rmsBuffer',['../classaudio__tools_1_1_filter_effect_base.html#af98d43bb141b7271517441be89776234',1,'audio_tools::FilterEffectBase']]],
-  ['rmsbufferindex_6',['rmsBufferIndex',['../classaudio__tools_1_1_filter_effect_base.html#aaa8caba16ab06d8a73dd5a2e4c31d4dc',1,'audio_tools::FilterEffectBase']]],
-  ['rmswindowsize_7',['rmsWindowSize',['../classaudio__tools_1_1_filter_effect_base.html#ab547c2a820df89e5452b595bf9a17df5',1,'audio_tools::FilterEffectBase']]],
-  ['rx_5ftx_5fmode_8',['rx_tx_mode',['../classaudio__tools_1_1_i2_s_config_e_s_p32.html#a31a454d24fb1f4d74779a0ff668d54cc',1,'audio_tools::I2SConfigESP32::rx_tx_mode'],['../classaudio__tools_1_1_i2_s_config_e_s_p32_v1.html#a31a454d24fb1f4d74779a0ff668d54cc',1,'audio_tools::I2SConfigESP32V1::rx_tx_mode'],['../classaudio__tools_1_1_i2_s_config_std.html#a31a454d24fb1f4d74779a0ff668d54cc',1,'audio_tools::I2SConfigStd::rx_tx_mode']]],
-  ['rxtx_5fmode_9',['rxtx_mode',['../structaudio__tools_1_1_audio_f_f_t_config.html#af885d3ab35bb6dd1535598c22ff03f30',1,'audio_tools::AudioFFTConfig']]],
-  ['rxtxmodenames_10',['RxTxModeNames',['../namespaceaudio__tools.html#a488e5410a2317335bc89c65de90ab41b',1,'audio_tools']]]
+  ['references_4',['references',['../classaudio__tools_1_1_goertzel_stream.html#a78ca1d0fc73b52f1052e1a0a6d1ce02e',1,'audio_tools::GoertzelStream']]],
+  ['resolution_5',['resolution',['../structaudio__tools_1_1_p_w_m_config.html#a2b8bdf2ccdee1cc9e95e8ec7714b68d2',1,'audio_tools::PWMConfig']]],
+  ['rmsbuffer_6',['rmsBuffer',['../classaudio__tools_1_1_filter_effect_base.html#af98d43bb141b7271517441be89776234',1,'audio_tools::FilterEffectBase']]],
+  ['rmsbufferindex_7',['rmsBufferIndex',['../classaudio__tools_1_1_filter_effect_base.html#aaa8caba16ab06d8a73dd5a2e4c31d4dc',1,'audio_tools::FilterEffectBase']]],
+  ['rmswindowsize_8',['rmsWindowSize',['../classaudio__tools_1_1_filter_effect_base.html#ab547c2a820df89e5452b595bf9a17df5',1,'audio_tools::FilterEffectBase']]],
+  ['rx_5ftx_5fmode_9',['rx_tx_mode',['../classaudio__tools_1_1_i2_s_config_e_s_p32.html#a31a454d24fb1f4d74779a0ff668d54cc',1,'audio_tools::I2SConfigESP32::rx_tx_mode'],['../classaudio__tools_1_1_i2_s_config_e_s_p32_v1.html#a31a454d24fb1f4d74779a0ff668d54cc',1,'audio_tools::I2SConfigESP32V1::rx_tx_mode'],['../classaudio__tools_1_1_i2_s_config_std.html#a31a454d24fb1f4d74779a0ff668d54cc',1,'audio_tools::I2SConfigStd::rx_tx_mode']]],
+  ['rxtx_5fmode_10',['rxtx_mode',['../structaudio__tools_1_1_audio_f_f_t_config.html#af885d3ab35bb6dd1535598c22ff03f30',1,'audio_tools::AudioFFTConfig']]],
+  ['rxtxmodenames_11',['RxTxModeNames',['../namespaceaudio__tools.html#a488e5410a2317335bc89c65de90ab41b',1,'audio_tools']]]
 ];
