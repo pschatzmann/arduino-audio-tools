9d605cd00643977468c1050f85ee3f59afbd11a1
pschatzmann <phil.schatzmann@gmail.com>
2025-09-11 06:00:52 +0200

    RTSPFormatADPCM

--- src/AudioTools/AudioLibs/RTSP.h
+++ src/AudioTools/AudioLibs/RTSP.h
@@ -25,7 +25,7 @@ namespace audio_tools {
  * @copyright GPLv3
  */
 class RTSPOutputPCMInfo : public PCMInfo {
-public:
+ public:
   RTSPOutputPCMInfo() = default;
   virtual void begin(AudioStream &stream) { p_stream = &stream; }
   int getSampleRate() override { return p_stream->audioInfo().sample_rate; }
@@ -35,7 +35,7 @@ public:
   };
   virtual void setAudioInfo(AudioInfo ai) { p_stream->setAudioInfo(ai); }
 
-protected:
+ protected:
   AudioStream *p_stream = nullptr;
 };
 
@@ -48,7 +48,7 @@ protected:
  */
 
 class RTSPPCMAudioInfo : public PCMInfo {
-public:
+ public:
   RTSPPCMAudioInfo() = default;
   virtual void begin(AudioInfo info) { this->info = info; }
   int getSampleRate() override { return info.sample_rate; }
@@ -56,7 +56,7 @@ public:
   int getSampleSizeBytes() override { return info.bits_per_sample / 8; };
   virtual void setAudioInfo(AudioInfo ai) { info = ai; }
 
-protected:
+ protected:
   AudioInfo info;
 };
 
@@ -70,7 +70,7 @@ protected:
  * @copyright GPLv3
  */
 class RTSPSourceFromAudioStream : public IAudioSource {
-public:
+ public:
   RTSPSourceFromAudioStream() = default;
   /**
    * @brief Construct a new RTSPOutputSource object from an AudioStream
@@ -154,7 +154,7 @@ public:
   /// Returns true after start() has been called.
   bool isStarted() { return started; }
 
-protected:
+ protected:
   AudioStream *p_audiostream = nullptr;
   uint32_t time_of_last_read = 0;
   bool started = true;
@@ -172,7 +172,7 @@ protected:
  * @copyright GPLv3
  */
 class RTSPSourceStream : public IAudioSource {
-public:
+ public:
   /**
    * @brief Construct a new RTSPOutputSource object from an Arduino Stream
    * You need to provide the audio information by calling setAudioInfo()
@@ -244,7 +244,7 @@ public:
 
   void setTimerPeriod(int period) { format.setTimerPeriod(period); }
 
-protected:
+ protected:
   Stream *p_stream = nullptr;
   bool active = true;
   RTSPPCMAudioInfo rtp_info;
@@ -257,30 +257,29 @@ protected:
  * @author Phil Schatzmann
  */
 class RTSPFormatAudioTools : public RTSPFormat {
-public:
+ public:
   virtual void begin(AudioInfo info) { cfg = info; }
   /// Provides the format string
   virtual const char *format(char *buffer, int len) = 0;
   /// Provide a default format that will just work
   virtual AudioInfo defaultConfig() = 0;
   /// Proides the name
-  const char* name() {
-    return name_str;
-  }
+  const char *name() { return name_str; }
   /// Defines the  name
-  void setName(const char* name){
-    name_str = name;
-  }
+  void setName(const char *name) { name_str = name; }
 
-  void setFragmentSize(int fragmentSize) { RTSPFormat::setFragmentSize(fragmentSize);}
+  void setFragmentSize(int fragmentSize) {
+    RTSPFormat::setFragmentSize(fragmentSize);
+  }
   int fragmentSize() { return RTSPFormat::fragmentSize(); }
 
   void setTimerPeriod(int period) { RTSPFormat::setTimerPeriod(period); }
   int timerPeriod() { return RTSPFormat::timerPeriod(); }
 
-protected:
+ protected:
   AudioInfo cfg;
-  const char* name_str="RTSP-Demo";;
+  const char *name_str = "RTSP-Demo";
+  ;
 };
 
 /**
@@ -290,7 +289,7 @@ protected:
  * @author Phil Schatzmann
  */
 class RTSPFormatOpus : public RTSPFormatAudioTools {
-public:
+ public:
   // Provides the Opus format information:
   //  m=audio 54312 RTP/AVP 101
   //  a=rtpmap:101 opus/48000/2
@@ -298,10 +297,10 @@ public:
   const char *format(char *buffer, int len) override {
     TRACEI();
     snprintf(buffer, len,
-             "s=%s\r\n"     // Stream Name
-             "c=IN IP4 0.0.0.0\r\n" // Connection Information
-             "t=0 0\r\n" // start / stop - 0 -> unbounded and permanent session
-             "m=audio 0 RTP/AVP 101\r\n" // UDP sessions with format 101=opus
+             "s=%s\r\n"              // Stream Name
+             "c=IN IP4 0.0.0.0\r\n"  // Connection Information
+             "t=0 0\r\n"  // start / stop - 0 -> unbounded and permanent session
+             "m=audio 0 RTP/AVP 101\r\n"  // UDP sessions with format 101=opus
              "a=rtpmap:101 opus/%d/2\r\n"
              "a=fmtp:101 stereo=1; sprop-stereo=%d\r\n",
              name(), cfg.sample_rate, cfg.channels == 2);
@@ -320,7 +319,7 @@ public:
  * @author Phil Schatzmann
  */
 class RTSPFormatAbtX : public RTSPFormatAudioTools {
-public:
+ public:
   // Provides the SBC format information:
   //    m=audio 5004 RTP/AVP 98
   //    a=rtpmap:98 aptx/44100/2
@@ -328,10 +327,10 @@ public:
   const char *format(char *buffer, int len) override {
     TRACEI();
     snprintf(buffer, len,
-             "s=%s\r\n"     // Stream Name
-             "c=IN IP4 0.0.0.0\r\n" // Connection Information
-             "t=0 0\r\n" // start / stop - 0 -> unbounded and permanent session
-             "m=audio 0 RTP/AVP 98\r\n" // UDP sessions with format 98=aptx
+             "s=%s\r\n"              // Stream Name
+             "c=IN IP4 0.0.0.0\r\n"  // Connection Information
+             "t=0 0\r\n"  // start / stop - 0 -> unbounded and permanent session
+             "m=audio 0 RTP/AVP 98\r\n"  // UDP sessions with format 98=aptx
              "a=rtpmap:98 aptx/%d/%d\r\n"
              "a=fmtp:98 variant=standard; bitresolution=%d\r\n",
              name(), cfg.sample_rate, cfg.channels, cfg.bits_per_sample);
@@ -350,7 +349,7 @@ public:
  * @author Phil Schatzmann
  */
 class RTSPFormatGSM : public RTSPFormatAudioTools {
-public:
+ public:
   /// Provides the GSM format information
   const char *format(char *buffer, int len) override {
     TRACEI();
@@ -358,11 +357,12 @@ public:
     assert(cfg.channels == 1);
 
     snprintf(buffer, len,
-             "s=%s\r\n"     // Stream Name
-             "c=IN IP4 0.0.0.0\r\n" // Connection Information
-             "t=0 0\r\n" // start / stop - 0 -> unbounded and permanent session
-             "m=audio 0 RTP/AVP 3\r\n" // UDP sessions with format 3=GSM
-             , name());
+             "s=%s\r\n"              // Stream Name
+             "c=IN IP4 0.0.0.0\r\n"  // Connection Information
+             "t=0 0\r\n"  // start / stop - 0 -> unbounded and permanent session
+             "m=audio 0 RTP/AVP 3\r\n"  // UDP sessions with format 3=GSM
+             ,
+             name());
     return (const char *)buffer;
   }
 
@@ -380,19 +380,21 @@ public:
  * @author Phil Schatzmann
  */
 class RTSPFormatG711 : public RTSPFormatAudioTools {
-public:
+ public:
   /// Provides the G711  format information
   const char *format(char *buffer, int len) override {
     TRACEI();
     assert(cfg.sample_rate == 8000);
     assert(cfg.channels == 1);
 
-    snprintf(buffer, len,
-             "s=%s\r\n"     // Stream Name
-             "c=IN IP4 0.0.0.0\r\n" // Connection Information
-             "t=0 0\r\n" // start / stop - 0 -> unbounded and permanent session
-             "m=audio 0 RTP/AVP %d\r\n" // UDP sessions with format 0=G711 μ-Law
-             , name(), getFormat());
+    snprintf(
+        buffer, len,
+        "s=%s\r\n"              // Stream Name
+        "c=IN IP4 0.0.0.0\r\n"  // Connection Information
+        "t=0 0\r\n"  // start / stop - 0 -> unbounded and permanent session
+        "m=audio 0 RTP/AVP %d\r\n"  // UDP sessions with format 0=G711 μ-Law
+        ,
+        name(), getFormat());
     return (const char *)buffer;
   }
 
@@ -404,7 +406,7 @@ public:
     return cfg;
   }
 
-protected:
+ protected:
   bool is_ulaw = true;
   uint8_t getFormat() { return is_ulaw ? 0 : 8; }
 };
@@ -416,19 +418,19 @@ protected:
  * @author Phil Schatzmann
  */
 class RTSPFormatPCM : public RTSPFormatAudioTools {
-public:
+ public:
   /// Provides the GSM format information
   const char *format(char *buffer, int len) override {
     TRACEI();
     snprintf(buffer, len,
-             "s=%s\r\n"     // Stream Name
-             "c=IN IP4 0.0.0.0\r\n" // Connection Information
-             "t=0 0\r\n" // start / stop - 0 -> unbounded and permanent session
-             "m=audio 0 RTP/AVP %d\r\n" // UDP sessions with format 10 or 11
+             "s=%s\r\n"              // Stream Name
+             "c=IN IP4 0.0.0.0\r\n"  // Connection Information
+             "t=0 0\r\n"  // start / stop - 0 -> unbounded and permanent session
+             "m=audio 0 RTP/AVP %d\r\n"  // UDP sessions with format 10 or 11
              "a=rtpmap:%s\r\n"
-             "a=rate:%i\r\n", // provide sample rate
-             name(), format(cfg.channels), payloadFormat(cfg.sample_rate, cfg.channels),
-             cfg.sample_rate);
+             "a=rate:%i\r\n",  // provide sample rate
+             name(), format(cfg.channels),
+             payloadFormat(cfg.sample_rate, cfg.channels), cfg.sample_rate);
     return (const char *)buffer;
   }
 
@@ -437,22 +439,22 @@ public:
     return cfg;
   }
 
-protected:
+ protected:
   char payload_fromat[30];
 
   // format for mono is 11 and stereo 11
   int format(int channels) {
     int result = 0;
     switch (channels) {
-    case 1:
-      result = 11;
-      break;
-    case 2:
-      result = 10;
-      break;
-    default:
-      LOGE("unsupported audio type");
-      break;
+      case 1:
+        result = 11;
+        break;
+      case 2:
+        result = 10;
+        break;
+      default:
+        LOGE("unsupported audio type");
+        break;
     }
     return result;
   }
@@ -462,17 +464,17 @@ protected:
     // 11 L16/%i/%i
 
     switch (channels) {
-    case 1:
-      snprintf(payload_fromat, 30, "%d L16/%i/%i", format(channels), sampleRate,
-               channels);
-      break;
-    case 2:
-      snprintf(payload_fromat, 30, "%d L16/%i/%i", format(channels), sampleRate,
-               channels);
-      break;
-    default:
-      LOGE("unsupported audio type");
-      break;
+      case 1:
+        snprintf(payload_fromat, 30, "%d L16/%i/%i", format(channels),
+                 sampleRate, channels);
+        break;
+      case 2:
+        snprintf(payload_fromat, 30, "%d L16/%i/%i", format(channels),
+                 sampleRate, channels);
+        break;
+      default:
+        LOGE("unsupported audio type");
+        break;
     }
     return payload_fromat;
   }
@@ -485,20 +487,71 @@ protected:
  * @author Phil Schatzmann
  */
 class RTSPFormatPCM8 : public RTSPFormatAudioTools {
-public:
+ public:
   const char *format(char *buffer, int len) override {
     TRACEI();
     snprintf(buffer, len,
-             "s=%s\r\n"     // Stream Name
-             "c=IN IP4 0.0.0.0\r\n" // Connection Information
-             "t=0 0\r\n" // start / stop - 0 -> unbounded and permanent session
-             "m=audio 0 RTP/AVP 96\r\n" // UDP sessions with format 96=dynamic
+             "s=%s\r\n"              // Stream Name
+             "c=IN IP4 0.0.0.0\r\n"  // Connection Information
+             "t=0 0\r\n"  // start / stop - 0 -> unbounded and permanent session
+             "m=audio 0 RTP/AVP 96\r\n"  // UDP sessions with format 96=dynamic
              "a=rtpmap:96 l8/%d/%d\r\n",
              name(), cfg.sample_rate, cfg.channels);
     return (const char *)buffer;
   }
   AudioInfo defaultConfig() {
     AudioInfo cfg(16000, 2, 8);
+    return cfg;
+  }
+};
+
+/**
+ * @brief ADPCM format for RTSP for the following mono
+ * sample rates: 8000, 16000, 11025, 22050
+ * https://en.wikipedia.org/wiki/RTP_payload_formats
+ * @ingroup rtsp
+ * @author Phil Schatzmann
+ */
+
+class RTSPFormatADPCM : public RTSPFormatAudioTools {
+ public:
+  // Provides the IMA ADPCM format information
+  // See RFC 3551 for details
+  const char *format(char *buffer, int len) override {
+    TRACEI();
+    // Only certain sample rates are valid for DVI4 (IMA ADPCM)
+    int sr = cfg.sample_rate;
+    int payload_type = 5;  // default to 8000 Hz
+    switch (sr) {
+      case 8000:
+        payload_type = 5;
+        break;
+      case 16000:
+        payload_type = 6;
+        break;
+      case 11025:
+        payload_type = 16;
+        break;
+      case 22050:
+        payload_type = 17;
+        break;
+      default:
+        LOGE("Unsupported sample rate for IMA ADPCM: %d", sr);
+        sr = 8000;
+        payload_type = 5;
+        break;
+    }
+    snprintf(buffer, len,
+             "s=%s\r\n"
+             "c=IN IP4 0.0.0.0\r\n"
+             "t=0 0\r\n"
+             "m=audio 0 RTP/AVP %d\r\n"
+             "a=rtpmap:%d DVI4/%d\r\n",
+             name(), payload_type, payload_type, sr);
+    return (const char *)buffer;
+  }
+  AudioInfo defaultConfig() {
+    AudioInfo cfg(22050, 1, 16);  // 4 bits per sample for IMA ADPCM
     return cfg;
   }
 };
@@ -514,7 +567,7 @@ public:
  * @author Phil Schatzmann
  */
 class RTSPOutput : public AudioOutput {
-public:
+ public:
   /// Default constructor
   RTSPOutput(RTSPFormatAudioTools &format, AudioEncoder &encoder,
              int buffer_size = 1024 * 2) {
@@ -587,7 +640,7 @@ public:
   /// @brief Returns true if the server has been started
   operator bool() { return rtps_source.isActive(); }
 
-protected:
+ protected:
   RTSPFormatPCM pcm;
   CopyEncoder copy_encoder;
   RTSPSourceFromAudioStream rtps_source;
@@ -598,5 +651,4 @@ protected:
   AudioStreamer rtsp_streamer;
 };
 
-
-} // namespace audio_tools
+}  // namespace audio_tools
