9a0fcb5997acc930ae3e2403e7ded4a0bffd20c6
pschatzmann <phil.schatzmann@gmail.com>
2025-09-26 16:05:32 +0000

    Deploying to doxygen from @ pschatzmann/arduino-audio-tools@4555762362afbfdf93a80509df5d1fddfbbb95d6 🚀

--- search/typedefs_7.js
+++ search/typedefs_7.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['icystream_0',['ICYStream',['../namespaceaudio__tools.html#ae1e819ad54e9849657902fb4f16f1f6d',1,'audio_tools']]],
-  ['icystreambuffered_1',['ICYStreamBuffered',['../namespaceaudio__tools.html#a57e84d2864ae61125b50e5d92f4dbbbd',1,'audio_tools']]],
+  ['icystream_0',['ICYStream',['../group__http.html#gae1e819ad54e9849657902fb4f16f1f6d',1,'audio_tools']]],
+  ['icystreambuffered_1',['ICYStreamBuffered',['../group__http.html#ga57e84d2864ae61125b50e5d92f4dbbbd',1,'audio_tools']]],
   ['icystreambufferedesp32_2',['ICYStreamBufferedESP32',['../namespaceaudio__tools.html#aedd4adeef1b5c5ab6dac8a0d5cd9539d',1,'audio_tools']]],
   ['icystreamesp32_3',['ICYStreamESP32',['../namespaceaudio__tools.html#a1a091fa31d0e6108398c90c8a8d28b2a',1,'audio_tools']]]
 ];
