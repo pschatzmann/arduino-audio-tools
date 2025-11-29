463f822a713029e108798bfbc54e9b5520f99107
pschatzmann <phil.schatzmann@gmail.com>
2025-11-29 11:34:46 +0100

    README

--- examples/examples-custom-boards/esp32-s3-ai-smart-speaker/player-sdfat-audiokit/player-sdfat-audiokit.ino
+++ examples/examples-custom-boards/esp32-s3-ai-smart-speaker/player-sdfat-audiokit/player-sdfat-audiokit.ino
@@ -1,9 +1,13 @@
 /**
  * @file player-sdfat-audiokit.ino
  * @brief SDFAT Audio Player example for ESP32-S3-AI-Smart-Speaker.
- * Unfortunatly the cd pins is mapped to EXIO4 and not an regualr GPIO pin.
- * This requires custom functions for SdCsInit and SdCsWrite. Make sure that
+ * Unfortunatly the SD cd pin is mapped to EXIO4 and not to a regular GPIO pin.
+ * This requires the custom functions SdCsInit and SdCsWrite. Make sure that
  * SD_CHIP_SELECT_MODE is set to 1 in SdFatConfig.h.
+ * 
+ * This makes sure that the SD is only active when it is actually used, so that
+ * we can extend the battery life!
+ * 
  * @author Phil Schatzmann
  * @copyright GPLv3
  */
@@ -21,10 +25,10 @@ AudioBoardStream kit(ESP32S3AISmartSpeaker);
 MP3DecoderHelix decoder;  // or change to MP3DecoderMAD
 AudioPlayer player(source, kit, decoder);
 
-// do nothing: pins are setup by the baord driver
+// SDFAT: do nothing: pins are setup by the baord driver
 void sdCsInit(SdCsPin_t pin) {}
 
-// activate/deactivate the CS pin
+// SDFAT: activate/deactivate the CS pin
 void sdCsWrite(SdCsPin_t pin, bool level) {
   kit.digitalWrite(EXIO4, level);
 }
