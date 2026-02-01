4d2f017a2a9b7bcda4953a955f56b94cb2581f5a
pschatzmann <phil.schatzmann@gmail.com>
2026-02-01 11:10:36 +0000

    Deploying to doxygen from @ pschatzmann/arduino-audio-tools@4b1fa7aa10971aebb33436c395a03eb89aaf6a43 🚀

--- search/typedefs_0.js
+++ search/typedefs_0.js
@@ -1,11 +1,12 @@
 var searchData=
 [
-  ['analogdriver_0',['AnalogDriver',['../namespaceaudio__tools.html#a91ca8c40ca55c1fbc577d0fa03ba527d',1,'audio_tools']]],
-  ['audioencoderserver_1',['AudioEncoderServer',['../group__http.html#ga0ec3485cc168907bfdf42103103b02ee',1,'audio_tools']]],
-  ['audioencoderserverwifi_2',['AudioEncoderServerWiFi',['../group__http.html#gab70ab5baaadaf10b5e83b9dd31afa88d',1,'audio_tools']]],
-  ['audioserver_3',['AudioServer',['../group__http.html#ga8e38f3ee8ba28b641249c88aa73a4edf',1,'audio_tools']]],
-  ['audioserverdatacallback_4',['AudioServerDataCallback',['../group__http.html#ga2909db5107c19b77138ce5e5a8653f0a',1,'audio_tools']]],
-  ['audioserverwifi_5',['AudioServerWiFi',['../group__http.html#ga3598c31c8a690f0d72d3521fb3e62732',1,'audio_tools']]],
-  ['audiowavserver_6',['AudioWAVServer',['../group__http.html#ga3a34ebd4add1aca368a9b999382ce45f',1,'audio_tools']]],
-  ['audiowavserverwifi_7',['AudioWAVServerWiFi',['../group__http.html#ga3ca40e5329e32b4609dbc179ce8a3255',1,'audio_tools']]]
+  ['activitycallback_0',['ActivityCallback',['../namespaceaudio__tools.html#aa85953fbc6fc993e340559bf2024d2a8',1,'audio_tools']]],
+  ['analogdriver_1',['AnalogDriver',['../namespaceaudio__tools.html#a91ca8c40ca55c1fbc577d0fa03ba527d',1,'audio_tools']]],
+  ['audioencoderserver_2',['AudioEncoderServer',['../group__http.html#ga0ec3485cc168907bfdf42103103b02ee',1,'audio_tools']]],
+  ['audioencoderserverwifi_3',['AudioEncoderServerWiFi',['../group__http.html#gab70ab5baaadaf10b5e83b9dd31afa88d',1,'audio_tools']]],
+  ['audioserver_4',['AudioServer',['../group__http.html#ga8e38f3ee8ba28b641249c88aa73a4edf',1,'audio_tools']]],
+  ['audioserverdatacallback_5',['AudioServerDataCallback',['../group__http.html#ga2909db5107c19b77138ce5e5a8653f0a',1,'audio_tools']]],
+  ['audioserverwifi_6',['AudioServerWiFi',['../group__http.html#ga3598c31c8a690f0d72d3521fb3e62732',1,'audio_tools']]],
+  ['audiowavserver_7',['AudioWAVServer',['../group__http.html#ga3a34ebd4add1aca368a9b999382ce45f',1,'audio_tools']]],
+  ['audiowavserverwifi_8',['AudioWAVServerWiFi',['../group__http.html#ga3ca40e5329e32b4609dbc179ce8a3255',1,'audio_tools']]]
 ];
