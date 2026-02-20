0f59a4cbc534b318497983442a2a62b6f56ac6c0
pschatzmann <phil.schatzmann@gmail.com>
2026-02-20 19:01:57 +0000

    Deploying to doxygen from @ pschatzmann/arduino-audio-tools@006c87dc79893d03a73f8fb70b37f4704a95bd6d 🚀

--- search/variables_e.js
+++ search/variables_e.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['ok_0',['ok',['../structaudio__tools_1_1_redis_buffer_1_1_redis_result.html#a1d67bd6634a9002df97231d8c8dbc66c',1,'audio_tools::RedisBuffer::RedisResult']]]
+  ['ok_0',['ok',['../structaudio__tools_1_1_redis_buffer_1_1_redis_result.html#a1d67bd6634a9002df97231d8c8dbc66c',1,'audio_tools::RedisBuffer::RedisResult']]],
+  ['oui_1',['oui',['../structaudio__tools_1_1_e_s_p_now_stream_config.html#aba6724ce067b3e9c93b0f13c0e0a3f49',1,'audio_tools::ESPNowStreamConfig']]]
 ];
