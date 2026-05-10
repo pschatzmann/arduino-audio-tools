fecfe00147d7fc282f0cac5322fcc49a62d3b07f
pschatzmann <phil.schatzmann@gmail.com>
2026-05-10 21:35:56 +0200

    InputMixer test with 19 FastSineGenerator

--- examples/examples-audiokit/streams-generator_inputmixer-audiokit/streams-generator_inputmixer-audiokit.ino
+++ examples/examples-audiokit/streams-generator_inputmixer-audiokit/streams-generator_inputmixer-audiokit.ino
@@ -1,33 +1,34 @@
 /**
  * @file streams-generator_inputmixer-audiokit.ino
- * @brief Tesing input mixer
+ * @brief Tesing input mixer: On a ESP32 we can mix max 19
+ * FastSineGenerator inputs together
  * @author Phil Schatzmann
  * @copyright GPLv3
  */
- 
+
 #include "AudioTools.h"
 #include "AudioTools/AudioLibs/AudioBoardStream.h"
 
-constexpr int N = 11;
+constexpr int N = 19;
 AudioInfo info(44100, 2, 16);
 constexpr float amplitude = 32000.0f;
-float notes[] = {N_B4, N_E4, N_G4, N_B5, N_E5, N_G5, N_B6, N_E6 };
-SineWaveGenerator<int16_t> sineWaves[N];
+float notes[] = {N_B4, N_E4, N_G4, N_B5, N_E5, N_G5, N_B6, N_E6};
+FastSineGenerator<int16_t> sineWaves[N];
 GeneratedSoundStream<int16_t> sounds[N];
 InputMixer<int16_t, int> mixer;
 AudioBoardStream out(AudioKitEs8388V1);
-StreamCopy copier(out, mixer);                             // copies sound into i2s
+StreamCopy copier(out, mixer);
 
 // Arduino Setup
-void setup(void) {  
-  // Open Serial 
+void setup(void) {
+  // Open Serial
   Serial.begin(115200);
   AudioToolsLogger.begin(Serial, AudioToolsLogLevel::Warning);
 
   // start I2S
   Serial.println("starting I2S...");
   auto config = out.defaultConfig(TX_MODE);
-  config.copyFrom(info); 
+  config.copyFrom(info);
   out.begin(config);
 
   // Setup sine waves
@@ -42,7 +43,5 @@ void setup(void) {
   Serial.println("started...");
 }
 
-// Arduino loop - copy sound to out 
-void loop() {
-  copier.copy();
-}
+// Arduino loop - copy sound to out
+void loop() { copier.copy(); }
