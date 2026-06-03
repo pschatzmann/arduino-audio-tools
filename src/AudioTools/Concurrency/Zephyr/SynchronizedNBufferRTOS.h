d489ff9c0f55839c969c0f1e85b9cae2c9e169a0
pschatzmann <phil.schatzmann@gmail.com>
2026-06-03 09:10:26 +0200

    Concurrency

---
+++ src/AudioTools/Concurrency/Zephyr/SynchronizedNBufferRTOS.h
@@ -0,0 +1,120 @@
+#pragma once
+
+#include "AudioTools/Concurrency/Zephyr/QueueRTOS.h"
+#include "AudioToolsConfig.h"
+
+#define DEFAULT_BUFFER_WAIT 1000
+
+namespace audio_tools {
+
+/**
+ * @brief NBuffer using RTOS queues for available and filled buffers.
+ * @ingroup buffers
+ * @ingroup concurrency
+ */
+template <typename T>
+class SynchronizedNBufferRTOST : public NBuffer<T> {
+ public:
+  SynchronizedNBufferRTOST(int bufferSize, int bufferCount,
+                           int writeMaxWait = DEFAULT_BUFFER_WAIT,
+                           int readMaxWait = DEFAULT_BUFFER_WAIT) {
+    read_max_wait = readMaxWait;
+    write_max_wait = writeMaxWait;
+    resize(bufferSize, bufferCount);
+  }
+
+  ~SynchronizedNBufferRTOST() { cleanup(); }
+
+  bool resize(int bufferSize, int bufferCount) {
+    if (buffer_size == bufferSize && buffer_count == bufferCount) {
+      return true;
+    }
+
+    max_size = bufferSize * bufferCount;
+    NBuffer<T>::buffer_count = bufferCount;
+    NBuffer<T>::buffer_size = bufferSize;
+
+    cleanup();
+    available_buffers.resize(bufferCount);
+    filled_buffers.resize(bufferCount);
+
+    setReadMaxWait(read_max_wait);
+    setWriteMaxWait(write_max_wait);
+
+    // setup buffers
+    for (int j = 0; j < bufferCount; j++) {
+      BaseBuffer<T> *tmp = new SingleBuffer<T>(bufferSize);
+      if (tmp != nullptr) {
+        available_buffers.enqueue(tmp);
+      } else {
+        LOGE("Not Enough Memory for buffer %d", j);
+        return false;
+      }
+    }
+    return true;
+  }
+
+  void setReadMaxWait(TickType_t ticks) {
+    available_buffers.setReadMaxWait(ticks);
+    filled_buffers.setReadMaxWait(ticks);
+  }
+
+  void setWriteMaxWait(TickType_t ticks) {
+    available_buffers.setWriteMaxWait(ticks);
+    filled_buffers.setWriteMaxWait(ticks);
+  }
+
+  size_t size() { return max_size; }
+
+  int bufferCountFilled() { return filled_buffers.size(); }
+
+  int bufferCountEmpty() { return available_buffers.size(); }
+
+ protected:
+  QueueRTOS<BaseBuffer<T> *> available_buffers{0, DEFAULT_BUFFER_WAIT, 0};
+  QueueRTOS<BaseBuffer<T> *> filled_buffers{0, DEFAULT_BUFFER_WAIT, 0};
+  size_t max_size = 0;
+  size_t read_max_wait = DEFAULT_BUFFER_WAIT;
+  size_t write_max_wait = DEFAULT_BUFFER_WAIT;
+  int buffer_size = 0;
+  int buffer_count = 0;
+
+  /// Removes all allocated buffers
+  void cleanup() {
+    BaseBuffer<T> *buffer = nullptr;
+    while (available_buffers.dequeue(buffer)) {
+      delete buffer;
+    }
+    while (filled_buffers.dequeue(buffer)) {
+      delete buffer;
+    }
+  }
+
+  BaseBuffer<T> *getNextAvailableBuffer() {
+    BaseBuffer<T> *result;
+    return available_buffers.dequeue(result) ? result : nullptr;
+  }
+
+  bool addAvailableBuffer(BaseBuffer<T> *buffer) {
+    return available_buffers.enqueue(buffer);
+  }
+
+  BaseBuffer<T> *getNextFilledBuffer() {
+    BaseBuffer<T> *result;
+    return filled_buffers.dequeue(result) ? result : nullptr;
+  }
+
+  bool addFilledBuffer(BaseBuffer<T> *buffer) {
+    return filled_buffers.enqueue(buffer);
+  }
+};
+
+/// @brief RTOS synchronized buffer for managing multiple audio buffers
+/// @ingroup buffers
+using SynchronizedNBufferRTOS = SynchronizedNBufferRTOST<uint8_t>;
+
+/// @brief Default synchronized buffer alias
+/// @ingroup buffers
+using SynchronizedNBuffer = SynchronizedNBufferRTOS;
+
+}  // namespace audio_tools
