7b184def1ca6eae0ac127f58639a627c648e33f6
pschatzmann <phil.schatzmann@gmail.com>
2026-06-04 10:00:51 +0200

    Logging

--- src/AudioTools/CoreAudio/AudioLogger.h
+++ src/AudioTools/CoreAudio/AudioLogger.h
@@ -1,9 +1,9 @@
 #pragma once
 #include "AudioToolsConfig.h"
-#if defined(IS_ZEPHYR)
-#  include "AudioTools/CoreAudio/AudioLoggerZephyr.h"
+#if defined(USE_ZEPHYR_LOGGER)
+#  include "AudioTools/CoreAudio/AudioLogger/AudioLoggerZephyr.h"
 #elif defined(USE_IDF_LOGGER)
-#  include "AudioTools/CoreAudio/AudioLoggerIDF.h"
+#  include "AudioTools/CoreAudio/AudioLogger/AudioLoggerIDF.h"
 #else
-#  include "AudioTools/CoreAudio/AudioLoggerSTD.h"
+#  include "AudioTools/CoreAudio/AudioLogger/AudioLoggerSTD.h"
 #endif
