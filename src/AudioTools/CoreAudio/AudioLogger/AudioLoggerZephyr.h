d7bbc7fc3f83f7caeaf66b74c1600a8146d405a2
pschatzmann <phil.schatzmann@gmail.com>
2026-06-04 10:31:19 +0200

    Logger

--- src/AudioTools/CoreAudio/AudioLogger/AudioLoggerZephyr.h
+++ src/AudioTools/CoreAudio/AudioLogger/AudioLoggerZephyr.h
@@ -7,10 +7,8 @@
 #  define LOG_METHOD __PRETTY_FUNCTION__
 #endif
 
-#define AUDIO_TOOLS_LOGGER_SETUP(level) \
-    LOG_MODULE_REGISTER(audio_tools, level)
-
-LOG_MODULE_DECLARE(audio_tools, LOG_LEVEL_DBG);
+#define AUDIO_TOOLS_LOGGER(level) LOG_MODULE_REGISTER(audio_tools, level)
+#define AUDIO_TOOLS_LOGGER_DECLARE(level) LOG_MODULE_DECLARE(audio_tools, level)
 
 #define LOGD(...) LOG_DBG(__VA_ARGS__);
 #define LOGI(...) LOG_INF(__VA_ARGS__);
