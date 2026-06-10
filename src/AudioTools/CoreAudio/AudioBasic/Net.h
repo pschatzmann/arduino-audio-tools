8f0117005316b76de7a3b341e49e24e5a02bc13f
pschatzmann <phil.schatzmann@gmail.com>
2026-06-10 13:30:18 +0200

    Zephyr WiFi: compile errors

--- src/AudioTools/CoreAudio/AudioBasic/Net.h
+++ src/AudioTools/CoreAudio/AudioBasic/Net.h
@@ -1,7 +1,15 @@
 #pragma once
 
+// in zephyr we use the definitions if they are available
+#if defined(__zephyr__)
+#  include <zephyr/kernel.h>
+#  if IS_ENABLED(CONFIG_NETWORKING)// prevent double declarations
+#    include "zephyr/net/net_ip.h"
+#    define IS_NET_DEFINED
+#  endif
+#endif
 
-#ifndef htons
+#if !defined(htons) && !defined(IS_NET_DEFINED)
 #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
 #  define htonl(x) (((x) << 24 & 0xFF000000UL) | ((x) << 8 & 0x00FF0000UL) | ((x) >> 8 & 0x0000FF00UL) | ((x) >> 24 & 0x000000FFUL))
 #  define htons(x) (((uint16_t)(x)&0xff00) >> 8) | (((uint16_t)(x)&0X00FF) << 8)
