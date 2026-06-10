97efa2f55bad270eb0d112ed86c6e316f831e2fa
pschatzmann <phil.schatzmann@gmail.com>
2026-06-10 17:22:18 +0200

    Zephyr: UDPStream support

--- src/AudioTools/Communication/WiFi/WiFiUDPZephyr.h
+++ src/AudioTools/Communication/WiFi/WiFiUDPZephyr.h
@@ -5,6 +5,7 @@
 #include <zephyr/sys/util.h>
 
 #include <cstring>
+#include "IPAddress.h"
 
 namespace audio_tools {
 
@@ -17,9 +18,10 @@ namespace audio_tools {
  * @copyright GPLv3
  */
 
-class WiFiUDP {
+class WiFiUDPZephyr {
  public:
-  WiFiUDP() = default;
+  WiFiUDPZephyr() = default;
+  ~WiFiUDPZephyr() { stop(); }
 
   // -------------------------
   // Arduino API
@@ -44,8 +46,63 @@ class WiFiUDP {
     return 1;
   }
 
+  /**
+   * @brief Join a multicast group and listen on the given port.
+   * @param multicast  Multicast group address (e.g. IPAddress(239,0,0,1))
+   * @param port       UDP port to bind to
+   * @return 1 on success, 0 on failure
+   */
+  uint8_t beginMulticast(IPAddress multicast, uint16_t port) {
+    local_port = port;
+
+    sock = zsock_socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (sock < 0) return 0;
+
+    // Allow multiple sockets to share the same port
+    int reuse = 1;
+    zsock_setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
+
+    // Bind to INADDR_ANY on the multicast port
+    sockaddr_in addr{};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    if (zsock_bind(sock, (sockaddr*)&addr, sizeof(addr)) < 0) {
+      zsock_close(sock);
+      sock = -1;
+      return 0;
+    }
+
+    // Join the multicast group on the default interface
+    struct ip_mreq mreq{};
+    mreq.imr_multiaddr.s_addr = htonl((uint32_t)multicast);
+    mreq.imr_interface.s_addr = htonl(INADDR_ANY);
+
+    if (zsock_setsockopt(sock, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq,
+                         sizeof(mreq)) < 0) {
+      zsock_close(sock);
+      sock = -1;
+      return 0;
+    }
+
+    multicast_addr = multicast;
+    is_multicast = true;
+    return 1;
+  }
+
   void stop() {
     if (sock >= 0) {
+      // Leave multicast group before closing
+      if (is_multicast) {
+        struct ip_mreq mreq{};
+        mreq.imr_multiaddr.s_addr = htonl((uint32_t)multicast_addr);
+        mreq.imr_interface.s_addr = htonl(INADDR_ANY);
+        zsock_setsockopt(sock, IPPROTO_IP, IP_DROP_MEMBERSHIP,
+                         &mreq, sizeof(mreq));
+        is_multicast = false;
+      }
+
       zsock_close(sock);
       sock = -1;
     }
@@ -64,6 +121,16 @@ class WiFiUDP {
       return 0;
     }
 
+    tx_len = 0;
+    return 1;
+  }
+
+  /// Begin a packet to an IPAddress (avoids implicit conversion issues)
+  int beginPacket(IPAddress ip, uint16_t port) {
+    memset(&tx_addr, 0, sizeof(tx_addr));
+    tx_addr.sin_family = AF_INET;
+    tx_addr.sin_port = htons(port);
+    tx_addr.sin_addr.s_addr = htonl((uint32_t)ip);
     tx_len = 0;
     return 1;
   }
@@ -154,6 +221,10 @@ class WiFiUDP {
     return len;
   }
 
+  inline size_t readBytes(uint8_t* dest, size_t len){
+    return read(dest, len);
+  }
+
   int peek() {
     if (rx_offset >= rx_len) return -1;
     return rx_buffer[rx_offset];
@@ -162,7 +233,18 @@ class WiFiUDP {
   // -------------------------
   // Remote info
   // -------------------------
-  char* remoteIP(char* buf) {
+
+  /// Returns the remote IP as an IPAddress (Arduino-compatible)
+  IPAddress remoteIP() const {
+    return IPAddress(ntohl(last_from.sin_addr.s_addr));
+  }
+
+  /// Fills buf with the dotted-decimal remote IP string; returns buf
+  char* remoteIP(char* buf = nullptr) {
+    if (buf ==nullptr){
+        static char buffer[40];
+        buf = buffer;
+    }
     zsock_inet_ntop(AF_INET, &last_from.sin_addr, buf, INET_ADDRSTRLEN);
     return buf;
   }
@@ -190,6 +272,12 @@ class WiFiUDP {
   size_t rx_offset = 0;
 
   sockaddr_in last_from{};
+  bool is_multicast = false;
+  IPAddress multicast_addr;
+
 };
+
+using WiFiUDP = WiFiUDPZephyr;
+using UDP = WiFiUDPZephyr;
 
 }  // namespace audio_tools
