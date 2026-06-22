584d04a435dc6d652defb7096b0af46c3a3699d9
pschatzmann <phil.schatzmann@gmail.com>
2026-06-22 17:55:08 +0000

    Deploying to doxygen from @ pschatzmann/arduino-audio-tools@a3ef5599bc07926eb878f76d1595893995220c35 🚀

--- search/all_19.js
+++ search/all_19.js
@@ -232,7 +232,7 @@ var searchData=
   ['vfsfile_2eh_229',['VFSFile.h',['../_v_f_s_file_8h.html',1,'']]],
   ['vi_230',['vi',['../classaudio__tools_1_1_vorbis_decoder.html#ab0f1683720d1dc40770009fd6c8d3722',1,'audio_tools::VorbisDecoder']]],
   ['vid_231',['vid',['../structaudio__tools_1_1_u_s_b_audio_config.html#aac5ffad7f07bfef80deaef77e0eeca69',1,'audio_tools::USBAudioConfig']]],
-  ['video_232',['video',['../group__basic.html#gga5b1869d184b84711842bfe4f25433338ae60ae31f67ab883c746bb71c7a145c18',1,'audio_tools::VIDEO'],['../namespaceaudio__tools.html#a3529160d6b73e5a63e0a95afeb424659aff13e87d7cbbe18fface435a5b84aaec',1,'audio_tools::Video'],['../group__video.html',1,'Video']]],
+  ['video_232',['video',['../namespaceaudio__tools.html#a3529160d6b73e5a63e0a95afeb424659aff13e87d7cbbe18fface435a5b84aaec',1,'audio_tools::Video'],['../group__basic.html#gga5b1869d184b84711842bfe4f25433338ae60ae31f67ab883c746bb71c7a145c18',1,'audio_tools::VIDEO'],['../group__video.html',1,'Video']]],
   ['video_2eh_233',['Video.h',['../_video_8h.html',1,'']]],
   ['video_5favc_234',['VIDEO_AVC',['../group__basic.html#gga5b1869d184b84711842bfe4f25433338a5a63b30a669280e6c747f70c796c53af',1,'audio_tools']]],
   ['video_5fformat_235',['video_format',['../classaudio__tools_1_1_a_v_i_decoder.html#a8ac94e596326d1711852e270b0adecd4',1,'audio_tools::AVIDecoder']]],
