cd7ffd8de73713d12b3765f6e1db8d94f6cfe126
pschatzmann <phil.schatzmann@gmail.com>
2026-07-05 06:56:02 +0000

    Deploying to doxygen from @ pschatzmann/arduino-audio-tools@7deee1c498f003842327984b4330d831593c7931 🚀

--- search/classes_10.js
+++ search/classes_10.js
@@ -6,14 +6,13 @@ var searchData=
   ['queue_3c_20audio_5ftools_3a_3azephyrfile_20_3e_3',['Queue&lt; audio_tools::ZephyrFile &gt;',['../classaudio__tools_1_1_queue.html',1,'audio_tools']]],
   ['queuefromvector_4',['QueueFromVector',['../classaudio__tools_1_1_queue_from_vector.html',1,'audio_tools']]],
   ['queuefromvector_3c_20audio_5ftools_3a_3abasebuffer_3c_20frame_20_3e_20_2a_20_3e_5',['QueueFromVector&lt; audio_tools::BaseBuffer&lt; Frame &gt; * &gt;',['../classaudio__tools_1_1_queue_from_vector.html',1,'audio_tools']]],
-  ['queuefromvector_3c_20audio_5ftools_3a_3abasebuffer_3c_20int16_5ft_20_3e_20_2a_20_3e_6',['QueueFromVector&lt; audio_tools::BaseBuffer&lt; int16_t &gt; * &gt;',['../classaudio__tools_1_1_queue_from_vector.html',1,'audio_tools']]],
-  ['queuefromvector_3c_20audio_5ftools_3a_3abasebuffer_3c_20t_20_3e_20_2a_20_3e_7',['QueueFromVector&lt; audio_tools::BaseBuffer&lt; T &gt; * &gt;',['../classaudio__tools_1_1_queue_from_vector.html',1,'audio_tools']]],
-  ['queuefromvector_3c_20audio_5ftools_3a_3abasebuffer_3c_20uint8_5ft_20_3e_20_2a_20_3e_8',['QueueFromVector&lt; audio_tools::BaseBuffer&lt; uint8_t &gt; * &gt;',['../classaudio__tools_1_1_queue_from_vector.html',1,'audio_tools']]],
-  ['queuelockfree_9',['QueueLockFree',['../classaudio__tools_1_1_queue_lock_free.html',1,'audio_tools']]],
-  ['queuertos_10',['QueueRTOS',['../classaudio__tools_1_1_queue_r_t_o_s.html',1,'audio_tools']]],
-  ['queuertos_3c_20audio_5ftools_3a_3abasebuffer_3c_20t_20_3e_20_2a_20_3e_11',['QueueRTOS&lt; audio_tools::BaseBuffer&lt; T &gt; * &gt;',['../classaudio__tools_1_1_queue_r_t_o_s.html',1,'audio_tools']]],
-  ['queuestream_12',['QueueStream',['../classaudio__tools_1_1_queue_stream.html',1,'audio_tools']]],
-  ['queuestream_3c_20uint8_5ft_20_3e_13',['QueueStream&lt; uint8_t &gt;',['../classaudio__tools_1_1_queue_stream.html',1,'audio_tools']]],
-  ['queuezephyr_14',['QueueZephyr',['../classaudio__tools_1_1_queue_zephyr.html',1,'audio_tools']]],
-  ['queuezephyr_3c_20audio_5ftools_3a_3abasebuffer_3c_20t_20_3e_20_2a_20_3e_15',['QueueZephyr&lt; audio_tools::BaseBuffer&lt; T &gt; * &gt;',['../classaudio__tools_1_1_queue_zephyr.html',1,'audio_tools']]]
+  ['queuefromvector_3c_20audio_5ftools_3a_3abasebuffer_3c_20t_20_3e_20_2a_20_3e_6',['QueueFromVector&lt; audio_tools::BaseBuffer&lt; T &gt; * &gt;',['../classaudio__tools_1_1_queue_from_vector.html',1,'audio_tools']]],
+  ['queuefromvector_3c_20audio_5ftools_3a_3abasebuffer_3c_20uint8_5ft_20_3e_20_2a_20_3e_7',['QueueFromVector&lt; audio_tools::BaseBuffer&lt; uint8_t &gt; * &gt;',['../classaudio__tools_1_1_queue_from_vector.html',1,'audio_tools']]],
+  ['queuelockfree_8',['QueueLockFree',['../classaudio__tools_1_1_queue_lock_free.html',1,'audio_tools']]],
+  ['queuertos_9',['QueueRTOS',['../classaudio__tools_1_1_queue_r_t_o_s.html',1,'audio_tools']]],
+  ['queuertos_3c_20audio_5ftools_3a_3abasebuffer_3c_20t_20_3e_20_2a_20_3e_10',['QueueRTOS&lt; audio_tools::BaseBuffer&lt; T &gt; * &gt;',['../classaudio__tools_1_1_queue_r_t_o_s.html',1,'audio_tools']]],
+  ['queuestream_11',['QueueStream',['../classaudio__tools_1_1_queue_stream.html',1,'audio_tools']]],
+  ['queuestream_3c_20uint8_5ft_20_3e_12',['QueueStream&lt; uint8_t &gt;',['../classaudio__tools_1_1_queue_stream.html',1,'audio_tools']]],
+  ['queuezephyr_13',['QueueZephyr',['../classaudio__tools_1_1_queue_zephyr.html',1,'audio_tools']]],
+  ['queuezephyr_3c_20audio_5ftools_3a_3abasebuffer_3c_20t_20_3e_20_2a_20_3e_14',['QueueZephyr&lt; audio_tools::BaseBuffer&lt; T &gt; * &gt;',['../classaudio__tools_1_1_queue_zephyr.html',1,'audio_tools']]]
 ];
