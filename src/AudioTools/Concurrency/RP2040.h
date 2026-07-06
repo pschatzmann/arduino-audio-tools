f1aa1f71a6b4331708e747269b4c179d1ca7b2ac
Nikita Savyolov <ga2mer@yandex.ru>
2026-07-06 10:35:18 +0300

    fix: use_freertos define typo

--- src/AudioTools/Concurrency/RP2040.h
+++ src/AudioTools/Concurrency/RP2040.h
@@ -4,7 +4,7 @@
 #include "AudioTools/Concurrency/SynchronizedQueue.h"
 #if defined(FREERTOS_H) || defined(INC_FREERTOS_H)
 #  include "RTOS.h" 
-#  define USE_FREETROS
+#  define USE_FREERTOS
 #else
 #  include "AudioTools/Concurrency/RP2040/BufferRP2040.h"
 #  include "AudioTools/Concurrency/RP2040/MutexRP2040.h"
