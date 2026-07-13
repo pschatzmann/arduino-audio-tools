35a8e7ca88c87fd42247eeb154e1db4156968a10
pschatzmann <phil.schatzmann@gmail.com>
2026-07-13 12:51:39 +0000

    Deploying to doxygen from @ pschatzmann/arduino-audio-tools@a8187326d63fa5a6b73255291ede19f41557d94f 🚀

--- search/groups_0.js
+++ search/groups_0.js
@@ -1,7 +1,8 @@
 var searchData=
 [
   ['acoustic_20echo_20canceller_0',['EchoState: Acoustic echo canceller',['../group___echo_state.html',1,'']]],
-  ['arduino_20audio_20tools_1',['Arduino Audio Tools',['../group__main.html',1,'']]],
-  ['audio_2',['SPI Audio',['../group__spi-audio.html',1,'']]],
-  ['audio_20tools_3',['Arduino Audio Tools',['../group__main.html',1,'']]]
+  ['aec_1',['AEC',['../group__aec.html',1,'']]],
+  ['arduino_20audio_20tools_2',['Arduino Audio Tools',['../group__main.html',1,'']]],
+  ['audio_3',['SPI Audio',['../group__spi-audio.html',1,'']]],
+  ['audio_20tools_4',['Arduino Audio Tools',['../group__main.html',1,'']]]
 ];
