9b4f42d7b1bcc27bb62495a9d1848f56a285a53e
pschatzmann <phil.schatzmann@gmail.com>
2026-07-21 15:23:38 +0000

    Deploying to doxygen from @ pschatzmann/arduino-audio-tools@2eee72a5383887faa83e5fa02aeb48b7d07077bc 🚀

--- search/defines_7.js
+++ search/defines_7.js
@@ -1,15 +1,16 @@
 var searchData=
 [
-  ['helix_5fprint_0',['helix_print',['../_codec_a_a_c_helix_8h.html#ad400f8a92e40568532cf4bf203f88036',1,'HELIX_PRINT:&#160;CodecAACHelix.h'],['../_codec_m_p3_helix_8h.html#ad400f8a92e40568532cf4bf203f88036',1,'HELIX_PRINT:&#160;CodecMP3Helix.h']]],
-  ['high_1',['HIGH',['../_arduino_8h.html#a5bb885982ff66a2e0a0a45a8ee9c35e2',1,'Arduino.h']]],
-  ['hls_5fbuffer_5fcount_2',['HLS_BUFFER_COUNT',['../_h_l_s_stream_8h.html#aeca9c4623ffe581215a56ed2a475283e',1,'HLSStream.h']]],
-  ['hls_5fmax_5fno_5fread_3',['HLS_MAX_NO_READ',['../_h_l_s_stream_8h.html#aa7e03ec9a2ad94a5404a4e48e07d4c5c',1,'HLSStream.h']]],
-  ['hls_5fmax_5furl_5flen_4',['HLS_MAX_URL_LEN',['../_h_l_s_stream_8h.html#a8807a8de817684e7d481d8d74a7ae17b',1,'HLSStream.h']]],
-  ['hls_5ftimeout_5',['HLS_TIMEOUT',['../_h_l_s_stream_8h.html#ab13e949e4c6b8b34f99e6075e135ea10',1,'HLSStream.h']]],
-  ['hls_5funder_5foverflow_5fwait_5ftime_6',['HLS_UNDER_OVERFLOW_WAIT_TIME',['../_h_l_s_stream_8h.html#aa1c0f32c3e7798b893be95f8700a59a1',1,'HLSStream.h']]],
-  ['htonl_7',['htonl',['../_net_8h.html#ae4027a6ad07f13aa12eab285a1b46019',1,'Net.h']]],
-  ['htonll_8',['htonll',['../_net_8h.html#a9f4bf0773c45ad9a9753a1b784a13fbb',1,'Net.h']]],
-  ['htons_9',['htons',['../_net_8h.html#a51799f5ebb4c7228ef7e95c247030f42',1,'Net.h']]],
-  ['http_5fchunked_5fsize_5fmax_5flen_10',['HTTP_CHUNKED_SIZE_MAX_LEN',['../_audio_tools_config_8h.html#a7e678a8232ce307a3e1062d88f786e37',1,'AudioToolsConfig.h']]],
-  ['http_5fmax_5flen_11',['HTTP_MAX_LEN',['../_audio_tools_config_8h.html#afd14aef32e8f25b56daa6dc1e219ed16',1,'AudioToolsConfig.h']]]
+  ['has_5fclient_5fsecure_0',['HAS_CLIENT_SECURE',['../_u_r_l_stream_8h.html#adac5166a4ab2a7a6ad477aff432f5279',1,'URLStream.h']]],
+  ['helix_5fprint_1',['helix_print',['../_codec_a_a_c_helix_8h.html#ad400f8a92e40568532cf4bf203f88036',1,'HELIX_PRINT:&#160;CodecAACHelix.h'],['../_codec_m_p3_helix_8h.html#ad400f8a92e40568532cf4bf203f88036',1,'HELIX_PRINT:&#160;CodecMP3Helix.h']]],
+  ['high_2',['HIGH',['../_arduino_8h.html#a5bb885982ff66a2e0a0a45a8ee9c35e2',1,'Arduino.h']]],
+  ['hls_5fbuffer_5fcount_3',['HLS_BUFFER_COUNT',['../_h_l_s_stream_8h.html#aeca9c4623ffe581215a56ed2a475283e',1,'HLSStream.h']]],
+  ['hls_5fmax_5fno_5fread_4',['HLS_MAX_NO_READ',['../_h_l_s_stream_8h.html#aa7e03ec9a2ad94a5404a4e48e07d4c5c',1,'HLSStream.h']]],
+  ['hls_5fmax_5furl_5flen_5',['HLS_MAX_URL_LEN',['../_h_l_s_stream_8h.html#a8807a8de817684e7d481d8d74a7ae17b',1,'HLSStream.h']]],
+  ['hls_5ftimeout_6',['HLS_TIMEOUT',['../_h_l_s_stream_8h.html#ab13e949e4c6b8b34f99e6075e135ea10',1,'HLSStream.h']]],
+  ['hls_5funder_5foverflow_5fwait_5ftime_7',['HLS_UNDER_OVERFLOW_WAIT_TIME',['../_h_l_s_stream_8h.html#aa1c0f32c3e7798b893be95f8700a59a1',1,'HLSStream.h']]],
+  ['htonl_8',['htonl',['../_net_8h.html#ae4027a6ad07f13aa12eab285a1b46019',1,'Net.h']]],
+  ['htonll_9',['htonll',['../_net_8h.html#a9f4bf0773c45ad9a9753a1b784a13fbb',1,'Net.h']]],
+  ['htons_10',['htons',['../_net_8h.html#a51799f5ebb4c7228ef7e95c247030f42',1,'Net.h']]],
+  ['http_5fchunked_5fsize_5fmax_5flen_11',['HTTP_CHUNKED_SIZE_MAX_LEN',['../_audio_tools_config_8h.html#a7e678a8232ce307a3e1062d88f786e37',1,'AudioToolsConfig.h']]],
+  ['http_5fmax_5flen_12',['HTTP_MAX_LEN',['../_audio_tools_config_8h.html#afd14aef32e8f25b56daa6dc1e219ed16',1,'AudioToolsConfig.h']]]
 ];
