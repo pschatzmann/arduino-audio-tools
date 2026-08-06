b2f34d16ea21b05acc14d3585401d4218c04cfb9
pschatzmann <phil.schatzmann@gmail.com>
2026-08-06 17:15:56 +0000

    Deploying to doxygen from @ pschatzmann/arduino-audio-tools@852ae9e0d3c8ad346db4fbfd030b9cc3e0da472e 🚀

--- search/defines_14.js
+++ search/defines_14.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['wave_5fformat_5fima_5fadpcm_0',['WAVE_FORMAT_IMA_ADPCM',['../_codec_wav_i_m_a_8h.html#a315f0ad8814e4f3da951782b83873c2d',1,'CodecWavIMA.h']]]
+  ['wav_5fencoder_5fcompressed_5fchunk_5fsize_0',['WAV_ENCODER_COMPRESSED_CHUNK_SIZE',['../_codec_w_a_v_8h.html#afe33858858ec2db2758cbdc03d87df91',1,'CodecWAV.h']]],
+  ['wave_5fformat_5fima_5fadpcm_1',['WAVE_FORMAT_IMA_ADPCM',['../_codec_wav_i_m_a_8h.html#a315f0ad8814e4f3da951782b83873c2d',1,'CodecWavIMA.h']]]
 ];
