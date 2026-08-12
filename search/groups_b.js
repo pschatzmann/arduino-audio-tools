84a88bd2c4f5ae18533257d4ce507ab429d56314
pschatzmann <phil.schatzmann@gmail.com>
2026-08-12 15:56:23 +0000

    Deploying to doxygen from @ pschatzmann/arduino-audio-tools@0bdd8b9984b42047d4cc5fcc93ad66facd755e7f 🚀

--- search/groups_b.js
+++ search/groups_b.js
@@ -3,5 +3,6 @@ var searchData=
   ['machine_20learning_0',['Machine Learning',['../group__ml.html',1,'']]],
   ['management_1',['Memory Management',['../group__memorymgmt.html',1,'']]],
   ['memory_20management_2',['Memory Management',['../group__memorymgmt.html',1,'']]],
-  ['metadata_3',['Metadata',['../group__metadata.html',1,'']]]
+  ['metadata_3',['Metadata',['../group__metadata.html',1,'']]],
+  ['mpg_4',['MPG',['../group__mpg.html',1,'']]]
 ];
